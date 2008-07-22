e6632ad55ba789c5622752d192630f211094b4a9
fgolf <fgolf>
2008-07-22 20:12:20 +0000

    change association to matching four vectors, added deltaR

--- src/MuToGenAssMaker.cc
+++ src/MuToGenAssMaker.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  pts/4
 //         Created:  Fri Jun  6 11:07:38 CDT 2008
-// $Id: MuToGenAssMaker.cc,v 1.2 2008/07/09 06:36:41 jmuelmen Exp $
+// $Id: MuToGenAssMaker.cc,v 1.3 2008/07/22 20:12:20 fgolf Exp $
 //
 //
 
@@ -42,6 +42,7 @@
 
 typedef math::XYZTLorentzVector LorentzVector;
 using std::vector;
+using namespace edm;
 
 MuToGenAssMaker::MuToGenAssMaker(const edm::ParameterSet& iConfig)
 {
@@ -49,9 +50,7 @@ MuToGenAssMaker::MuToGenAssMaker(const edm::ParameterSet& iConfig)
      produces<vector<int>           >("musmcmotherid").setBranchAlias("mus_mc_motherid");
      produces<vector<int>           >("musmcidx"     ).setBranchAlias("mus_mcidx"      );
      produces<vector<LorentzVector> >("musmcp4"      ).setBranchAlias("mus_mcp4"       );
-
-     //muonsInputTag        = iConfig.getParameter<edm::InputTag>("muonsInputTag"       );
-     //genParticlesInputTag = iConfig.getParameter<edm::InputTag>("genParticlesInputTag");
+     produces<vector<double>        >("musmcdr"      ).setBranchAlias("mus_mcdr"       );
 }
 
 void MuToGenAssMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
@@ -62,30 +61,46 @@ void MuToGenAssMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
      std::auto_ptr<vector<int>           > vector_mus_mc_motherid(new vector<int>          );
      std::auto_ptr<vector<int>           > vector_mus_mcidx      (new vector<int>          );
      std::auto_ptr<vector<LorentzVector> > vector_mus_mcp4       (new vector<LorentzVector>);
+     std::auto_ptr<vector<double>        > vector_mus_mcdr       (new vector<double>       );
 
      // get muons
-     Handle<edm::View<reco::Muon> > muonhandle;
-     iEvent.getByLabel("allLayer1TopMuons", muonhandle);      // change this in the future
+     Handle<vector<LorentzVector> > mus_p4_h;
+     iEvent.getByLabel("muonMaker", "musp4", mus_p4_h);
 
-     // get MC particle collection
-     edm::Handle<reco::GenParticleCollection> genParticlesHandle;
-     iEvent.getByLabel("genParticles", genParticlesHandle);
+     // get MC particles
+     Handle<vector<LorentzVector> > genps_p4_h;
+     iEvent.getByLabel("genMaker", "genpsp4", genps_p4_h);
 
-     for (edm::View<reco::Muon>::const_iterator muon = muonhandle->begin(),
-	  muons_end = muonhandle->end();
-	  muon != muons_end; ++muon) {
+     Handle<vector<int> > genps_id_h;
+     iEvent.getByLabel("genMaker", "genpsid", genps_id_h);
+
+     Handle<vector<int> > genps_id_mother_h;
+     iEvent.getByLabel("genMaker", "genpsidmother", genps_id_mother_h);
+
+     for (vector<LorentzVector>::const_iterator muon = mus_p4_h->begin(),
+	  mus_end = mus_p4_h->end();
+	  muon != mus_end; ++muon) { 
 
        //MC matching stuff
        int mcid = -999, mom_mcid = -999, genidx = -999;
-
-       const reco::GenParticle* matchedGenParticle = MatchUtilities::matchCandToGen(*muon, genParticlesHandle.product(), genidx);
-
        LorentzVector mc_p4(0,0,0,0);
 
-       if(matchedGenParticle != 0) {
-	 mcid                = matchedGenParticle->pdgId();
-	 LorentzVector mc_p4 = matchedGenParticle->p4();
-	 mom_mcid            = MCUtilities::motherID(*matchedGenParticle)->pdgId();
+       int genp = 0; // gen particle counter
+       double min_dR = 999;
+
+       for (vector<LorentzVector>::const_iterator genps = genps_p4_h->begin(),
+	    genps_end = genps_p4_h->end();
+	    genps != genps_end; ++genp) { 
+
+	 const double deltaR = ROOT::Math::VectorUtil::DeltaR(*muon, *genps);
+
+	 if (deltaR < min_dR) {
+	   min_dR   = deltaR;
+	   mcid     = (*genps_id_h)[genp];
+	   genidx   = genp;
+	   mom_mcid = (*genps_id_mother_h)[genp];
+	   mc_p4    = (*genps_p4_h)[genp];
+	 }	 
        }
 
        // fill vector
@@ -93,6 +108,7 @@ void MuToGenAssMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
        vector_mus_mc_motherid->push_back(mom_mcid);
        vector_mus_mcidx      ->push_back(genidx  );
        vector_mus_mcp4       ->push_back(mc_p4   );
+       vector_mus_mcdr       ->push_back(min_dR  );
      }
 
      // store vectors
@@ -100,6 +116,7 @@ void MuToGenAssMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
      iEvent.put(vector_mus_mc_motherid, "musmcmotherid");
      iEvent.put(vector_mus_mcidx      , "musmcidx"     );
      iEvent.put(vector_mus_mcp4       , "musmcp4"      );
+     iEvent.put(vector_mus_mcdr       , "musmcdr"      );
 }
 
 // ------------ method called once each job just before starting event loop  ------------
--- src/TrackToGenAssMaker.cc
+++ src/TrackToGenAssMaker.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  pts/4
 //         Created:  Fri Jun  6 11:07:38 CDT 2008
-// $Id: TrackToGenAssMaker.cc,v 1.2 2008/07/09 06:36:41 jmuelmen Exp $
+// $Id: TrackToGenAssMaker.cc,v 1.3 2008/07/22 20:12:20 fgolf Exp $
 //
 //
 
@@ -41,6 +41,7 @@
 
 typedef math::XYZTLorentzVector LorentzVector;
 using std::vector;
+using namespace edm;
 
 TrackToGenAssMaker::TrackToGenAssMaker(const edm::ParameterSet& iConfig)
 {
@@ -48,9 +49,7 @@ TrackToGenAssMaker::TrackToGenAssMaker(const edm::ParameterSet& iConfig)
      produces<vector<int>           >("trkmcmotherid").setBranchAlias("trk_mc_motherid");
      produces<vector<int>           >("trkmcidx"     ).setBranchAlias("trk_mcidx"      );
      produces<vector<LorentzVector> >("trkmcp4"      ).setBranchAlias("trk_mcp4"       );
-
-     //tracksInputTag       = iConfig.getParameter<InputTag>("tracksInputTag"      );
-     //genParticlesInputTag = iConfig.getParameter<InputTag>("genParticlesInputTag");
+     produces<vector<double>        >("trkmcdr"      ).setBranchAlias("trk_mcdr"       );
 }
 
 void TrackToGenAssMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
@@ -61,31 +60,46 @@ void TrackToGenAssMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSet
      std::auto_ptr<vector<int>           > vector_trk_mc_motherid(new vector<int>          );
      std::auto_ptr<vector<int>           > vector_trk_mcidx      (new vector<int>          );
      std::auto_ptr<vector<LorentzVector> > vector_trk_mcp4       (new vector<LorentzVector>);
+     std::auto_ptr<vector<double>        > vector_trk_mcdr       (new vector<double>       );
 
      // get tracks
-     Handle<edm::View<reco::Track> > trackhandle;
-     iEvent.getByLabel("ctfWithMaterialTracks", trackhandle);
+     Handle<vector<LorentzVector> > trks_p4_h;
+     iEvent.getByLabel("trackMaker", "trkstrkp4", trks_p4_h);
 
-     // get MC particle collection
-     edm::Handle<reco::GenParticleCollection> genParticlesHandle;
-     iEvent.getByLabel("genParticles", genParticlesHandle);
+     // get MC particles
+     Handle<vector<LorentzVector> > genps_p4_h;
+     iEvent.getByLabel("genMaker", "genpsp4", genps_p4_h);
 
-     for (edm::View<reco::Track>::const_iterator track = trackhandle->begin(),
-	  tracks_end = trackhandle->end();
-	  track != tracks_end; ++track) {
+     Handle<vector<int> > genps_id_h;
+     iEvent.getByLabel("genMaker", "genpsid", genps_id_h);
+
+     Handle<vector<int> > genps_id_mother_h;
+     iEvent.getByLabel("genMaker", "genpsidmother", genps_id_mother_h);
+
+     for (vector<LorentzVector>::const_iterator track = trks_p4_h->begin(),
+	  trks_end = trks_p4_h->end();
+	  track != trks_end; ++track) { 
 
        //MC matching stuff
-
        int mcid = -999, mom_mcid = -999, genidx = -999;
-
-       const reco::GenParticle* matchedGenParticle = MatchUtilities::matchCandToGen(*track, genParticlesHandle.product(), genidx);
-
        LorentzVector mc_p4(0,0,0,0);
 
-       if(matchedGenParticle != 0) {
-	 mcid                = matchedGenParticle->pdgId();
-	 LorentzVector mc_p4 = matchedGenParticle->p4();
-	 mom_mcid            = MCUtilities::motherID(*matchedGenParticle)->pdgId();
+       int genp = 0; // gen particle counter
+       double min_dR = 999;
+
+       for (vector<LorentzVector>::const_iterator genps = genps_p4_h->begin(),
+	    genps_end = genps_p4_h->end();
+	    genps != genps_end; ++genp) { 
+
+	 const double deltaR = ROOT::Math::VectorUtil::DeltaR(*track, *genps);
+
+	 if (deltaR < min_dR) {
+	   min_dR   = deltaR;
+	   mcid     = (*genps_id_h)[genp];
+	   genidx   = genp;
+	   mom_mcid = (*genps_id_mother_h)[genp];
+	   mc_p4    = (*genps_p4_h)[genp];
+	 }	 
        }
 
        // fill vector
@@ -93,6 +107,7 @@ void TrackToGenAssMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSet
        vector_trk_mc_motherid->push_back(mom_mcid);
        vector_trk_mcidx      ->push_back(genidx  );
        vector_trk_mcp4       ->push_back(mc_p4   );
+       vector_trk_mcdr       ->push_back(min_dR  );
      }
 
      // store vectors
@@ -100,6 +115,7 @@ void TrackToGenAssMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSet
      iEvent.put(vector_trk_mc_motherid, "trkmcmotherid");
      iEvent.put(vector_trk_mcidx      , "trkmcidx"     );
      iEvent.put(vector_trk_mcp4       , "trkmcp4"      );
+     iEvent.put(vector_trk_mcdr       , "trkmcdr"      );
 }
 
 // ------------ method called once each job just before starting event loop  ------------
