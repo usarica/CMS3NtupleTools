1373213363dd7ac33aafa310effb237b900ba342
kalavase <kalavase>
2008-12-17 09:08:53 +0000

    cosmetic change

--- interface/MuToTrackAssMaker.h
+++ interface/MuToTrackAssMaker.h
@@ -13,11 +13,11 @@
 //
 // Original Author:  pts/4
 //         Created:  Fri Jun  6 11:07:38 CDT 2008
-// $Id: MuToTrackAssMaker.h,v 1.3 2008/12/16 03:48:22 slava77 Exp $
+// $Id: MuToTrackAssMaker.h,v 1.4 2008/12/17 09:08:53 kalavase Exp $
 //
 //
-#ifndef CMS2_MuToTrackAssMaker_H
-#define CMS2_MuToTrackAssMaker_H
+#ifndef CMS2_MUTOTRACKASSMAKER_H
+#define CMS2_MUTOTRACKASSMAKER_H
 
 // user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
