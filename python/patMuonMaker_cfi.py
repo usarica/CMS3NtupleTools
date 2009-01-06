8d6f65bd9a7374fc90fd0039587d67bc329f9c7f
kalavase <kalavase>
2009-01-06 00:56:14 +0000

    changed muon collection to selectedLayer1Muons

--- python/patMuonMaker_cfi.py
+++ python/patMuonMaker_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 patMuonMaker = cms.EDFilter("PATMuonMaker",
     # pat muon collection
-    patMuonsInputTag = cms.InputTag("allLayer1Muons")
+    patMuonsInputTag = cms.InputTag("selectedLayer1Muons")
 )
 
 
