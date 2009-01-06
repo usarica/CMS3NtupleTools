92ede7d4c7c2efd003bcd868f387f665236f5772
kalavase <kalavase>
2009-01-06 00:56:57 +0000

    changed MET collection to selectedLayer1MET

--- python/patMETMaker_cfi.py
+++ python/patMETMaker_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 patMETMaker = cms.EDFilter("PATMETMaker",
     # met collection
-    patMETsInputTag = cms.InputTag("allLayer1METs")
+    patMETsInputTag = cms.InputTag("selectedLayer1METs")
 )
 
 
