d12aef3380799cc836f9f193c83fd988e25d91cd
fgolf <fgolf>
2009-02-12 00:44:14 +0000

    updated to siscone jets

--- python/jptMaker_cfi.py
+++ python/jptMaker_cfi.py
@@ -5,7 +5,8 @@ jptMaker = cms.EDFilter("JPTMaker",
     jptInputTag = cms.InputTag("JetPlusTrackZSPCorJetIcone5"),
 
     # ic5jet collection
-    ic5jetInputTag = cms.InputTag("iterativeCone5CaloJets")
+#    ic5jetInputTag = cms.InputTag("iterativeCone5CaloJets")
+    ic5jetInputTag = cms.InputTag("sisCone5CaloJets")
 )
 
 
