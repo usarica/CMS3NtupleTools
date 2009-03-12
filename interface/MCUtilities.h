44da73b3ba69b9f9156c28212e8f0acc722a9773
warren <warren>
2009-03-12 22:46:35 +0000

    Added function for genps_lepdaughter branches

--- interface/MCUtilities.h
+++ interface/MCUtilities.h
@@ -11,7 +11,7 @@ Description: MC utilities
 //
 // Original Author:  Oliver Gutsche
 // Thu Jun 12 22:55:46 UTC 2008
-// $Id: MCUtilities.h,v 1.1 2008/06/12 23:39:38 gutsche Exp $
+// $Id: MCUtilities.h,v 1.2 2009/03/12 22:46:35 warren Exp $
 //
 //
 #ifndef CMS2_MCUTILITIES_H
@@ -19,6 +19,10 @@ Description: MC utilities
 
 #include "DataFormats/Candidate/interface/Candidate.h"
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include <vector>
+#include "DataFormats/Math/interface/LorentzVector.h"
+
+typedef math::XYZTLorentzVector LorentzVector;
 
 class MCUtilities {
 public:
@@ -26,6 +30,8 @@ public:
   ~MCUtilities();
 
   static const reco::GenParticle* motherID(const reco::GenParticle& gp);
+  static void writeDaughter(const reco::GenParticle& gp, int idx, std::auto_ptr<std::vector<int> > &genps_ld_id,
+							std::auto_ptr<std::vector<int> > &genps_ld_idx, std::auto_ptr<std::vector<LorentzVector> > &genps_ld_p4 );
 
 private:
 
--- src/MCUtilities.cc
+++ src/MCUtilities.cc
@@ -1,4 +1,10 @@
 #include "CMS2/NtupleMaker/interface/MCUtilities.h"
+#include "DataFormats/Math/interface/LorentzVector.h"
+
+typedef math::XYZTLorentzVector LorentzVector;
+using namespace reco;
+using namespace std;
+
 
 MCUtilities::MCUtilities() {
 }
@@ -16,9 +22,32 @@ const reco::GenParticle* MCUtilities::motherID(const reco::GenParticle& gp) {
     for(uint j=0; j<mom->numberOfMothers(); ++j) {
       mom = dynamic_cast<const reco::GenParticle*>(mom->mother(j));
       if(mom->pdgId()!=gp.pdgId()){
-	return mom;
+		return mom;
       }
     }
   }
   return mom;
 }
+
+
+void MCUtilities::writeDaughter( const reco::GenParticle& gp, int idx, auto_ptr<vector<int> > &genps_ld_id,
+								 auto_ptr<vector<int> > &genps_ld_idx, auto_ptr<vector<LorentzVector> > &genps_ld_p4) {
+  //call this for the status 3 particles to add all of their status 1 (not 2) daughters ( and grand daughters and great grand daughters ... )
+
+  for( unsigned int i=0; i < gp.numberOfDaughters(); i++ ) {
+
+	if( gp.daughter(i)->status() == 1 ) {
+
+	  genps_ld_id->push_back( gp.daughter(i)->pdgId() );
+	  genps_ld_idx->push_back( idx );
+	  genps_ld_p4->push_back( LorentzVector(gp.daughter(i)->p4().px(), gp.daughter(i)->p4().py(),
+											gp.daughter(i)->p4().pz(), gp.daughter(i)->p4().e() ) );
+	}
+	else { 
+	  const reco::GenParticle* dau = dynamic_cast<const reco::GenParticle*>(gp.daughter(i));
+	  MCUtilities::writeDaughter( *dau, idx, genps_ld_id, genps_ld_idx, genps_ld_p4 );
+	}
+  }
+
+}
+
