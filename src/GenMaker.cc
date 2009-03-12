48686784dbb415edc824600c70f82b9157a834f2
warren <warren>
2009-03-12 22:53:50 +0000

    Added status 1 lep daughter information

--- src/GenMaker.cc
+++ src/GenMaker.cc
@@ -13,7 +13,7 @@ Implementation:
 //
 // Original Author:  Puneeth Kalavase
 //         Created:  Fri Jun  6 11:07:38 CDT 2008
-// $Id: GenMaker.cc,v 1.4 2009/02/12 22:54:11 ibloch Exp $
+// $Id: GenMaker.cc,v 1.5 2009/03/12 22:53:50 warren Exp $
 //
 //
 
@@ -28,7 +28,7 @@ Implementation:
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
-#include "CMS2/NtupleMaker/interface/GenMaker.h"
+#include "CMS2/NtupleMaker/interface/GenMaker.h" 
 #include "CMS2/NtupleMaker/interface/MCUtilities.h"
 #include "DataFormats/Math/interface/LorentzVector.h"
 
@@ -46,17 +46,18 @@ using namespace std;
 
 GenMaker::GenMaker(const edm::ParameterSet& iConfig) {
 
-  produces<vector<int> >            ("genpsid"          ).setBranchAlias("genps_id"          );
-  produces<vector<int> >            ("genpsidmother"    ).setBranchAlias("genps_id_mother"   );
-  produces<vector<LorentzVector> >  ("genpsp4"          ).setBranchAlias("genps_p4"          );
-  produces<vector<LorentzVector> >  ("genpsprodvtx"     ).setBranchAlias("genps_prod_vtx"    );
-  produces<vector<int> >            ("genpsstatus"      ).setBranchAlias("genps_status"      );
+  produces<vector<int> >           ("genpsid"          ).setBranchAlias("genps_id"          );
+  produces<vector<int> >           ("genpsidmother"    ).setBranchAlias("genps_id_mother"   );
+  produces<vector<LorentzVector> > ("genpsp4"          ).setBranchAlias("genps_p4"          );
+  produces<vector<LorentzVector> > ("genpsprodvtx"     ).setBranchAlias("genps_prod_vtx"    );
+  produces<vector<int> >           ("genpsstatus"      ).setBranchAlias("genps_status"      );
+  produces<vector<int> >           ("genpslepdaughterid").setBranchAlias("genps_lepdaughter_id");
+  produces<vector<int> >           ("genpslepdaughteridx").setBranchAlias("genps_lepdaughter_idx");
+  produces<vector<LorentzVector> > ("genpslepdaughterp4").setBranchAlias("genps_lepdaughter_p4");
   
-  produces< double >                ("genpspthat"       ).setBranchAlias("genps_pthat"       );
-
   genParticlesInputTag = iConfig.getParameter<InputTag>("genParticlesInputTag");
-  genEventScaleInputTag= iConfig.getParameter<InputTag>("genEventScaleInputTag");
   ntupleOnlyStatus3    = iConfig.getParameter<bool>("ntupleOnlyStatus3");
+  ntupleDaughters      = iConfig.getParameter<bool>("ntupleDaughters");
 
 }
 
@@ -73,36 +74,33 @@ void GenMaker::endJob() {
 // ------------ method called to produce the data  ------------
 void GenMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   
-
-  auto_ptr<vector<int> >            genps_id          (new vector<int>             );
-  auto_ptr<vector<int> >            genps_id_mother   (new vector<int>             );
-  auto_ptr<vector<LorentzVector> >  genps_p4          (new vector<LorentzVector>   );
-  auto_ptr<vector<LorentzVector> >  genps_prod_vtx    (new vector<LorentzVector>   );
-  auto_ptr<vector<int> >            genps_status      (new vector<int>             );
-
-  auto_ptr< double >                genps_pthat       (new double                  );
-
-   // get MC particle collection
+  auto_ptr<vector<int> >           genps_id          (new vector<int>             );
+  auto_ptr<vector<int> >           genps_id_mother   (new vector<int>             );
+  auto_ptr<vector<LorentzVector> > genps_p4          (new vector<LorentzVector>   );
+  auto_ptr<vector<LorentzVector> > genps_prod_vtx    (new vector<LorentzVector>   );
+  auto_ptr<vector<int> >           genps_status      (new vector<int>             );
+  auto_ptr<vector<int> >           genps_lepdaughter_id(new vector<int> );
+  auto_ptr<vector<int> >           genps_lepdaughter_idx(new vector<int> );
+  auto_ptr<vector<LorentzVector> > genps_lepdaughter_p4(new vector<LorentzVector> );
+  
+  // get MC particle collection
   edm::Handle<reco::GenParticleCollection> genpsHandle;
   iEvent.getByLabel(genParticlesInputTag, genpsHandle);
   const vector<GenParticle>* genps_coll = genpsHandle.product();
 
-  // get the ptHat scale variable
-  edm::Handle<double> genEventScale;
-  iEvent.getByLabel(genEventScaleInputTag,genEventScale);
-  if( genEventScale.isValid() ) {
-    double ptHat = *genEventScale;
-    *genps_pthat = ptHat;
-  }
-  else {
-    // if handle is not valid, set genEventScale to -1
-    double ptHat = -1;
-    *genps_pthat = ptHat;
-  }
-  
   for(vector<GenParticle>::const_iterator genps_it = genps_coll->begin();
       genps_it != genps_coll->end(); genps_it++) {
-    
+
+	//fill daughter branches
+	if(ntupleDaughters) { //check flag from cfi
+	  int id = genps_it->pdgId();
+	  if((TMath::Abs(id) == 11 || TMath::Abs(id) == 13 || TMath::Abs(id) == 15)
+		 && genps_it->status() == 3 ) {
+		//genps_it->numberOfDaughters() > 1 //if want to ignore e->e
+		MCUtilities::writeDaughter(*genps_it, genps_it-genps_coll->begin(), genps_lepdaughter_id, genps_lepdaughter_idx, genps_lepdaughter_p4);
+	  }
+	}
+
     //look 
     if(ntupleOnlyStatus3 && (genps_it->status() !=3)) continue;
 
@@ -138,7 +136,9 @@ void GenMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   iEvent.put(genps_p4           ,"genpsp4"         );
   iEvent.put(genps_prod_vtx     ,"genpsprodvtx"    );
   iEvent.put(genps_status       ,"genpsstatus"     );
-  iEvent.put(genps_pthat        ,"genpspthat"      );
+  iEvent.put(genps_lepdaughter_id,"genpslepdaughterid");
+  iEvent.put(genps_lepdaughter_idx,"genpslepdaughteridx");
+  iEvent.put(genps_lepdaughter_p4,"genpslepdaughterp4");
 
 }
 
