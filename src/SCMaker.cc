108eb58cbd07501f440b5b1ae6198d6acd707b6a
dlevans <dlevans>
2009-04-30 15:24:01 +0000

    bug fix in SC maker so that scs_pos and scs_vtx are both stored as LorentzVector rather than Point which doesn't work

--- src/SCMaker.cc
+++ src/SCMaker.cc
@@ -64,8 +64,8 @@ SCMaker::SCMaker(const edm::ParameterSet& iConfig)
 
      // positions
      produces<std::vector<LorentzVector> >("scsp4").setBranchAlias("scs_p4");
-     produces<std::vector<Point> >("scsvtx").setBranchAlias("scs_vtx");
-     produces<std::vector<Point> >("scspos").setBranchAlias("scs_pos");
+     produces<std::vector<LorentzVector> >("scsvtx").setBranchAlias("scs_vtx");
+     produces<std::vector<LorentzVector> >("scspos").setBranchAlias("scs_pos");
      produces<std::vector<float> >("scseta").setBranchAlias("scs_eta");
      produces<std::vector<float> >("scsphi").setBranchAlias("scs_phi");
 
@@ -176,8 +176,8 @@ void SCMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
      std::auto_ptr<unsigned int> evt_nscs (new unsigned int);
      std::auto_ptr<std::vector<LorentzVector> > vector_scs_p4 (new std::vector<LorentzVector>);
-     std::auto_ptr<std::vector<Point> > vector_scs_pos (new std::vector<Point>);
-     std::auto_ptr<std::vector<Point> > vector_scs_vtx (new std::vector<Point>);
+     std::auto_ptr<std::vector<LorentzVector> > vector_scs_pos (new std::vector<LorentzVector>);
+     std::auto_ptr<std::vector<LorentzVector> > vector_scs_vtx (new std::vector<LorentzVector>);
      std::auto_ptr<std::vector<float> > vector_scs_eta (new std::vector<float>);
      std::auto_ptr<std::vector<float> > vector_scs_phi (new std::vector<float>);
      std::auto_ptr<std::vector<float> > vector_scs_clustersSize (new std::vector<float>);
@@ -229,8 +229,8 @@ void SCMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
              LorentzVector p4 = initP4(pv, *sc);
 	     vector_scs_p4->push_back( p4 );
-	     vector_scs_vtx->push_back( pv );
-             vector_scs_pos->push_back( sc->position() );
+	     vector_scs_vtx->push_back( LorentzVector(pv.x(), pv.y(), pv.z(), 0.) );
+             vector_scs_pos->push_back( LorentzVector(sc->position().x(), sc->position().y(), sc->position().z(), 0.) );
 	     vector_scs_eta->push_back( sc->eta() );
              vector_scs_phi->push_back( sc->phi() );
              vector_scs_energy->push_back( sc->energy() );
