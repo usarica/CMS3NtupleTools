f86f62b9fcb80eb69b69768d986746ad5c74b554
fgolf <fgolf>
2009-05-14 01:47:16 +0000

    added edm::InputTags for configurability

--- interface/TCMETMaker.h
+++ interface/TCMETMaker.h
@@ -5,15 +5,15 @@
 // 
 /**\class TCMETMaker.cc CMS2/NtupleMaker/src/TCMETMaker.cc
 
- Description: <one line class summary>
+   Description: <one line class summary>
 
- Implementation:
-     <Notes on implementation>
+   Implementation:
+   <Notes on implementation>
 */
 //
 // Original Author:  Puneeth Kalavase
 //         Created:  Fri Jun  6 11:07:38 CDT 2008
-// $Id: TCMETMaker.h,v 1.1 2008/12/17 10:29:20 fgolf Exp $
+// $Id: TCMETMaker.h,v 1.2 2009/05/14 01:47:16 fgolf Exp $
 //
 //
 #ifndef NTUPLEMAKER_TCMETMAKER_H
@@ -32,21 +32,23 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 
 //
-// class decleration
+// class declaration
 //
 
 class TCMETMaker : public edm::EDProducer {
 public:
-     explicit TCMETMaker (const edm::ParameterSet&);
-      ~TCMETMaker();
+  explicit TCMETMaker (const edm::ParameterSet&);
+  ~TCMETMaker();
 
 private:
-     virtual void beginJob(const edm::EventSetup&) ;
-     virtual void produce(edm::Event&, const edm::EventSetup&);
-     virtual void endJob() ;
+  virtual void beginJob(const edm::EventSetup&) ;
+  virtual void produce(edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
 
-      // ----------member data ---------------------------
-  
+  // ----------member data ---------------------------
+  edm::InputTag muon_tag;
+  edm::InputTag tcmet_tag;
+  edm::InputTag tcmet_vm_tag;  
 };
 
 
