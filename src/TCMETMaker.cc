166e73a466df112fd9c0419e51d4bef70eda96bb
fgolf <fgolf>
2009-05-14 01:48:41 +0000

    added edm::ValueMap data members for muons used by tcMET

--- src/TCMETMaker.cc
+++ src/TCMETMaker.cc
@@ -5,15 +5,15 @@
 // 
 /**\class TCMETMaker TCMETMaker.cc CMS2/TCMETMaker/src/TCMETMaker.cc
 
-Description: <one line class summary>
+   Description: <one line class summary>
 
-Implementation:
-<Notes on implementation>
+   Implementation:
+   <Notes on implementation>
 */
 //
 // Original Author:  pts/4
 //         Created:  Fri Jun  6 11:07:38 CDT 2008
-// $Id: TCMETMaker.cc,v 1.3 2009/01/22 05:01:29 fgolf Exp $
+// $Id: TCMETMaker.cc,v 1.4 2009/05/14 01:48:41 fgolf Exp $
 //
 //
 
@@ -33,6 +33,10 @@ Implementation:
 #include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "DataFormats/METReco/interface/CaloMET.h"
 #include "DataFormats/METReco/interface/MET.h"
+#include "DataFormats/METReco/interface/METFwd.h"
+#include "DataFormats/MuonReco/interface/Muon.h"
+#include "DataFormats/MuonReco/interface/MuonFwd.h"
+#include "DataFormats/MuonReco/interface/MuonMETCorrectionData.h"
 
 #include "SimDataFormats/HepMCProduct/interface/HepMCProduct.h"
 
@@ -46,9 +50,17 @@ using namespace std;
 
 TCMETMaker::TCMETMaker(const edm::ParameterSet& iConfig) {
 
-  produces<float> ("evttcmet"          ).setBranchAlias("evt_tcmet"          );
-  produces<float> ("evttcmetPhi"       ).setBranchAlias("evt_tcmetPhi"       );
-  produces<float> ("evttcsumet"         ).setBranchAlias("evt_tcsumet"       );
+  produces<float>         ("evttcmet"       ).setBranchAlias("evt_tcmet"       );
+  produces<float>         ("evttcmetPhi"    ).setBranchAlias("evt_tcmetPhi"    );
+  produces<float>         ("evttcsumet"     ).setBranchAlias("evt_tcsumet"     );
+  produces<vector<int> >  ("mustcmetflag"   ).setBranchAlias("mus_tcmet_flag"  );
+  produces<vector<float> >("mustcmetdeltax" ).setBranchAlias("mus_tcmet_deltax");
+  produces<vector<float> >("mustcmetdeltay" ).setBranchAlias("mus_tcmet_deltay");
+
+  // input tags
+  muon_tag     = iConfig.getParameter<edm::InputTag>("muon_tag_"    );
+  tcmet_tag    = iConfig.getParameter<edm::InputTag>("tcmet_tag_"   );
+  tcmet_vm_tag = iConfig.getParameter<edm::InputTag>("tcmet_vm_tag_");
 }
 
 
@@ -64,21 +76,50 @@ void TCMETMaker::endJob() {
 // ------------ method called to produce the data  ------------
 void TCMETMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   
-  auto_ptr<float>   evt_tcmet                 (new float     );
-  auto_ptr<float>   evt_tcmetPhi              (new float     );
-  auto_ptr<float>   evt_tcsumet              (new float     );
+  auto_ptr<float>          evt_tcmet        (new float         );
+  auto_ptr<float>          evt_tcmetPhi     (new float         );
+  auto_ptr<float>          evt_tcsumet      (new float         );
+  auto_ptr<vector<int>   > mus_tcmet_flag   (new vector<int>   ); 
+  auto_ptr<vector<float> > mus_tcmet_deltax (new vector<float> ); 
+  auto_ptr<vector<float> > mus_tcmet_deltay (new vector<float> ); 
 
-  Handle< View<MET> > tcmet_h;
-    
-  iEvent.getByLabel("tcMet",       tcmet_h       );
-  
+  // handles to collections
+  edm::Handle<reco::MuonCollection> muon_h;
+  edm::Handle<reco::METCollection>  tcmet_h;
+  edm::Handle<edm::ValueMap<reco::MuonMETCorrectionData> > tcmet_vm_h;
+
+
+  // get collections
+  iEvent.getByLabel(muon_tag    , muon_h    );
+  iEvent.getByLabel(tcmet_tag   , tcmet_h   );
+  iEvent.getByLabel(tcmet_vm_tag, tcmet_vm_h);
+
+  // fill met quantities
   *evt_tcmet          = (tcmet_h->front()).et();
   *evt_tcmetPhi       = (tcmet_h->front()).phi();
-  *evt_tcsumet          = (tcmet_h->front()).sumEt();
+  *evt_tcsumet        = (tcmet_h->front()).sumEt();
 
-  iEvent.put(evt_tcmet            ,"evttcmet"           );
-  iEvent.put(evt_tcmetPhi         ,"evttcmetPhi"        );
-  iEvent.put(evt_tcsumet          ,"evttcsumet"         ); 
+  edm::ValueMap<reco::MuonMETCorrectionData> tcmet_data = *tcmet_vm_h;
+
+  const unsigned int nMuons = muon_h->size();
+
+  // loop over muons and extract quantities from ValueMap
+  for( unsigned int mus = 0; mus < nMuons; mus++ ) {
+
+    reco::MuonRef muref( muon_h, mus);
+    reco::MuonMETCorrectionData muCorrData = (tcmet_data)[muref];
+
+    mus_tcmet_flag->push_back(muCorrData.type());
+    mus_tcmet_deltax->push_back(muCorrData.corrX());
+    mus_tcmet_deltay->push_back(muCorrData.corrY());
+  }
+
+  iEvent.put(evt_tcmet            , "evttcmet"           );
+  iEvent.put(evt_tcmetPhi         , "evttcmetPhi"        );
+  iEvent.put(evt_tcsumet          , "evttcsumet"         );
+  iEvent.put(mus_tcmet_flag       , "mustcmetflag"       );
+  iEvent.put(mus_tcmet_deltax     , "mustcmetdeltax"     );
+  iEvent.put(mus_tcmet_deltay     , "mustcmetdeltay"     );
 }
 
 //define this as a plug-in
