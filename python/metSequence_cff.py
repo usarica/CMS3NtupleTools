b3f63f2776da7df791c6163f81808640ba0db4f4
fgolf <fgolf>
2009-05-14 01:50:46 +0000

    modified sequence to run new muon met correction and tcMET sequence and incorporated these plus CMS2 modules into a single sequence

--- python/metSequence_cff.py
+++ python/metSequence_cff.py
@@ -1,7 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 from JetMETCorrections.Type1MET.MetMuonCorrections_cff import *
+from JetMETCorrections.Type1MET.MuonMETValueMapProducer_cff import *
 from CMS2.NtupleMaker.metMaker_cfi import *
+from JetMETCorrections.Type1MET.MuonTCMETValueMapProducer_cff import *
+from RecoMET.METProducers.TCMET_cfi import *
+from CMS2.NtupleMaker.tcmetMaker_cfi import *
 
-metCorSequence = cms.Sequence(MetMuonCorrections*metMaker)
+metCorSequence = cms.Sequence(muonMETValueMapProducer*corMetGlobalMuons*metMaker*muonTCMETValueMapProducer*tcMet*tcmetMaker)
 
