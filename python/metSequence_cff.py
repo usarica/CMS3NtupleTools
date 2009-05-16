1380e5e240082fae46d25e6b40e8e236628f1cc2
fgolf <fgolf>
2009-05-16 00:36:06 +0000

    updated met sequence to use new muon, tcmet corrections with edm::ValueMaps

--- python/metSequence_cff.py
+++ python/metSequence_cff.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
+from JetMETCorrections.Type1MET.MuonMETValueMapProducer_cff import *
 from JetMETCorrections.Type1MET.MetMuonCorrections_cff import *
 from JetMETCorrections.Type1MET.MuonMETValueMapProducer_cff import *
 from CMS2.NtupleMaker.metMaker_cfi import *
