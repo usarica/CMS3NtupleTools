154d00debebddcef38018fde0ce92bc603a607f5
kalavase <kalavase>
2009-05-17 22:49:45 +0000

    changed pat MET collection label to reflect PATv2

--- python/patMETMaker_cfi.py
+++ python/patMETMaker_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 patMETMaker = cms.EDFilter("PATMETMaker",
     # met collection
-    patMETsInputTag = cms.InputTag("selectedLayer1METs")
+    patMETsInputTag = cms.InputTag("layer1METs")
 )
 
 
