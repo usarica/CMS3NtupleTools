b62dc9836adf025d6332f396a7874de3f8c405cf
kalavase <kalavase>
2009-05-17 19:44:29 +0000

    Added corrected input collections

--- python/jetMaker_cfi.py
+++ python/jetMaker_cfi.py
@@ -1,18 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 jetMaker = cms.EDFilter("JetMaker",
-    #EMF corrected Jet Collection
-    emfJetCorrectionInputTag = cms.InputTag("L2L3L4CorJet"),
-    #MC corrected Jet Collection
-    mcJetCorrectionInputTag = cms.InputTag("L2L3CorJet"),
-    # jet collection
-    jetsInputTag = cms.InputTag("selectedLayer1Jets"),
-    # generator jets collection (for RECO)
-    #genJetsInputTag = cms.InputTag("sisCone5GenJets"), 
-    # generator jets collection (for AOD)
-    #genJetsInputTag = cms.InputTag("sisCone5GenJets"),
-    # MC particles
-    #genParticlesInputTag = cms.InputTag("genParticles")
+           uncorJetsInputTag = cms.InputTag("sisCone5CaloJets"),
+           L2L3corJetsInputTag = cms.InputTag("L2L3CorJet"),
+           L2L3L4corJetsInputTag = cms.InputTag("L2L3L4CorJet")
 )
 
 
