a61ce18f6203c2016480946ca3e38299a7b582b0
kalavase <kalavase>
2009-05-17 22:52:37 +0000

    changed input collection from pat muons to reco Muons

--- python/muonMaker_cfi.py
+++ python/muonMaker_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 muonMaker = cms.EDFilter("MuonMaker",
     # muon collection
-    muonsInputTag = cms.InputTag("selectedLayer1Muons"),
+    muonsInputTag = cms.InputTag("muons"),                         
     #track collection
     tracksInputTag = cms.InputTag("generalTracks"),
     #beamSpot (from CMS2)
