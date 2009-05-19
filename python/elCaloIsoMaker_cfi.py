11281750068fc0fbd6bb0ca346bef0f7e9ac0eb6
warren <warren>
2009-05-19 18:55:05 +0000

    Changed collection from layer1 to uniqueElectrons to remove PAT

--- python/elCaloIsoMaker_cfi.py
+++ python/elCaloIsoMaker_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 elCaloIsoMaker = cms.EDProducer("ElCaloIsoMaker",
-  electronsInputTag = cms.InputTag("selectedLayer1Electrons"),
+  electronsInputTag = cms.InputTag("uniqueElectrons"),
   basicClusterInputTag = cms.InputTag("egammaBasicClusterMerger"),
   caloTowersInputTag = cms.InputTag("towerMaker"),
   minDR = cms.double(0.05),
