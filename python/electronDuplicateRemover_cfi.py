d90d52c0d804fa7bee7384910285d7554f28e8d9
kalavase <kalavase>
2009-05-19 06:08:11 +0000

    Removed stray comma

--- python/electronDuplicateRemover_cfi.py
+++ python/electronDuplicateRemover_cfi.py
@@ -1,9 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 uniqueElectrons = cms.EDFilter("ElectronDuplicateRemover",
-
     # Electron collection
-    electronsInputTag = cms.InputTag("pixelMatchGsfElectrons"),
-
+    electronsInputTag = cms.InputTag("pixelMatchGsfElectrons")
 )
 
