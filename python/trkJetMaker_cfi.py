92d967a59553fc4b5d061ccaf25689cbe21c626c
fgolf <fgolf>
2009-05-19 18:10:40 +0000

    removed one module - only produce one track jets collection now

--- python/trkJetMaker_cfi.py
+++ python/trkJetMaker_cfi.py
@@ -3,40 +3,20 @@ import FWCore.ParameterSet.Config as cms
 from RecoJets.JetProducers.GenJetParameters_cfi import *
 from RecoJets.JetProducers.SISConeJetParameters_cfi import *
 
-SISCone5TrkJets =  cms.EDProducer("SISConeJetProducer",
-       SISConeJetParameters,
-       alias = cms.untracked.string('SISCone05TrackJets'),
-       inputEtMin = cms.double(0.5),
-       inputEMin = cms.double(0.),
-       src = cms.InputTag('subTrkColl'),
-       jetType = cms.untracked.string("BasicJet"),
-       jetPtMin = cms.double(0.),
-       UE_Subtraction = cms.string('no'),
-       coneRadius = cms.double(0.5)
+SISCone5TrkJets =  cms.EDProducer("SISConeJetProducer"                                       ,
+                                  SISConeJetParameters                                       ,
+                                  alias          = cms.untracked.string('SISCone05TrackJets'),
+                                  inputEtMin     = cms.double(0.5)                           ,
+                                  inputEMin      = cms.double(0.)                            ,
+                                  src            = cms.InputTag('subTrkColl')                ,
+                                  jetType        = cms.untracked.string("BasicJet")          ,
+                                  jetPtMin       = cms.double(0.)                            ,
+                                  UE_Subtraction = cms.string('no')                          ,
+                                  coneRadius     = cms.double(0.5)
 )
-
-
-SISCone5AllTrkJets =  cms.EDProducer("SISConeJetProducer",
-       SISConeJetParameters,
-       alias = cms.untracked.string('SISCone05AllTrackJets'),
-       inputEtMin = cms.double(0.5),
-       inputEMin = cms.double(0.),
-       src = cms.InputTag('trkColl'),
-       jetType = cms.untracked.string("BasicJet"),
-       jetPtMin = cms.double(0.),
-       UE_Subtraction = cms.string('no'),
-       coneRadius = cms.double(0.5)
-)
-
-
 
 trkJetMaker = cms.EDProducer("TrkJetMaker", 
-  trkJetsInputTag = cms.InputTag('SISCone5TrkJets')
+                             trkJetsInputTag = cms.InputTag('SISCone5TrkJets')
 )
 
-alltrkJetMaker = cms.EDProducer("AllTrkJetMaker",
-  trkJetsInputTag = cms.InputTag('SISCone5AllTrkJets')
-)
-
-
-trkjetmaker = cms.Sequence(SISCone5TrkJets+SISCone5AllTrkJets+trkJetMaker+alltrkJetMaker)
+trkjetmaker = cms.Sequence(SISCone5TrkJets * trkJetMaker)
