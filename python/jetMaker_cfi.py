08fa013fc00b4fca88e01c7ed4ae583d21fd4a2c
kalavase <kalavase>
2009-05-20 03:33:08 +0000

    changed input jet collection to the one that JetCollectionPruner outputs

--- python/jetMaker_cfi.py
+++ python/jetMaker_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 jetMaker = cms.EDFilter("JetMaker",
-           uncorJetsInputTag = cms.InputTag("sisCone5CaloJets"),
+           uncorJetsInputTag = cms.InputTag("prunedUncorrectedCMS2Jets"),
            L2L3corJetsInputTag = cms.InputTag("L2L3CorJet"),
            L2L3L4corJetsInputTag = cms.InputTag("L2L3L4CorJet")
 )
