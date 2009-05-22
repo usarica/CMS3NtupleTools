b067e9ffa124237f130c0486763484a407de3eff
kalavase <kalavase>
2009-05-22 02:14:53 +0000

    removed jetmaker from jet sequence so that we can run the reco seperate from the Makers

--- python/jetSequence_cff.py
+++ python/jetSequence_cff.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-from CMS2.NtupleMaker.jetMaker_cfi import *
 from CMS2.NtupleMaker.jetCollectionPruner_cfi import *
 
-cms2CaloJetSequence = cms.Sequence(prunedUncorrectedCMS2Jets*jetMaker)
+cms2CaloJetSequence = cms.Sequence(prunedUncorrectedCMS2Jets)
