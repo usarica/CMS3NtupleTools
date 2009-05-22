77b4c14fc251d2ece426fc7512543f0159b8a304
kalavase <kalavase>
2009-05-22 02:18:52 +0000

    moved metMaker out of MET sequence

--- python/metSequence_cff.py
+++ python/metSequence_cff.py
@@ -6,9 +6,7 @@ from JetMETCorrections.Type1MET.MuonMETValueMapProducer_cff import *
 from JetMETCorrections.Type1MET.MuonTCMETValueMapProducer_cff import *
 from JetMETCorrections.Configuration.L2L3Corrections_Summer08_cff import *
 from JetMETCorrections.Type1MET.MetType1Corrections_cff import corMetType1Icone5
-from CMS2.NtupleMaker.metMaker_cfi import *
 from RecoMET.METProducers.TCMET_cfi import *
-from CMS2.NtupleMaker.tcmetMaker_cfi import *
 
 metMuonJESCorSC5CMS2 = corMetType1Icone5.clone()
 metMuonJESCorSC5CMS2.inputUncorJetsLabel = "prunedUncorrectedCMS2Jets"
@@ -16,5 +14,5 @@ metMuonJESCorSC5CMS2.corrector = "L2L3JetCorrectorSC5Calo"
 metMuonJESCorSC5CMS2.inputUncorMetLabel = "corMetGlobalMuons"
 
 
-metCorSequence = cms.Sequence(muonMETValueMapProducer*corMetGlobalMuons*metMuonJESCorSC5CMS2*metMaker*muonTCMETValueMapProducer*tcMet*tcmetMaker)
+metCorSequence = cms.Sequence(muonMETValueMapProducer*corMetGlobalMuons*metMuonJESCorSC5CMS2*muonTCMETValueMapProducer*tcMet)
 
