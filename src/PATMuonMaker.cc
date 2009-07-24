0dd7b6ffacdd079586a638fe86f6ea2afeda48d9
warren <warren>
2009-07-24 09:57:23 +0000

    Added one new branch: mus_pat_trckvetoDep for the pt of tracks in the muon isolation veto cone. (The ecal and hcal veto deps were already there, but this one was missing.)

--- src/PATMuonMaker.cc
+++ src/PATMuonMaker.cc
@@ -14,7 +14,7 @@ Description: copy additional PAT muon variables in simple data structures into t
 //
 // Original Author:  Frank Golf
 // Thu Jun 25 16:39:55 UTC 2008
-// $Id: PATMuonMaker.cc,v 1.8 2009/05/24 19:36:51 kalavase Exp $
+// $Id: PATMuonMaker.cc,v 1.9 2009/07/24 09:57:23 warren Exp $
 //
 //
 
@@ -60,6 +60,7 @@ PATMuonMaker::PATMuonMaker(const edm::ParameterSet& iConfig) {
   produces<vector<float>          >   ("muspatecalIso"     ).setBranchAlias("mus_pat_ecalIso"     );
   produces<vector<float>          >   ("muspathcalIso"     ).setBranchAlias("mus_pat_hcalIso"     );
   //deposit in the veto cone - gotten from the IsoDeposits
+  produces<vector<float>          >   ("muspattrckvetoDep" ).setBranchAlias("mus_pat_trckvetoDep" );
   produces<vector<float>          >   ("muspatvetoDep"     ).setBranchAlias("mus_pat_vetoDep"     );
   produces<vector<float>          >   ("muspatecalvetoDep" ).setBranchAlias("mus_pat_ecalvetoDep" );
   produces<vector<float>          >   ("muspathcalvetoDep" ).setBranchAlias("mus_pat_hcalvetoDep" );
@@ -97,9 +98,10 @@ void PATMuonMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   auto_ptr<vector<float>             >   mus_pat_caloIso     ( new vector<float>             );
   auto_ptr<vector<float>             >   mus_pat_ecalIso     ( new vector<float>             );
   auto_ptr<vector<float>             >   mus_pat_hcalIso     ( new vector<float>             );
-  auto_ptr<vector<float>             >   mus_pat_vetoDep     ( new vector<float>             );
-  auto_ptr<vector<float>             >   mus_pat_ecalvetoDep ( new vector<float>             );
-  auto_ptr<vector<float>             >   mus_pat_hcalvetoDep ( new vector<float>             );
+  auto_ptr<vector<float>             >   mus_pat_trckvetoDep ( new vector<float>             ); //energy in track veto cone
+  auto_ptr<vector<float>             >   mus_pat_vetoDep     ( new vector<float>             ); //sum of ecal+hcal veto cones
+  auto_ptr<vector<float>             >   mus_pat_ecalvetoDep ( new vector<float>             ); //energy in ecal veto cone
+  auto_ptr<vector<float>             >   mus_pat_hcalvetoDep ( new vector<float>             ); //energy in hcal veto cone
   auto_ptr<vector<int>               >   mus_pat_genID       ( new vector<int>               );
   auto_ptr<vector<int>               >   mus_pat_genMotherID ( new vector<int>               ); 
   auto_ptr<vector<uint32_t>          >   mus_pat_flag        ( new vector<uint32_t>          ); 
@@ -117,13 +119,15 @@ void PATMuonMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
 
     //the veto cone size is 0.07 in the ecal and 0.1 in the hcal 
+	const reco::IsoDeposit *trckIsoDep = patmu_it->trackerIsoDeposit();
     const reco::IsoDeposit *ecalIsoDep = patmu_it->ecalIsoDeposit();
     const reco::IsoDeposit *hcalIsoDep = patmu_it->hcalIsoDeposit();
     
     mus_pat_p4          ->push_back( patmu_it->p4()             );
     mus_pat_trackIso    ->push_back( patmu_it->trackIso()       );
+    mus_pat_trckvetoDep ->push_back( trckIsoDep->candEnergy()   );
     mus_pat_vetoDep     ->push_back( ecalIsoDep->candEnergy()
-				     + hcalIsoDep->candEnergy() );
+									 + hcalIsoDep->candEnergy() );
     mus_pat_ecalvetoDep ->push_back( ecalIsoDep->candEnergy()   );
     mus_pat_hcalvetoDep ->push_back( hcalIsoDep->candEnergy()   );
     mus_pat_ecalIso     ->push_back( patmu_it->ecalIso()        );
@@ -142,6 +146,7 @@ void PATMuonMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   // put containers into event
   iEvent.put(mus_pat_p4,           "muspatp4"              );
   iEvent.put(mus_pat_trackIso,     "muspattrackIso"        );
+  iEvent.put(mus_pat_trckvetoDep,  "muspattrckvetoDep"     );
   iEvent.put(mus_pat_vetoDep,      "muspatvetoDep"         );
   iEvent.put(mus_pat_ecalvetoDep,  "muspatecalvetoDep"     );
   iEvent.put(mus_pat_hcalvetoDep,  "muspathcalvetoDep"     );
