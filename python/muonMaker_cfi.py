e60ef9bdddd2cdcc3a481e7e958f5b8ff3e52feb
kalavase <kalavase>
2009-08-29 10:11:06 +0000

    Removed some unnecessary input tags

--- python/muonMaker_cfi.py
+++ python/muonMaker_cfi.py
@@ -3,14 +3,8 @@ import FWCore.ParameterSet.Config as cms
 muonMaker = cms.EDFilter("MuonMaker",
     # muon collection
     muonsInputTag = cms.InputTag("muons"),                         
-    #track collection
-    tracksInputTag = cms.InputTag("generalTracks"),
     #beamSpot (from CMS2)
-    beamSpotInputTag = cms.InputTag("beamSpotMaker"),
-    #isoDeposits tags						 
-    isoDepTrackInputTag = cms.InputTag("muIsoDepositTk"),
-	isoDepEcalInputTag  = cms.InputTag("muIsoDepositCalByAssociatorTowers","ecal"),
-	isoDepHcalInputTag  = cms.InputTag("muIsoDepositCalByAssociatorTowers","hcal"),
+    beamSpotInputTag = cms.InputTag("beamSpotMaker")
 )
 
 
