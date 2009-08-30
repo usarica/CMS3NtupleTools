7b2e63294db7565d1ceea5c2166a85c25f7412d0
kalavase <kalavase>
2009-08-30 16:37:52 +0000

    updated list of b tag algos, added a prefix

--- python/bTagMaker_cfi.py
+++ python/bTagMaker_cfi.py
@@ -2,36 +2,20 @@ import FWCore.ParameterSet.Config as cms
 
 bTagMaker = cms.EDFilter(
 	"BTagMaker",
-        AlgoSuffix                         = cms.string("Calo"),
+        AliasPrefix                        = cms.string("jets"),
 	uncorRecoJetsTag                   = cms.InputTag("prunedUncorrectedCMS2Jets"),
 	combinedSecondaryVertexBJetTags    = cms.InputTag("CMS2CombinedSecondaryVertexBJetTags"),
 	combinedSecondaryVertexMVABJetTags = cms.InputTag("CMS2CombinedSecondaryVertexMVABJetTags"),
-	impactParameterMVABJetTags         = cms.InputTag("CMS2ImpactParameterMVABJetTags"),
-	jetBProbabilityBJetTags            = cms.InputTag("CMS2JetBProbabilityBJetTags"),
+        jetBProbabilityBJetTags            = cms.InputTag("CMS2JetBProbabilityBJetTags"),
 	jetProbabilityBJetTags             = cms.InputTag("CMS2JetProbabilityBJetTags"),
 	simpleSecondaryVertexBJetTags      = cms.InputTag("CMS2SimpleSecondaryVertexBJetTags"),
-	softElectronBJetTags               = cms.InputTag("CMS2SoftElectronBJetTags"),
+        softElectronByIP3dBJetTags         = cms.InputTag("CMS2SoftElectronByIP3dBJetTags"),
+        softElectronByPtBJetTags           = cms.InputTag("CMS2SoftElectronByPtBJetTags"),
 	softMuonBJetTags                   = cms.InputTag("CMS2SoftMuonBJetTags"),
+        softMuonByIP3dBJetTags             = cms.InputTag("CMS2SoftMuonByIP3dBJetTags"),
+        softMuonByPtBJetTags               = cms.InputTag("CMS2SoftMuonByPtBJetTags"),
 	softMuonNoIPBJetTags               = cms.InputTag("CMS2SoftMuonNoIPBJetTags"),
 	trackCountingHighEffBJetTags       = cms.InputTag("CMS2TrackCountingHighEffBJetTags"),
-	trackCountingHighPurBJetTags       = cms.InputTag("CMS2TrackCountingHighPurBJetTags"),
-)
-
-
-bTagTrkMaker = cms.EDFilter(
-        "BTagMaker",
-        AlgoSuffix                         = cms.string("Trk"),
-        uncorRecoJetsTag                   = cms.InputTag("SISCone5TrkJets"),
-        combinedSecondaryVertexBJetTags    = cms.InputTag("CMS2TrkCombinedSecondaryVertexBJetTags"),
-        combinedSecondaryVertexMVABJetTags = cms.InputTag("CMS2TrkCombinedSecondaryVertexMVABJetTags"),
-        impactParameterMVABJetTags         = cms.InputTag("CMS2TrkImpactParameterMVABJetTags"),
-        jetBProbabilityBJetTags            = cms.InputTag("CMS2TrkJetBProbabilityBJetTags"),
-        jetProbabilityBJetTags             = cms.InputTag("CMS2TrkJetProbabilityBJetTags"),
-        simpleSecondaryVertexBJetTags      = cms.InputTag("CMS2TrkSimpleSecondaryVertexBJetTags"),
-        softElectronBJetTags               = cms.InputTag("CMS2TrkSoftElectronBJetTags"),
-        softMuonBJetTags                   = cms.InputTag("CMS2TrkSoftMuonBJetTags"),
-        softMuonNoIPBJetTags               = cms.InputTag("CMS2TrkSoftMuonNoIPBJetTags"),
-        trackCountingHighEffBJetTags       = cms.InputTag("CMS2TrkTrackCountingHighEffBJetTags"),
-        trackCountingHighPurBJetTags       = cms.InputTag("CMS2TrkTrackCountingHighPurBJetTags"),
+	trackCountingHighPurBJetTags       = cms.InputTag("CMS2TrackCountingHighPurBJetTags")
 )
 
