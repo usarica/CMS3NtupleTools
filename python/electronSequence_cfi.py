13c4dc0ef0a089251edea4a48abaee57b8aad44e
kalavase <kalavase>
2009-08-31 13:09:46 +0000

    Removed the electron isolation sequence as we don't run it anymore.

--- python/electronSequence_cfi.py
+++ python/electronSequence_cfi.py
@@ -5,75 +5,6 @@ from CMS2.NtupleMaker.electronDuplicateRemover_cfi import *
 from RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi import *
 
 ###
-### Isolation
-###
-
-#from RecoEgamma.EgammaIsolationAlgos.eleTrackExtractorBlocks_cff import *
-#
-#eleIsoDepositTkCMS2 = cms.EDProducer("CandIsoDepositProducer",
-#    src = cms.InputTag("gsfElectrons"),
-#    trackType = cms.string('candidate'),
-#    MultipleDepositsFlag = cms.bool(False),
-#    ExtractorPSet = cms.PSet(EleIsoTrackExtractorBlock)
-#)
-#
-#from RecoEgamma.EgammaIsolationAlgos.eleEcalExtractorBlocks_cff import *
-#
-#eleIsoDepositEcalFromHitsCMS2 = cms.EDProducer("CandIsoDepositProducer",
-#    src = cms.InputTag("gsfElectrons"),
-#    trackType = cms.string('candidate'),
-#    MultipleDepositsFlag = cms.bool(False),
-#    ExtractorPSet = cms.PSet(EleIsoEcalFromHitsExtractorBlock)
-#)
-
-
-##in 21X, there doesn't seem to be an extractor from towers
-#from RecoEgamma.EgammaIsolationAlgos.eleHcalExtractorBlocks_cff import *
-#eleIsoDepositHcalFromTowersCMS2 = cms.EDProducer("CandIsoDepositProducer",
-#    src = cms.InputTag("gsfElectrons"),
-#    trackType = cms.string('candidate'),
-#    MultipleDepositsFlag = cms.bool(False),
-#    ExtractorPSet = cms.PSet(EleIsoHcalFromTowersExtractorBlock)
-#)
-
-#ValueMap Producers
-#eleIsoFromDepsTkCMS2 = cms.EDFilter("CandIsolatorFromDeposits",
-#    deposits = cms.VPSet(cms.PSet(
-#        mode = cms.string('sum'),
-#        src = cms.InputTag("eleIsoDepositTkCMS2"),
-#        weight = cms.string('1'),
-#        deltaR = cms.double(0.3),
-#        vetos = cms.vstring('0.015','Threshold(1.0)'),
-#        skipDefaultVeto = cms.bool(True)
-#    ))
-#)
-#eleIsoFromDepsEcalFromHitsCMS2 = cms.EDFilter("CandIsolatorFromDeposits",
-#    deposits = cms.VPSet(cms.PSet(
-#        mode = cms.string('sum'),
-#        src = cms.InputTag("eleIsoDepositEcalFromHitsCMS2"),
-#        weight = cms.string('1'),
-#        deltaR = cms.double(0.4),
-#        vetos = cms.vstring('EcalBarrel:0.045', 
-#                            'EcalBarrel:RectangularEtaPhiVeto(-0.02,0.02,-0.5,0.5)',
-#                            'EcalBarrel:ThresholdFromTransverse(0.08)',
-#                            'EcalEndcaps:ThresholdFromTransverse(0.3)',
-#                            'EcalEndcaps:0.070', 
-#                            'EcalEndcaps:RectangularEtaPhiVeto(-0.02,0.02,-0.5,0.5)'), 
-#        skipDefaultVeto = cms.bool(True)
-#    ))
-#)
-#eleIsoFromDepsHcalFromTowersCMS2 = cms.EDFilter("CandIsolatorFromDeposits",
-#    deposits = cms.VPSet(cms.PSet(
-#        src = cms.InputTag("eleIsoDepositHcalFromTowersCMS2"),
-#        deltaR = cms.double(0.4),
-#        weight = cms.string('1'),
-#        vetos = cms.vstring('0.0'),
-#        skipDefaultVeto = cms.bool(True),
-#        mode = cms.string('sum')
-#    ))
-#)
-
-###
 ### Electron ID
 ###
 
@@ -107,15 +38,7 @@ eidTightCMS2.electronQuality = 'tight'
 ### Sequences
 ###
 
-### Isolation sequences
-#eleIsoDepositsCMS2 = cms.Sequence(eleIsoDepositTkCMS2+eleIsoDepositEcalFromHitsCMS2+eleIsoDepositHcalFromTowersCMS2)
-#eleIsoFromDepositsCMS2 = cms.Sequence(eleIsoFromDepsTkCMS2*eleIsoFromDepsEcalFromHitsCMS2*eleIsoFromDepsHcalFromTowersCMS2)
-#egammaIsolationSequenceCMS2 = cms.Sequence(eleIsoDepositsCMS2*eleIsoFromDepositsCMS2)
-
 ### Electron ID sequences
 egammaElectronIDCMS2 = cms.Sequence(eidRobustLooseCMS2+eidRobustTightCMS2+eidRobustHighEnergyCMS2+eidLooseCMS2+eidTightCMS2)
 
-### Master sequence
-#electronSequence = cms.Sequence(gsfElectrons*egammaIsolationSequenceCMS2*egammaElectronIDCMS2)
-electronSequence = cms.Sequence(egammaElectronIDCMS2)
 
