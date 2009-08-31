793b23a151df95193d85ddd16f815cbe66d5d83f
kalavase <kalavase>
2009-08-31 11:49:50 +0000

    Removed option to match by dR if hits are not available. We now get the reference to the ctf track from the electron object itself as this information is available in 3X

--- src/ElToTrackAssMaker.cc
+++ src/ElToTrackAssMaker.cc
@@ -13,7 +13,7 @@ Implementation:
 //
 // Original Author:  pts/4
 //         Created:  Fri Jun  6 11:07:38 CDT 2008
-// $Id: ElToTrackAssMaker.cc,v 1.8 2009/05/20 01:55:17 warren Exp $
+// $Id: ElToTrackAssMaker.cc,v 1.9 2009/08/31 11:49:34 kalavase Exp $
 //
 //
 
@@ -43,22 +43,12 @@ using namespace std;
 using namespace reco;
 using namespace edm;
 
-/*
-  Association between electron and ctf track is done by shared hits in the pixels
-  and the inner strip tracker if we have the hits
-  If we're running on AOD, the association is done by deltaR matching
-  If running on AOD, the branch "els_trkshFrac" will contain 999   
-*/
-
 ElToTrackAssMaker::ElToTrackAssMaker(const ParameterSet& iConfig)
 {
   produces<vector<int>   >("elstrkidx"     ).setBranchAlias("els_trkidx");// track index matched to electron
   produces<vector<float> >("elstrkshFrac"  ).setBranchAlias("els_trkshFrac");
   produces<vector<float> >("elstrkdr"      ).setBranchAlias("els_trkdr" );
-
      
-  minDr_             = iConfig.getParameter<double>("minDR");
-  haveHits_          = iConfig.getParameter<bool>("haveHits");
   electronsInputTag_ = iConfig.getParameter<InputTag>("electronsInputTag");
   tracksInputTag_    = iConfig.getParameter<InputTag>("tracksInputTag");
 }
@@ -71,82 +61,34 @@ void ElToTrackAssMaker::produce(Event& iEvent, const EventSetup& iSetup)
   auto_ptr<vector<float>   > els_trkshFrac (new vector<float>   );
   auto_ptr<vector<float>   > els_trkdr     (new vector<float>   );
 
-  //get the reco electron collection 
-  //vector<const GsfElectron*> els_coll = ElUtilities::getElectrons(iEvent, electronsInputTag_);
-  //ElUtilities::removeElectrons(&els_coll);
-     
   Handle<View<reco::GsfElectron> > els_h;
   iEvent.getByLabel(electronsInputTag_, els_h);
   
-  //get the reco track collection
-  Handle<TrackCollection> trks_h;
-  iEvent.getByLabel(tracksInputTag_, trks_h);
-  const TrackCollection *trks_coll = trks_h.product();
-
   // get electron p4's produced by CMS2
   Handle<vector<LorentzVector> > els_p4_h;
   iEvent.getByLabel("electronMaker", "elsp4", els_p4_h);  
 
-  // get track p4's produced by CMS2
-  Handle<vector<LorentzVector> > trks_p4_h;
-  iEvent.getByLabel("trackMaker", "trkstrkp4", trks_p4_h);
-          
-     
-  if(haveHits_) {
-    for(View<reco::GsfElectron>::const_iterator els_it = els_h->begin();
-	els_it != els_h->end(); els_it++) {
+  for(View<reco::GsfElectron>::const_iterator els_it = els_h->begin();
+      els_it != els_h->end(); els_it++) {
       
-      GsfTrackRef gsfTkRef = els_it->gsfTrack();
-      //if we don't find the the ctf partner,
-      //then the index will be -999
-      pair<int,float> elCtfPair = getCTFTrackIndex(gsfTkRef,*trks_coll);
-	 
-      els_trkidx         ->push_back(elCtfPair.first                             );
-      els_trkshFrac      ->push_back(elCtfPair.first>=0 ? elCtfPair.second : 999 );
-      double dR = 999;
-      if(elCtfPair.first >=0) 
-	dR = deltaR(gsfTkRef->eta(), gsfTkRef->phi(),
-		    trks_p4_h->at(elCtfPair.first).eta(),
-		    trks_p4_h->at(elCtfPair.first).phi());
-      els_trkdr          ->push_back(dR                                          );
-
-
+    TrackRef ctfTkRef = els_it->closestCtfTrackRef();
+    GsfTrackRef gsfTkRef = els_it->gsfTrack();
+    
+    double dR = -999;
+    if(ctfTkRef.isNonnull() ) {
+      els_trkidx       ->push_back(static_cast<int>(ctfTkRef.key())            );
+      els_trkshFrac    ->push_back(static_cast<int>(els_it->shFracInnerHits()) );
+      dR = deltaR(gsfTkRef->eta(), gsfTkRef->phi(),
+		  ctfTkRef->eta(), ctfTkRef->phi()                             );
+    } else {
+      els_trkidx       ->push_back(-999                                        );
+      els_trkshFrac    ->push_back(-999.                                       );
+    }
+    
+    els_trkdr          ->push_back(dR                                          );
 
     }
-  } else { //only if we are in AOD!!!!!
-    for(vector<LorentzVector>::const_iterator els_it = els_p4_h->begin(),
-	  els_end = els_p4_h->end();
-	els_it != els_end; els_it++) {
-	 
-      double el_eta = els_it->Eta();
-      double el_phi = els_it->Phi();
-	 
-      double minDR   = minDr_;
-      unsigned int i = 0;
-      int index      = -999;
-
-      // loop over tracks
-      for(vector<LorentzVector>::const_iterator trk_it = trks_p4_h->begin(),
-	    trk_end = trks_p4_h->end();
-	  trk_it != trk_end; ++trk_it, ++i) {
-	 
-	double trk_eta = trk_it->Eta();
-	double trk_phi = trk_it->Phi();
-	double dR = deltaR(el_eta, el_phi, trk_eta, trk_phi);
-	if(dR > minDR) continue;
-
-	if(dR < minDR) {
-	  minDR = dR;
-	  index = i;
-	}
-      }
-
-      // fill vector
-      els_trkidx     ->push_back(index);
-      els_trkdr      ->push_back(index >=0 ? minDR : 999);
-      els_trkshFrac  ->push_back(999.);
-    }
-  }
+  
   // store vectors
   iEvent.put(els_trkidx,      "elstrkidx"     );
   iEvent.put(els_trkdr ,      "elstrkdr"      );
@@ -163,84 +105,6 @@ ElToTrackAssMaker::beginJob(const EventSetup&)
 void 
 ElToTrackAssMaker::endJob() {
 }
-
-//-------------------------------------------------------------------------
-//Get the CTF track corresponding to the gsf using shared hits
-//-------------------------------------------------------------------------
-pair<int, float> ElToTrackAssMaker::getCTFTrackIndex(const GsfTrackRef& gsfTrackRef, 
-						     const TrackCollection& ctfTrackCollection) {
-  
-  
-  float maxFracShared =  0;
-  unsigned int counter = 0;
-  int elTkIndex = -999;
-  TrackRef ctfTrackRef = TrackRef();
-  
-
-  //get the Hit Pattern for the gsfTrack
-  const HitPattern& gsfHitPattern = gsfTrackRef->hitPattern();
-
-
-  for(TrackCollection::const_iterator ctfTkIter = ctfTrackCollection.begin();
-      ctfTkIter != ctfTrackCollection.end(); ctfTkIter++, counter++) {
-    
-    double dEta = gsfTrackRef->eta() - ctfTkIter->eta();
-    double dPhi = gsfTrackRef->phi() - ctfTkIter->phi();
-    double pi = acos(-1.);
-    if(fabs(dPhi) > pi) dPhi = 2*pi - fabs(dPhi);
-
-    //dont want to look at every single track in the event!
-    if(sqrt(dEta*dEta + dPhi*dPhi) > 0.3) continue;
-    
-    unsigned int shared = 0;
-    int gsfHitCounter = 0;
-    int numGsfInnerHits = 0;
-    int numCtfInnerHits = 0;
-    //get the CTF Track Hit Pattern
-    const HitPattern& ctfHitPattern = ctfTkIter->hitPattern();
-
-    for(trackingRecHit_iterator elHitsIt = gsfTrackRef->recHitsBegin();
-        elHitsIt != gsfTrackRef->recHitsEnd(); elHitsIt++, gsfHitCounter++) {
-      if(!((**elHitsIt).isValid()))  //count only valid Hits
-        continue;
-      
-      //look only in the pixels/TIB/TID
-      uint32_t gsfHit = gsfHitPattern.getHitPattern(gsfHitCounter);
-      if(!(gsfHitPattern.pixelHitFilter(gsfHit) ||
-           gsfHitPattern.stripTIBHitFilter(gsfHit) ||
-           gsfHitPattern.stripTIDHitFilter(gsfHit) ) ) continue;
-      numGsfInnerHits++;
-      int ctfHitsCounter = 0;
-      numCtfInnerHits = 0;
-      for(trackingRecHit_iterator ctfHitsIt = ctfTkIter->recHitsBegin();
-          ctfHitsIt != ctfTkIter->recHitsEnd(); ctfHitsIt++, ctfHitsCounter++) {
-        if(!((**ctfHitsIt).isValid())) //count only valid Hits!
-          continue;
-	uint32_t ctfHit = ctfHitPattern.getHitPattern(ctfHitsCounter);
-        if( !(ctfHitPattern.pixelHitFilter(ctfHit) ||
-              ctfHitPattern.stripTIBHitFilter(ctfHit) ||
-              ctfHitPattern.stripTIDHitFilter(ctfHit) ) ) continue;
-        numCtfInnerHits++;
-        if( (**elHitsIt).sharesInput(&(**ctfHitsIt), TrackingRecHit::all) ) {
-          shared++;
-	  break;
-        }
-      }//ctfHits iterator
-      
-    }//gsfHits iterator
-    
-    if(static_cast<float>(shared)/min(numGsfInnerHits,numCtfInnerHits) > maxFracShared) {
-      maxFracShared = static_cast<float>(shared)/min(numGsfInnerHits, numCtfInnerHits);
-      elTkIndex = counter;      
-    }
-  
-  }//ctfTrack iterator
-  
-  return make_pair(elTkIndex,maxFracShared);
-
-}
-
-
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(ElToTrackAssMaker);
--- src/TrackToElAssMaker.cc
+++ src/TrackToElAssMaker.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  pts/4
 //         Created:  Fri Jun  6 11:07:38 CDT 2008
-// $Id: TrackToElAssMaker.cc,v 1.4 2009/05/20 01:05:56 warren Exp $
+// $Id: TrackToElAssMaker.cc,v 1.5 2009/08/31 11:49:50 kalavase Exp $
 //
 //
 
@@ -46,7 +46,6 @@ using namespace edm;
 
 
 TrackToElAssMaker::TrackToElAssMaker(const edm::ParameterSet& iConfig)
-     : m_minDR(iConfig.getParameter<double>("minDR"))
 {
 
   // index in electron collection of track matched to electron
@@ -54,8 +53,6 @@ TrackToElAssMaker::TrackToElAssMaker(const edm::ParameterSet& iConfig)
   produces<vector<float> >("trkselsdr"      ).setBranchAlias("trks_elsdr"     );
   produces<vector<float> >("trkselsshFrac"  ).setBranchAlias("trks_elsshFrac" );
   
-  m_minDR = iConfig.getParameter<double>("minDR");
-  haveHits_          = iConfig.getParameter<bool>("haveHits");
   electronsInputTag_ = iConfig.getParameter<InputTag>("electronsInputTag");
   tracksInputTag_    = iConfig.getParameter<InputTag>("tracksInputTag");
      
@@ -80,58 +77,24 @@ void TrackToElAssMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetu
   iEvent.getByLabel(tracksInputTag_, trks_h);
   const TrackCollection *trks_coll = trks_h.product();
   
-  // get electron p4's
-  Handle<vector<LorentzVector> > els_trk_p4_h;
-  iEvent.getByLabel("electronMaker", "elstrkp4", els_trk_p4_h);  
-
   // get track p4's
   Handle<vector<LorentzVector> > trks_p4_h;
   iEvent.getByLabel("trackMaker", "trkstrkp4", trks_p4_h);
 
-  if(haveHits_) {
-    for(vector<Track>::const_iterator trks_it = trks_coll->begin();
+  for(vector<Track>::const_iterator trks_it = trks_coll->begin();
 	trks_it != trks_coll->end(); trks_it++) {
-      
-      pair<int,float> elCTFPair = getElectronIndex(*trks_it, els_coll);
-      trks_elsidx          ->push_back(elCTFPair.first     );
-      trks_elsshFrac       ->push_back(elCTFPair.first > -5 ? elCTFPair.second : 999. );
-      double dR = 999;
-      if(elCTFPair.first > -5 )
-	dR = deltaR(trks_it->eta(), trks_it->phi(),
-		    els_trk_p4_h->at(elCTFPair.first).eta(),
-		    els_trk_p4_h->at(elCTFPair.first).phi() );
-      
-      trks_elsdr           ->push_back(dR                  );
-      
-    }
-  } else {
-    //loop over tracks and find the closest track
-    for(vector<LorentzVector>::const_iterator trks_it = trks_p4_h->begin();
-	trks_it != trks_p4_h->end(); trks_it++) {
-      
-      double minDR = m_minDR;
-      int i = 0;
-      int index = -999;
-      
-      for(vector<LorentzVector>::const_iterator els_it = els_trk_p4_h->begin();
-	  els_it != els_trk_p4_h->end(); els_it++, i++) {
-	
-	double dR = deltaR(trks_it->eta(), trks_it->phi(),
-			   els_it->eta(), els_it->phi());
-	
-	if(dR < minDR) {
-	  minDR = dR;
-	  index = i;
-	}
-      }
-      
-      // fill vector
-      trks_elsidx          ->push_back(index);
-      trks_elsshFrac       ->push_back(999.);
-      trks_elsdr           ->push_back(minDR);
-    }
+    
+    int elidx    = -999 ;
+    float shFrac = -999.;
+    float dR     = -999.;
+    
+    getMatchedElInfo(*trks_it, els_coll, elidx, shFrac, dR);
+    trks_elsidx          ->push_back(elidx   );
+    trks_elsdr           ->push_back(dR      );
+    trks_elsshFrac       ->push_back(shFrac  );
+        
   }
-
+  
   // store vectors
   iEvent.put(trks_elsidx,        "trkselsidx"    );
   iEvent.put(trks_elsdr,         "trkselsdr"     );
@@ -143,79 +106,36 @@ void TrackToElAssMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetu
 // Returns the index of the track that is closest to the electron
 //-------------------------------------------------------------------------
 
-pair<int, float> TrackToElAssMaker::getElectronIndex(const Track& ctfTk,
-                                                     std::vector<const GsfElectron*> gsfTrackCollection) {
+void TrackToElAssMaker::getMatchedElInfo(const Track& ctfTk,
+						    std::vector<const GsfElectron*> gsfElectrons,
+						    int& idx, float& shFrac, float& dR) {
 
-  float maxFracShared  =  0;
-  unsigned int counter = 0;
-  int elIndex = -999;
+  double mindR = 0.1;
   
-  //get the Hit Pattern for the recoTrack
-  const HitPattern& ctfHitPattern = ctfTk.hitPattern();
-  
-  //loop over the electrons
-  for(std::vector<const GsfElectron*>::const_iterator gsfIter = gsfTrackCollection.begin();
-      gsfIter != gsfTrackCollection.end(); gsfIter++, counter++) {
+  idx         = -999;
+  shFrac      = -999.;
+  dR          = -999.;
+  int counter = 0;
+  for(std::vector<const GsfElectron*>::const_iterator gsfIter = gsfElectrons.begin();
+      gsfIter != gsfElectrons.end(); gsfIter++, counter++) {
 
     const GsfElectron *el = *gsfIter;
-    GsfTrackRef gsfTrackRef = el->gsfTrack();
-    double dEta = ctfTk.eta() - gsfTrackRef->eta();
-    double dPhi = ctfTk.phi() - gsfTrackRef->phi();
-    double pi = acos(-1.);
-    if(fabs(dPhi) > pi) dPhi = 2*pi - fabs(dPhi);
-
-    //dont want to look at every single track in the event!
-    if(sqrt(dEta*dEta + dPhi*dPhi) > m_minDR) continue;
-
-    unsigned int shared = 0;
-    int gsfHitCounter   = 0;
-    int numGsfInnerHits = 0;
-    int numCtfInnerHits = 0;
-    //get the gsf Track Hit Pattern
-    const HitPattern& gsfHitPattern = gsfTrackRef->hitPattern();
-
-    //loop over the electron's gsfTrack's hits
-    for(trackingRecHit_iterator elHitsIt = gsfTrackRef->recHitsBegin();
-        elHitsIt != gsfTrackRef->recHitsEnd(); elHitsIt++, gsfHitCounter++) {
-      if(!((**elHitsIt).isValid()))  //count only valid Hits
-        continue;
-
-      //look only in the pixels/TIB/TID
-      uint32_t gsfHit = gsfHitPattern.getHitPattern(gsfHitCounter);
-      if(!(gsfHitPattern.pixelHitFilter(gsfHit) ||
-           gsfHitPattern.stripTIBHitFilter(gsfHit) ||
-           gsfHitPattern.stripTIDHitFilter(gsfHit) ) ) continue;
-      numGsfInnerHits++;
-
-      int ctfHitsCounter = 0;
-      numCtfInnerHits = 0;
-      for(trackingRecHit_iterator ctfHitsIt = ctfTk.recHitsBegin();
-          ctfHitsIt != ctfTk.recHitsEnd(); ctfHitsIt++, ctfHitsCounter++) {
-        if(!((**ctfHitsIt).isValid())) //count only valid Hits!
-          continue;
-
-        uint32_t ctfHit = ctfHitPattern.getHitPattern(ctfHitsCounter);
-        if( !(ctfHitPattern.pixelHitFilter(ctfHit) ||
-              ctfHitPattern.stripTIBHitFilter(ctfHit) ||
-              ctfHitPattern.stripTIDHitFilter(ctfHit) ) ) continue;
-        numCtfInnerHits++;
-        if( (**elHitsIt).sharesInput(&(**ctfHitsIt), TrackingRecHit::all) ) {
-          shared++;
-          break;
-        }
-      }//ctfHits iterator
-
-  }//gsfHits iterator
-
-    if(static_cast<float>(shared)/min(numGsfInnerHits,numCtfInnerHits) > maxFracShared) {
-      maxFracShared = static_cast<float>(shared)/min(numGsfInnerHits, numCtfInnerHits);
-      elIndex = counter;
+    TrackRef    ctfTrackRef = el->closestCtfTrackRef();
+    
+    if(!ctfTrackRef.isNonnull())
+      continue;
+    
+    double tempdR =  deltaR(ctfTk.eta(), ctfTk.phi(),
+			    ctfTrackRef->eta(), ctfTrackRef->phi() );
+    
+    if(tempdR < mindR ) {
+      mindR = tempdR;
+      idx    = counter;
+      shFrac = el->shFracInnerHits();
+      dR = tempdR;
     }
-
-  }//electron iterator
-
-  return make_pair(elIndex,maxFracShared);
-
+  }
+    return;
 }
 
 
