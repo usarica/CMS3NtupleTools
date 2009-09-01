0fcfab48f33690277793cdca0117354924503f57
dlevans <dlevans>
2009-09-01 11:43:42 +0000

    Have updated the electron sequence to use the most recent official egamma ID.  This ID has been optimised with the 2_2_X samples and is the most official that is currently available.

--- python/electronSequence_cfi.py
+++ python/electronSequence_cfi.py
@@ -1,38 +1,50 @@
-import FWCore.ParameterSet.Config as cms
 
-#from RecoEgamma.EgammaIsolationAlgos.egammaIsolationSequence_cff import *
-#from CMS2.NtupleMaker.electronDuplicateRemover_cfi import *
+import FWCore.ParameterSet.Config as cms
 from RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi import *
 
 ###
 ### Electron ID
 ###
 
+###
+### Here version V01 is used for all IDs.
+### This version was tuned in 2_2_X releases and represents 
+### the latest official Egamma electron ID.
+###
+
+### Robust loose
 import RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi
 eidRobustLooseCMS2 = RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi.eidCutBasedExt.clone()
-eidRobustLooseCMS2.src = "gsfElectrons"
+eidRobustLooseCMS2.electronIDType = 'robust'
+eidRobustLooseCMS2.electronQuality = 'loose'
+eidRobustLooseCMS2.electronVersion = 'V01'
 
+### Robust tight
 import RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi
 eidRobustTightCMS2 = RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi.eidCutBasedExt.clone()
-eidRobustTightCMS2.src = "gsfElectrons"
-#eidRobustTightCMS2.robustEleIDCuts.barrel = [0.015, 0.0092, 0.020, 0.0025]
-#eidRobustTightCMS2.robustEleIDCuts.endcap = [0.018, 0.025, 0.020, 0.0040]
+eidRobustTightCMS2.electronIDType = 'robust'
+eidRobustTightCMS2.electronQuality = 'tight'
+eidRobustTightCMS2.electronVersion = 'V01'
 
+### Robust high energy (HEEP)
 import RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi
 eidRobustHighEnergyCMS2 = RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi.eidCutBasedExt.clone()
-eidRobustHighEnergyCMS2.src = "gsfElectrons"
-#eidRobustHighEnergyCMS2.robustEleIDCuts.barrel = [0.050, 0.011, 0.090, 0.005]
-#eidRobustHighEnergyCMS2.robustEleIDCuts.endcap = [0.100, 0.0275, 0.090, 0.007]
+eidRobustHighEnergyCMS2.electronIDType = 'robust'
+eidRobustHighEnergyCMS2.electronQuality = 'highenergy'
+eidRobustHighEnergyCMS2.electronVersion = 'V01'
 
+### Loose catagorised (Sani)
 import RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi
 eidLooseCMS2 = RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi.eidCutBasedExt.clone()
-eidLooseCMS2.src = "gsfElectrons"
-eidLooseCMS2.electronQuality = 'loose'
+eidLooseCMS2.electronIDType 	= 'classbased'
+eidLooseCMS2.electronQuality 	= 'loose'
+eidLooseCMS2.electronVersion 	= 'V01'
 
 import RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi
 eidTightCMS2 = RecoEgamma.ElectronIdentification.electronIdCutBasedExt_cfi.eidCutBasedExt.clone()
-eidTightCMS2.src = "gsfElectrons"
-eidTightCMS2.electronQuality = 'tight'
+eidTightCMS2.electronIDType     = 'classbased'
+eidTightCMS2.electronQuality    = 'tight'
+eidTightCMS2.electronVersion   	= 'V01'
 
 ###
 ### Sequences
@@ -40,5 +52,4 @@ eidTightCMS2.electronQuality = 'tight'
 
 ### Electron ID sequences
 egammaElectronIDCMS2 = cms.Sequence(eidRobustLooseCMS2+eidRobustTightCMS2+eidRobustHighEnergyCMS2+eidLooseCMS2+eidTightCMS2)
-
 
