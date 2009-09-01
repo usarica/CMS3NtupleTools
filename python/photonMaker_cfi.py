a5e38e7a286459a77df4e781245ba0ee4472cf76
dlevans <dlevans>
2009-09-01 09:15:48 +0000

    remove nolonger needed isolation map input tags as isolation is now extracted from the photon object itself

--- python/photonMaker_cfi.py
+++ python/photonMaker_cfi.py
@@ -3,9 +3,5 @@ import FWCore.ParameterSet.Config as cms
 photonMaker = cms.EDFilter("PhotonMaker",
     # Photon collection
     photonsInputTag = cms.InputTag("photons"),
-    # Isolation
-    ecalIsoTag = cms.InputTag("gamIsoFromDepsEcalFromHitsCMS2"),
-    hcalIsoTag = cms.InputTag("gamIsoFromDepsHcalFromTowersCMS2"),
-    tkIsoTag = cms.InputTag("gamIsoFromDepsTkCMS2")
 )
 
