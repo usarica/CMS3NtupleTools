bfd810e1adeef3b0ae36905a757247f8d8e9759e
fgolf <fgolf>
2009-09-02 10:07:06 +0000

    removed uncorrectd caloJets which are no longer needed

--- python/jptMaker_cfi.py
+++ python/jptMaker_cfi.py
@@ -2,6 +2,5 @@ import FWCore.ParameterSet.Config as cms
 
 jptMaker = cms.EDFilter("JPTMaker",
                         jptInputTag       = cms.InputTag("JetPlusTrackZSPCorJetIcone5"),
-                        L2L3jptInputTag   = cms.InputTag("L2L3CorJetIC5JPT"             ),
-                        uncorJetsInputTag = cms.InputTag("prunedUncorrectedCMS2Jets"    )
+                        L2L3jptInputTag   = cms.InputTag("L2L3CorJetIC5JPT"           )
 )
