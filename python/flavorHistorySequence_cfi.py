820f239883389a86ab2837b63d9189fda7193f16
kalavase <kalavase>
2009-09-02 23:51:39 +0000

    Added flavorHistoryMaker to the sequence

--- python/flavorHistorySequence_cfi.py
+++ python/flavorHistorySequence_cfi.py
@@ -3,10 +3,12 @@ import FWCore.ParameterSet.Config as cms
 
 from PhysicsTools.HepMCCandAlgos.flavorHistoryProducer_cfi import *
 from PhysicsTools.HepMCCandAlgos.flavorHistoryFilter_cfi import *
+from CMS2.NtupleMaker.flavorHistoryMaker_cfi import *
+
 
 #needs the gen jets thats produced by the genJetMaker
 cFlavorHistoryProducer.matchedSrc = cms.InputTag("sisCone5StGenJets")
 bFlavorHistoryProducer.matchedSrc = cms.InputTag("sisCone5StGenJets")
 
 
-CMS2FlavorHistorySequence = cms.Sequence(bFlavorHistoryProducer * cFlavorHistoryProducer * flavorHistoryFilter)
+CMS2FlavorHistorySequence = cms.Sequence(bFlavorHistoryProducer * cFlavorHistoryProducer * flavorHistoryFilter * flavorHistoryMaker)
