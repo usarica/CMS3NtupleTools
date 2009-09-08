c525518af843fea6196d4dff7caf21c9e9eb7e54
kalavase <kalavase>
2009-09-08 10:49:34 +0000

    Commented out the EMF corrections as they're not available in the latest JEC

--- src/JetMaker.cc
+++ src/JetMaker.cc
@@ -14,7 +14,7 @@
 //
 // Original Author:  Oliver Gutsche
 // Created:  Tue Jun  9 11:07:38 CDT 2008
-// $Id: JetMaker.cc,v 1.18 2009/09/02 11:07:29 fgolf Exp $
+// $Id: JetMaker.cc,v 1.19 2009/09/08 10:49:34 kalavase Exp $
 //
 //
 
@@ -60,7 +60,7 @@ JetMaker::JetMaker(const edm::ParameterSet& iConfig)
   produces<std::vector<LorentzVector> > ("jetsvertexp4" ).setBranchAlias("jets_vertex_p4"   );
   produces<std::vector<float> >	        ("jetsemFrac"   ).setBranchAlias("jets_emFrac"      ); // electromagnetic energy fraction
   produces<std::vector<float> >	        ("jetscor"      ).setBranchAlias("jets_cor"         ); // energy scale correction -> only L2 and L3
-  produces<std::vector<float> >	        ("jetsEMFcor"   ).setBranchAlias("jets_EMFcor"      ); // energy scale corrections including electromagnetic fraction of jet
+  //produces<std::vector<float> >	        ("jetsEMFcor"   ).setBranchAlias("jets_EMFcor"      ); // energy scale corrections including electromagnetic fraction of jet
 
   // parameters from configuration
   uncorJetsInputTag_      = iConfig.getParameter<edm::InputTag>("uncorJetsInputTag"       );
@@ -82,7 +82,7 @@ void JetMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   auto_ptr<vector<LorentzVector> >   vector_jets_vertex_p4(new vector<LorentzVector> );
   auto_ptr<vector<float> >           vector_jets_emFrac   (new vector<float>         );
   auto_ptr<vector<float> >           vector_jets_cor      (new vector<float>         );
-  auto_ptr<vector<float> >           vector_jets_EMFcor   (new vector<float>         );
+  //auto_ptr<vector<float> >           vector_jets_EMFcor   (new vector<float>         );
 
   std::map<float, float>         uncorJets;
   std::map<float, float>         L2L3L4corJets;
@@ -96,23 +96,25 @@ void JetMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   *evt_njets = uncorJetsHandle->size();
 
   //get the correctors
-  const JetCorrector* L2L3corrector   = JetCorrector::getJetCorrector("L2L3JetCorrector"  , iSetup);
-  const JetCorrector* L2L3L4corrector = JetCorrector::getJetCorrector("L2L3L4JetCorrector", iSetup);
+  //the corrector is the the process from http://cmssw.cvs.cern.ch/cgi-bin/cmssw.cgi/CMSSW/JetMETCorrections/Configuration/python/
+  //for 312: L2L3Corrections_Summer09_cff.py
+  const JetCorrector* L2L3corrector   = JetCorrector::getJetCorrector("L2L3JetCorrectorSC5Calo"  , iSetup);
+  //const JetCorrector* L2L3L4corrector = JetCorrector::getJetCorrector("L2L3L4JetCorrector", iSetup);
 
   for(View<reco::CaloJet>::const_iterator it = uncorJetsHandle->begin(); it != uncorJetsHandle->end(); it++) {
     
     reco::CaloJet uncorJet   =  *it;
     reco::CaloJet L2L3Jet    =  uncorJet;
-    reco::CaloJet L2L3L4Jet  =  uncorJet;
+    //reco::CaloJet L2L3L4Jet  =  uncorJet;
 
     double L2L3Jetscale = L2L3corrector->correction( uncorJet.p4() );
     L2L3Jet.scaleEnergy( L2L3Jetscale );
 
-    double L2L3L4Jetscale = L2L3L4corrector->correction( uncorJet );
-    L2L3L4Jet.scaleEnergy( L2L3L4Jetscale );
+    //double L2L3L4Jetscale = L2L3L4corrector->correction( uncorJet );
+    //L2L3L4Jet.scaleEnergy( L2L3L4Jetscale );
 
     uncorJets[ L2L3Jet.p4().pt() ] = ( 1 / L2L3Jetscale );
-    L2L3L4corJets[ L2L3Jet.p4().pt() ] = ( L2L3L4Jetscale/L2L3Jetscale );
+    //L2L3L4corJets[ L2L3Jet.p4().pt() ] = ( L2L3L4Jetscale/L2L3Jetscale );
     emFracJets[ L2L3Jet.p4().pt() ] = L2L3Jet.emEnergyFraction();
     vertexJets[ L2L3Jet.p4().pt() ] = LorentzVector(L2L3Jet.vx(), L2L3Jet.vy(), L2L3Jet.vz(), 0.);
     
@@ -126,13 +128,14 @@ void JetMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   }
 
   map<float, float>::const_iterator         uncorIter     = uncorJets.begin();
-  map<float, float>::const_iterator         L2L3L4corIter = L2L3L4corJets.begin();
+  //map<float, float>::const_iterator         L2L3L4corIter = L2L3L4corJets.begin();
   map<float, float>::const_iterator         emFracIter    = emFracJets.begin();
   map<float, LorentzVector>::const_iterator vertexIter    = vertexJets.begin();
 
-  for( ; uncorIter != uncorJets.end(); uncorIter++, L2L3L4corIter++, emFracIter++, vertexIter++ ) {
+  //for( ; uncorIter != uncorJets.end(); uncorIter++, L2L3L4corIter++, emFracIter++, vertexIter++ ) {
+  for( ; uncorIter != uncorJets.end(); uncorIter++, emFracIter++, vertexIter++ ) {
     vector_jets_cor      ->push_back( uncorIter    ->second );
-    vector_jets_EMFcor   ->push_back( L2L3L4corIter->second );
+    //vector_jets_EMFcor   ->push_back( L2L3L4corIter->second );
     vector_jets_emFrac   ->push_back( emFracIter   ->second );
     vector_jets_vertex_p4->push_back( vertexIter   ->second );
   }
@@ -143,7 +146,7 @@ void JetMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   iEvent.put(vector_jets_vertex_p4,"jetsvertexp4" );
   iEvent.put(vector_jets_emFrac,   "jetsemFrac"   );
   iEvent.put(vector_jets_cor,      "jetscor"      );
-  iEvent.put(vector_jets_EMFcor,   "jetsEMFcor"   );
+  //iEvent.put(vector_jets_EMFcor,   "jetsEMFcor"   );
   
 }
 
