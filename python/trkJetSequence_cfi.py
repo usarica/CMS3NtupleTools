89d97658ee9693f08f51d84768b9bd2cb9bb2428
kalavase <kalavase>
2009-09-08 09:39:52 +0000

    Now running the CMS default TrackJet Sequence. Muons are not removed

--- python/trkJetSequence_cfi.py
+++ python/trkJetSequence_cfi.py
@@ -1,20 +1,29 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoJets.JetProducers.GenJetParameters_cfi import *
 from RecoJets.JetProducers.SISConeJetParameters_cfi import *
-from CMS2.NtupleMaker.trkMuonFilter_cfi import *
+from RecoJets.JetProducers.FastjetParameters_cfi import *
 
-SISCone5TrkJets =  cms.EDProducer("SISConeJetProducer"                                       ,
-                                  SISConeJetParameters                                       ,
-                                  alias          = cms.untracked.string('SISCone05TrackJets'),
-                                  inputEtMin     = cms.double(0.5)                           ,
-                                  inputEMin      = cms.double(0.)                            ,
-                                  src            = cms.InputTag('subTrkColl')                ,
-                                  jetType        = cms.untracked.string("BasicJet")          ,
-                                  jetPtMin       = cms.double(0.)                            ,
-                                  UE_Subtraction = cms.string('no')                          ,
-                                  coneRadius     = cms.double(0.5)
+selectTracks = cms.EDFilter("TrackSelector",
+    src = cms.InputTag("generalTracks"),
+    cut = cms.string('pt > 0.3 & pt<500 & numberOfValidHits > 0')
+)
+trackCandidates = cms.EDProducer("ConcreteChargedCandidateProducer",
+    src = cms.InputTag("selectTracks"),
+    particleType = cms.string('pi+')
+)
+scTrackJetParameters = cms.PSet(
+    SISConeJetParameters,FastjetNoPU, 
+    jetType = cms.untracked.string('BasicJet'),
+    src = cms.InputTag("trackCandidates"),
+    jetPtMin = cms.double( 0.0 ),
+    inputEtMin = cms.double(0.3),
+    inputEMin = cms.double(0.0)
+)
+SISCone5TrkJets = cms.EDProducer("SISConeJetProducer",
+    scTrackJetParameters,
+    alias = cms.untracked.string('SISCone5TrkJets'),
+    coneRadius = cms.double(0.5)
 )
 
 
-cms2TrkJetSequence = cms.Sequence( trkMuonFilter * SISCone5TrkJets)
+cms2TrkJetSequence = cms.Sequence( selectTracks * trackCandidates * SISCone5TrkJets)
