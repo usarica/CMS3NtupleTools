16c788d9542794fe6d344cf6744509b716a0d170
fgolf <fgolf>
2009-09-09 17:10:07 +0000

    deprecating obsolete module

--- python/wwCutMaker_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-wwCutMaker = cms.EDFilter("WWCutMaker",
-       dileptonInputTag = cms.InputTag("hypDilepMaker"),
-       muonsInputTag = cms.InputTag("muonMaker"),
-       electronsInputTag = cms.InputTag("electronMaker"),
-       eltomuassInputTag = cms.InputTag("elToMuAssMaker"),
-       genpsInputTag = cms.InputTag("genMaker")
-)
