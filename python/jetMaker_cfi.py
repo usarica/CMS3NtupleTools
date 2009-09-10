70a51ee4e97d86b860d89ed7a9c76906f45f1ff1
kalavase <kalavase>
2009-09-10 17:13:48 +0000

    added some tags

--- python/jetMaker_cfi.py
+++ python/jetMaker_cfi.py
@@ -1,7 +1,15 @@
 import FWCore.ParameterSet.Config as cms
 
 jetMaker = cms.EDFilter("JetMaker",
-           uncorJetsInputTag     = cms.InputTag("prunedUncorrectedCMS2Jets")
+           uncorJetsInputTag     = cms.InputTag("prunedUncorrectedCMS2Jets"),
+           runningOnReco         = cms.untracked.bool(True),
+           jetIDInputTag = cms.PSet(
+                 hbheRecHitsColl = cms.InputTag("hbhereco"),
+                 hoRecHitsColl   = cms.InputTag("horeco"),
+                 hfRecHitsColl   = cms.InputTag("hfreco"),
+                 ebRecHitsColl   = cms.InputTag("ecalRecHit", "EcalRecHitsEB"),
+                 eeRecHitsColl   = cms.InputTag("ecalRecHit", "EcalRecHitsEE")
+    )
 )
 
 
