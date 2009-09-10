e535a250cc569f08f64882649616562c7e558894
kalavase <kalavase>
2009-09-10 14:27:52 +0000

    Checks whether a double is -inf or inf. Needed 'cause some btag discriminators are -inf

---
+++ interface/CommonUtils.h
@@ -0,0 +1,30 @@
+// -*- C++ -*-
+//
+// Package:    NtupleMaker
+// Class:      CommonUtils
+//
+/**\class CommonUtils CommonUtils.cc CMS2/CommonUtils/src/CommonUtils.cc
+
+ Description: make associations between muons and tracks
+
+ Implementation:
+     <Notes on implementation>
+*/
+//
+// Original Author:  Puneeth Kalavase
+//         Created:  Thu Sep  10 11:07:38 CDT 2008
+//
+//
+
+
+#include <Math/VectorUtil.h>
+#include <iostream>
+#include <limits>
+
+namespace CommonUtils {
+  template<typename T> inline bool isinf(T value)
+    {
+      value = TMath::Abs(value);
+      return std::numeric_limits<T>::has_infinity && value == std::numeric_limits<T>::infinity();
+    }
+}
