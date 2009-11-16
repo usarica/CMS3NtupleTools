5bfa35a19bf61ddbb560ce3487e7be1ce37a350a
kalavase <kalavase>
2009-11-16 23:07:31 +0000

    Removed hlt8e29 process from sequence

--- python/hltMaker_cff.py
+++ python/hltMaker_cff.py
@@ -1,4 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 
 from CMS2.NtupleMaker.hltMaker_cfi import *
-hltMakerSequence = cms.Sequence(hlt8e29Maker*hltMaker)
+hltMakerSequence = cms.Sequence(hltMaker)
