6cdabb342a428d11bebf80608e9095654b2aabaf
kalavase <kalavase>
2009-11-18 21:53:03 +0000

    Moved calculation of electron's dist and dcot to the ElectronMaker.cc

--- src/ConversionMaker.cc
+++ src/ConversionMaker.cc
@@ -11,7 +11,7 @@ Description: make associations between jets and muons
 //
 // Original Author:  Puneeth Kalavase 
 //         Created:  Wed Oct 15 18:32:24 UTC 2008
-// $Id: ConversionMaker.cc,v 1.6 2009/10/19 00:02:51 kalavase Exp $
+// $Id: ConversionMaker.cc,v 1.7 2009/11/18 21:53:03 kalavase Exp $
 //
 //
 
@@ -34,7 +34,9 @@ Description: make associations between jets and muons
 #include "DataFormats/Math/interface/Point3D.h"
 #include "Math/VectorUtil.h"
 #include "Math/Point3D.h"
+#include "TMath.h"
 #include "DataFormats/Math/interface/deltaR.h"
+#include "RecoEgamma/EgammaTools/interface/ConversionFinder.h"
 
 typedef math::XYZTLorentzVectorF LorentzVector;
 typedef math::XYZPoint Point;
@@ -43,18 +45,11 @@ using namespace edm;
 
 ConversionMaker::ConversionMaker(const ParameterSet& iConfig)
 { 
-  produces<vector<int>   >   ("elsconvtkidx"   ).setBranchAlias("els_conv_tkidx"   );
-  produces<vector<float> >   ("elsconvdcot"    ).setBranchAlias("els_conv_dcot"    );
-  produces<vector<float> >   ("elsconvdist"    ).setBranchAlias("els_conv_dist" );
+  produces<vector<int>   >   ("trksconvtkidx"   ).setBranchAlias("trks_conv_tkidx" );
+  produces<vector<float> >   ("trksconvdcot"    ).setBranchAlias("trks_conv_dcot"  );
+  produces<vector<float> >   ("trksconvdist"    ).setBranchAlias("trks_conv_dist"  );
 
-  produces<vector<int>   >   ("trksconvtkidx"   ).setBranchAlias("trks_conv_tkidx"   );
-  produces<vector<float> >   ("trksconvdcot"    ).setBranchAlias("trks_conv_dcot"    );
-  produces<vector<float> >   ("trksconvdist"    ).setBranchAlias("trks_conv_dist" );
-
-
-  //electronMakerInputTag_ = iConfig.getParameter<InputTag>("electronMakerInputTag"   );
-  //tracksMakerInputTag_   = iConfig.getParameter<InputTag>("tracksMakerInputTag"     );
-  //bFieldInputTag_        = iConfig.getParameter<InputTag>("bFieldInputTag"          );
+  minFracShHits_ = iConfig.getParameter<double>("minFracSharedHits");
      
 }
 
@@ -63,34 +58,12 @@ void ConversionMaker::produce(Event& iEvent, const EventSetup& iSetup)
   using namespace edm;
   using namespace std;
   
-  // make vectors to hold the information
-  auto_ptr<vector<int>   > els_conv_tkidx        (new vector<int>    );
-  auto_ptr<vector<float> > els_conv_dcot         (new vector<float>  );
-  auto_ptr<vector<float> > els_conv_dist         (new vector<float>  );
-
-  auto_ptr<vector<int>   > trks_conv_tkidx        (new vector<int>    );
-  auto_ptr<vector<float> > trks_conv_dcot         (new vector<float>  );
-  auto_ptr<vector<float> > trks_conv_dist         (new vector<float>  );
+  auto_ptr<vector<int>   > trks_conv_tkidx       (new vector<int>    );
+  auto_ptr<vector<float> > trks_conv_dcot        (new vector<float>  );
+  auto_ptr<vector<float> > trks_conv_dist        (new vector<float>  );
 
 
-  // get electron p4s
-  Handle<vector<LorentzVector> > els_trk_p4_h;
-  iEvent.getByLabel("electronMaker", "elstrkp4", els_trk_p4_h);  
-          
-  //Handle<vector<float> > els_d0corr_h;
-  //iEvent.getByLabel("electronMaker", "elsd0corr", els_d0corr_h);  
-     
-  Handle<vector<float> > els_d0_h;
-  iEvent.getByLabel("electronMaker", "elsd0", els_d0_h);  
-     
-  Handle<vector<int> > els_charge_h;
-  iEvent.getByLabel("electronMaker", "elscharge", els_charge_h);
-     
-  //get the electron to track association 
-  Handle<vector<int> > els_trkidx_h;
-  //  iEvent.getByLabel("elToTrackAssMaker", "elstrkidx", els_trkidx_h);
-  iEvent.getByLabel("electronMaker", "elstrkidx", els_trkidx_h);
-     
+    
   //now get the Track quantities
   Handle<vector<LorentzVector> > trks_p4_h;
   iEvent.getByLabel("trackMaker", "trkstrkp4", trks_p4_h);  
@@ -106,115 +79,51 @@ void ConversionMaker::produce(Event& iEvent, const EventSetup& iSetup)
 
   Handle<float> evt_bField_h;
   iEvent.getByLabel("eventMaker", "evtbField", evt_bField_h);
+  float bField = *evt_bField_h.product();
   
-  for(unsigned int elIndex = 0; elIndex < els_trk_p4_h->size();
-      elIndex++) { //electron loop
-       
-    int closestTkIdx = -999;
-    float mindEta = 999;
-    //pair.first = dist, pair.second = dcot
-    pair <float, float> p_elConvInfo = make_pair(-999,-999);  
-    for(unsigned int tkIndex = 0; tkIndex < trks_p4_h->size();
-	tkIndex++) {
-      
-      if(els_charge_h->at(elIndex) + trks_charge_h->at(tkIndex) != 0) continue;
-      //don't include the electron's track
-      
-      if(els_trkidx_h->at(elIndex) == (int)tkIndex) continue;
-      //look only in cone of 0.5, which is probably a little big, but whatever
-      if(ROOT::Math::VectorUtil::DeltaR(els_trk_p4_h->at(elIndex), trks_p4_h->at(tkIndex)) > 0.5) 
-	continue;
-      if(fabs(els_trk_p4_h->at(elIndex).Eta() - trks_p4_h->at(tkIndex).Eta()) < mindEta) {
-	mindEta = fabs(els_trk_p4_h->at(elIndex).Eta() - trks_p4_h->at(tkIndex).Eta());
-	closestTkIdx = tkIndex;
-	p_elConvInfo = getConversionInfo(els_trk_p4_h->at(elIndex), els_charge_h->at(elIndex),
-					 els_d0_h->at(elIndex), trks_p4_h->at(tkIndex),
-					 trks_charge_h->at(tkIndex), trks_d0_h->at(tkIndex),
-					 *evt_bField_h.product());
-      }//if dEta < minDeta
-    }//for(unsigned int tkIndex = 0.....
-    els_conv_tkidx    ->push_back(closestTkIdx            );
-    els_conv_dist     ->push_back(p_elConvInfo.first          );
-    els_conv_dcot     ->push_back(p_elConvInfo.second         );
-    
-  }//for(unsigned int elIndex = 0........
-
 
   //now do the same for the tracks
   for(unsigned int tk1Index = 0; tk1Index < trks_p4_h->size();
       tk1Index++) {
        
-    int closestTkIdx = -999;
-    float mindEta = 999;
+    int closestTkIdx = -9999;
+    float mindR = 9999.;
     //pair.first=dist, pair.second = dcot
-    pair <float, float> p_tkConvInfo = make_pair(-999,-999);
+    pair <float, float> p_tkConvInfo = make_pair(-9999.,-9999.);
     for(unsigned int tk2Index = 0; tk2Index < trks_p4_h->size();
 	tk2Index++) {
 	 
       if(trks_charge_h->at(tk1Index) + trks_charge_h->at(tk2Index) != 0 ) continue;
       if(tk1Index == tk2Index) continue; //don't want to use the same track!
       //look only in cone of 0.3
-      if(ROOT::Math::VectorUtil::DeltaR(trks_p4_h->at(tk1Index), trks_p4_h->at(tk2Index)) > 0.3) 
+      double dR = deltaR( trks_p4_h->at(tk1Index).eta(), trks_p4_h->at(tk1Index).phi(),
+			  trks_p4_h->at(tk2Index).eta(), trks_p4_h->at(tk2Index).phi());
+      
+      if(dR  > 0.3)
 	continue;
       
-      if(fabs(trks_p4_h->at(tk1Index).Eta() - trks_p4_h->at(tk2Index).Eta()) < mindEta) {
-	mindEta = fabs(trks_p4_h->at(tk1Index).Eta() - trks_p4_h->at(tk2Index).Eta());
+      if(dR < mindR) {
+	mindR = dR;
 	closestTkIdx = tk2Index;
-	p_tkConvInfo = getConversionInfo(trks_p4_h->at(tk1Index), trks_charge_h->at(tk1Index),
-					 trks_d0_h->at(tk1Index), trks_p4_h->at(tk2Index),
-					 trks_charge_h->at(tk2Index), trks_d0_h->at(tk2Index),
-					 *evt_bField_h.product());
-      }//if dEta < mindEta
+	p_tkConvInfo = ConversionFinder::getConversionInfo(math::XYZTLorentzVector(trks_p4_h->at(tk1Index)), trks_charge_h->at(tk1Index),
+							   trks_d0_h->at(tk1Index), math::XYZTLorentzVector(trks_p4_h->at(tk2Index)),
+							   trks_charge_h->at(tk2Index), trks_d0_h->at(tk2Index),
+							   bField);
+      }//if(dR < mindR)
     }//tk2 loop
-    trks_conv_tkidx    ->push_back(closestTkIdx            );
+
+    trks_conv_tkidx    ->push_back(closestTkIdx                );
     trks_conv_dist     ->push_back(p_tkConvInfo.first          );
     trks_conv_dcot     ->push_back(p_tkConvInfo.second         );
   }//tk1 loop
 
   // store vectors
-  iEvent.put(els_conv_tkidx,        "elsconvtkidx"   );
-  iEvent.put(els_conv_dcot,         "elsconvdcot"    );
-  iEvent.put(els_conv_dist,         "elsconvdist"    );
   iEvent.put(trks_conv_tkidx,       "trksconvtkidx"  );
   iEvent.put(trks_conv_dist,        "trksconvdist"   );
   iEvent.put(trks_conv_dcot,        "trksconvdcot"   );
 
 }
 		     
-
-
-//-------------------------------------------------------------------------------------
-//Function that does the work of finding the dist and dcot theta 
-//--------------------------
-
-pair<float, float> ConversionMaker::getConversionInfo(math::XYZTLorentzVectorF trk1_p4, 
-						      int trk1_q, float trk1_d0, 
-						      math::XYZTLorentzVectorF trk2_p4,
-						      int trk2_q, float trk2_d0,
-						      float bField) {
-  
-  
-  double tk1Curvature = -0.3*bField*(trk1_q/trk1_p4.pt())/100.;
-  double rTk1 = fabs(1./tk1Curvature);
-  double xTk1 = (1./tk1Curvature - trk1_d0)*cos(trk1_p4.phi());
-  double yTk1 = (1./tk1Curvature - trk1_d0)*sin(trk1_p4.phi());
-  
-  
-  
-  
-  double tk2Curvature = -0.3*bField*(trk2_q/trk2_p4.pt())/100.;
-  double rTk2 = fabs(1./tk2Curvature);
-  double xTk2 = (1./tk2Curvature - trk2_d0)*cos(trk2_p4.phi());
-  double yTk2 = (1./tk2Curvature - trk2_d0)*sin(trk2_p4.phi());
-	 
-  double dist = sqrt(pow(xTk1-xTk2, 2) + pow(yTk1-yTk2 , 2));
-  dist = dist - (rTk1 + rTk2);
-
-  double dcot = 1/tan(trk1_p4.theta()) - 1/tan(trk2_p4.theta());
-
-  return make_pair(dist, dcot);
-  
-}
 
 
 // ------------ method called once each job just before starting event loop  ------------
