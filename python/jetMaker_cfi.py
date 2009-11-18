8a2fbaa316e3cc38a1e845453c2ba9cf15154da5
kalavase <kalavase>
2009-11-18 03:50:53 +0000

    Changed jetid InputTag

--- python/jetMaker_cfi.py
+++ python/jetMaker_cfi.py
@@ -3,17 +3,9 @@ import FWCore.ParameterSet.Config as cms
 jetMaker = cms.EDFilter("JetMaker",
            uncorJetsInputTag     = cms.InputTag("prunedUncorrectedCMS2Jets"),
            runningOnReco         = cms.untracked.bool(True),
-           jetIDInputTag = cms.PSet(
-     	         useRecHits = cms.bool(True),
-	         hbheRecHitsColl = cms.InputTag("hbhereco"),
-	         hoRecHitsColl   = cms.InputTag("horeco"),
-	         hfRecHitsColl   = cms.InputTag("hfreco"),
-	         ebRecHitsColl   = cms.InputTag("ecalRecHit", "EcalRecHitsEB"),
-	         eeRecHitsColl   = cms.InputTag("ecalRecHit", "EcalRecHitsEE"),
-	         rpcRecHits      = cms.InputTag("rpcRecHits")
-                 ),
            L2L3JetCorrectorName = cms.string("L2L3JetCorrectorAK5Calo"),
-           AliasPrefix          = cms.string("jets")
+           AliasPrefix          = cms.string("jets"),
+           jetIDIputTag = cms.InputTag("cms2ak5JetID")
 )
 
 
--- python/scjetMaker_cfi.py
+++ python/scjetMaker_cfi.py
@@ -3,17 +3,9 @@ import FWCore.ParameterSet.Config as cms
 scjetMaker = cms.EDFilter("JetMaker",
            uncorJetsInputTag     = cms.InputTag("prunedUncorrectedCMS2scJets"),
            runningOnReco         = cms.untracked.bool(True),
-           jetIDInputTag = cms.PSet(
-     	         useRecHits = cms.bool(True),
-	         hbheRecHitsColl = cms.InputTag("hbhereco"),
-	         hoRecHitsColl   = cms.InputTag("horeco"),
-	         hfRecHitsColl   = cms.InputTag("hfreco"),
-	         ebRecHitsColl   = cms.InputTag("ecalRecHit", "EcalRecHitsEB"),
-	         eeRecHitsColl   = cms.InputTag("ecalRecHit", "EcalRecHitsEE"),
-	         rpcRecHits      = cms.InputTag("rpcRecHits")
-                 ),
            L2L3JetCorrectorName = cms.string("L2L3JetCorrectorSC5Calo"),
-           AliasPrefix          = cms.string("scjets")
+           AliasPrefix          = cms.string("scjets"),
+           jetIDIputTag = cms.InputTag("cms2sc5JetID")
 )
 
 
