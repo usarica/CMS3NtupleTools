004fc7433d6a11b2b0c450792f1318af2d84fb5c
kalavase <kalavase>
2009-11-18 03:51:05 +0000

    Run jetID valuemap producer

--- python/jetCollectionPruner_cfi.py
+++ python/jetCollectionPruner_cfi.py
@@ -1,7 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
+from RecoJets.JetProducers.ak5JetID_cfi import ak5JetID
+
 prunedUncorrectedCMS2Jets = cms.EDFilter("JetCollectionPruner",
    inputUncorrectedJetCollection = cms.InputTag("ak5CaloJets"),
    uncorrectedJetPtCut           = cms.double(5.0) ##cut on the UNCORRECTED reco jets!!!!!
 )
 
+
+cms2ak5JetID = ak5JetID.clone()
+cms2ak5JetID.src = cms.InputTag("prunedUncorrectedCMS2Jets")
