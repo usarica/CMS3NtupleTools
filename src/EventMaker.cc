4406115c7af4dd13c1b8755649a6b2abaeb3c47f
kalavase <kalavase>
2009-11-18 03:47:47 +0000

    Added bunch crossing Id, time stamp, orbit number and experiment type

--- src/EventMaker.cc
+++ src/EventMaker.cc
@@ -13,7 +13,7 @@ Implementation:
 //
 // Original Author:  Puneeth Kalavase
 //         Created:  Fri Jun  6 11:07:38 CDT 2008
-// $Id: EventMaker.cc,v 1.23 2009/09/10 10:51:43 fgolf Exp $
+// $Id: EventMaker.cc,v 1.24 2009/11/18 03:47:47 kalavase Exp $
 //
 //
 
@@ -46,12 +46,17 @@ using namespace std;
 
 EventMaker::EventMaker(const edm::ParameterSet& iConfig) {
 
-  produces<unsigned int>     ("evtrun"            ).setBranchAlias("evt_run"           );
-  produces<unsigned int>     ("evtevent"          ).setBranchAlias("evt_event"         );
-  produces<unsigned int>     ("evtlumiBlock"      ).setBranchAlias("evt_lumiBlock"     );
-  produces<TString>          ("evtdataset"        ).setBranchAlias("evt_dataset"       );
-  produces<TString>          ("evtCMS2tag"        ).setBranchAlias("evt_CMS2tag"       );
-  produces<float>            ("evtbField"         ).setBranchAlias("evt_bField"        );
+  produces<unsigned int>        ("evtrun"            ).setBranchAlias("evt_run"           );
+  produces<unsigned int>        ("evtevent"          ).setBranchAlias("evt_event"         );
+  produces<unsigned int>        ("evtlumiBlock"      ).setBranchAlias("evt_lumiBlock"     );
+  produces<int>                 ("evtbunchCrossing"  ).setBranchAlias("evt_bunchCrossing" );
+  produces<int>                 ("evtorbitNumber"    ).setBranchAlias("evt_orbitNumber"   );
+  produces<int>                 ("evtstoreNumber"    ).setBranchAlias("evt_storeNumber"   );
+  produces<int>                 ("evtexperimentType" ).setBranchAlias("evt_experimentType");
+  produces<unsigned long long>  ("evttimestamp"      ).setBranchAlias("evt_timestamp"     );
+  produces<TString>             ("evtdataset"        ).setBranchAlias("evt_dataset"       );
+  produces<TString>             ("evtCMS2tag"        ).setBranchAlias("evt_CMS2tag"       );
+  produces<float>               ("evtbField"         ).setBranchAlias("evt_bField"        );
   
   datasetName_ = iConfig.getParameter<std::string>("datasetName");
   CMS2tag_     = iConfig.getParameter<std::string>("CMS2tag");
@@ -69,16 +74,27 @@ void EventMaker::endJob() {
 // ------------ method called to produce the data  ------------
 void EventMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   
-  auto_ptr<unsigned int>     evt_run             (new unsigned int);
-  auto_ptr<unsigned int>     evt_event           (new unsigned int);
-  auto_ptr<unsigned int>     evt_lumiBlock       (new unsigned int);
-  auto_ptr<TString>          evt_dataset         (new TString(datasetName_.c_str()));
-  auto_ptr<TString>          evt_CMS2tag         (new TString(CMS2tag_.c_str()));
-  auto_ptr<float>            evt_bField          (new float);
+  auto_ptr<unsigned int>         evt_run             (new unsigned int              );
+  auto_ptr<unsigned int>         evt_event           (new unsigned int              );
+  auto_ptr<unsigned int>         evt_lumiBlock       (new unsigned int              );
+  auto_ptr<int>                  evt_bunchCrossing   (new int                       );
+  auto_ptr<int>                  evt_orbitNumber     (new int                       );
+  auto_ptr<int>                  evt_storeNumber     (new int                       );
+  auto_ptr<int>                  evt_experimentType  (new int                       );
+  auto_ptr<unsigned long long>   evt_timestamp       (new unsigned long long        );
+  auto_ptr<TString>              evt_dataset         (new TString(datasetName_.c_str()));
+  auto_ptr<TString>              evt_CMS2tag         (new TString(CMS2tag_.c_str()) );
+  auto_ptr<float>                evt_bField          (new float                     );
   
-  *evt_run   = iEvent.id().run();
-  *evt_event = iEvent.id().event();
-  *evt_lumiBlock = iEvent.luminosityBlock();
+  *evt_run                       = iEvent.id().run()        ;
+  *evt_event                     = iEvent.id().event()      ;
+  *evt_lumiBlock                 = iEvent.luminosityBlock() ;
+  *evt_bunchCrossing             = iEvent.bunchCrossing()   ;
+  *evt_orbitNumber               = iEvent.orbitNumber()     ;
+  *evt_storeNumber               = iEvent.eventAuxiliary().storeNumber();
+  *evt_experimentType            = iEvent.experimentType()  ;
+  *evt_timestamp                 = iEvent.eventAuxiliary().time().value();
+  
   
  //need the magnetic field
   ESHandle<MagneticField> magneticField;
@@ -88,6 +104,11 @@ void EventMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   iEvent.put(evt_run              ,"evtrun"             );
   iEvent.put(evt_event            ,"evtevent"           );
   iEvent.put(evt_lumiBlock        ,"evtlumiBlock"       );
+  iEvent.put(evt_bunchCrossing    ,"evtbunchCrossing"   );
+  iEvent.put(evt_orbitNumber      ,"evtorbitNumber"     );
+  iEvent.put(evt_storeNumber      ,"evtstoreNumber"     );
+  iEvent.put(evt_experimentType   ,"evtexperimentType"  );
+  iEvent.put(evt_timestamp        ,"evttimestamp"       );
   iEvent.put(evt_dataset          ,"evtdataset"         );
   iEvent.put(evt_CMS2tag          ,"evtCMS2tag"         );
   iEvent.put(evt_bField           ,"evtbField"          );
