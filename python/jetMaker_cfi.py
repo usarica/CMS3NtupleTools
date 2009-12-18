080c0fab2367a5e73f11ff5a5338bc5282d27ecc
kalavase <kalavase>
2009-12-18 19:11:29 +0000

    Added parameters necessary for new JEC

--- python/jetMaker_cfi.py
+++ python/jetMaker_cfi.py
@@ -3,7 +3,9 @@ import FWCore.ParameterSet.Config as cms
 jetMaker = cms.EDFilter("JetMaker",
            uncorJetsInputTag     = cms.InputTag("prunedUncorrectedCMS2Jets"),
            runningOnReco         = cms.untracked.bool(True),
-           L2L3JetCorrectorName = cms.string("L2L3JetCorrectorAK5Calo"),
+           #L2L3JetCorrectorName = cms.string("L2L3JetCorrectorAK5Calo"),
+           correctionLevels      = cms.string("L2:L3"),
+           correctionTags        = cms.string("900GeV_L2Relative_AK5Calo:900GeV_L3Absolute_AK5Calo"),
            AliasPrefix          = cms.string("jets"),
            jetIDIputTag = cms.InputTag("cms2ak5JetID")
 )
--- python/scjetMaker_cfi.py
+++ python/scjetMaker_cfi.py
@@ -4,6 +4,8 @@ scjetMaker = cms.EDFilter("JetMaker",
            uncorJetsInputTag     = cms.InputTag("prunedUncorrectedCMS2scJets"),
            runningOnReco         = cms.untracked.bool(True),
            L2L3JetCorrectorName = cms.string("L2L3JetCorrectorSC5Calo"),
+           correctionLevels      = cms.string("L2:L3"),
+           correctionTags        = cms.string("900GeV_L2Relative_SC5Calo:900GeV_L3Absolute_SC5Calo"),
            AliasPrefix          = cms.string("scjets"),
            jetIDIputTag = cms.InputTag("cms2sc5JetID")
 )
