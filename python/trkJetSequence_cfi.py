d705e57275aee491260c4363fb0f5a9c28ccc950
kalavase <kalavase>
2009-12-31 01:24:28 +0000

    Updated trkJetSequence for 341

--- python/trkJetSequence_cfi.py
+++ python/trkJetSequence_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
+from RecoJets.JetProducers.ak5TrackJets_cfi import ak5TrackJets
 from RecoJets.JetProducers.TracksForJets_cff import *
-from RecoJets.JetProducers.ak5TrackJets_cfi import *
 
-cms2TrkJetSequence = cms.Sequence( tracksForJets * ak5TrackJets)
+cms2TrkJetSequence = cms.Sequence(trackWithVertexRefSelector*trackRefsForJets*ak5TrackJets)
