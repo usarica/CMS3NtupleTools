b01ea6e2b775f668eb3f4376bf0299742e482158
warren <warren>
2010-02-17 02:51:17 +0000

    need new import because definition moved

--- python/trkJetSequence_cfi.py
+++ python/trkJetSequence_cfi.py
@@ -2,5 +2,6 @@ import FWCore.ParameterSet.Config as cms
 
 from RecoJets.JetProducers.ak5TrackJets_cfi import ak5TrackJets
 from RecoJets.JetProducers.TracksForJets_cff import *
+from CommonTools.RecoAlgos.TrackWithVertexRefSelector_cfi import *
 
 cms2TrkJetSequence = cms.Sequence(trackWithVertexRefSelector*trackRefsForJets*ak5TrackJets)
