5cc780802efffdd92175d5b012fe0344e22a6efb
jribnik <jribnik>
2010-03-11 17:11:35 +0000

    Extract trckvetoDep the PAT way, i.e. from isodeposit candenergy, as is done for the ecal and hcal, rather than from the inherited muon object. Btw it kinda bothers me that it's trckvetoDep when we have trackIso, but I'm going to resist changing these names here and in the MuonMaker.
    In the same vein, get iso values the PAT way, i.e. userIsolation(pat::blah),
    rather than from the inherited muon as the pat::[track|ecal|hcal]Iso methods
    do.
    
    It's all rather silly, they are exactly the same. We could just as well have
    left the iso values as they were and instead extracted the vetoDeps from the
    reco::Muon.  However, since this is PATMuonMaker we'll do things the PAT way
    and ensure consistency between our iso and vetoDep values.
    
    Renamed mus_pat_vetoDep to mus_pat_calovetoDep so that I don't have to check
    the code to see that it does not include the tracker. Also we have caloIso
    and not just iso so it kinda makes sense. I checked that this is not used by
    anyone anywhere so probably we can just drop it altogether as it is trivial to
    recalculate. mus_pat_caloIso is also trivial to recalculate...

--- src/PATMuonMaker.cc
+++ src/PATMuonMaker.cc
@@ -14,7 +14,7 @@ Description: copy additional PAT muon variables in simple data structures into t
 //
 // Original Author:  Frank Golf
 // Thu Jun 25 16:39:55 UTC 2008
-// $Id: PATMuonMaker.cc,v 1.12 2010/03/02 19:36:08 fgolf Exp $
+// $Id: PATMuonMaker.cc,v 1.13 2010/03/11 17:11:35 jribnik Exp $
 //
 //
 
@@ -61,7 +61,7 @@ PATMuonMaker::PATMuonMaker(const edm::ParameterSet& iConfig) {
   produces<vector<float>          >   ("muspathcalIso"     ).setBranchAlias("mus_pat_hcalIso"     );
   //deposit in the veto cone - gotten from the IsoDeposits
   produces<vector<float>          >   ("muspattrckvetoDep" ).setBranchAlias("mus_pat_trckvetoDep" );
-  produces<vector<float>          >   ("muspatvetoDep"     ).setBranchAlias("mus_pat_vetoDep"     );
+  produces<vector<float>          >   ("muspatcalovetoDep" ).setBranchAlias("mus_pat_calovetoDep" );
   produces<vector<float>          >   ("muspatecalvetoDep" ).setBranchAlias("mus_pat_ecalvetoDep" );
   produces<vector<float>          >   ("muspathcalvetoDep" ).setBranchAlias("mus_pat_hcalvetoDep" );
   produces<vector<int>            >   ("muspatgenID"       ).setBranchAlias("mus_pat_genID"       );
@@ -99,7 +99,7 @@ void PATMuonMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   auto_ptr<vector<float>             >   mus_pat_ecalIso     ( new vector<float>             );
   auto_ptr<vector<float>             >   mus_pat_hcalIso     ( new vector<float>             );
   auto_ptr<vector<float>             >   mus_pat_trckvetoDep ( new vector<float>             ); //energy in track veto cone
-  auto_ptr<vector<float>             >   mus_pat_vetoDep     ( new vector<float>             ); //sum of ecal+hcal veto cones
+  auto_ptr<vector<float>             >   mus_pat_calovetoDep ( new vector<float>             ); //sum of ecal+hcal veto cones
   auto_ptr<vector<float>             >   mus_pat_ecalvetoDep ( new vector<float>             ); //energy in ecal veto cone
   auto_ptr<vector<float>             >   mus_pat_hcalvetoDep ( new vector<float>             ); //energy in hcal veto cone
   auto_ptr<vector<int>               >   mus_pat_genID       ( new vector<int>               );
@@ -118,24 +118,24 @@ void PATMuonMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
     const GenParticle *gen_mom = MCUtilities::motherID(gen);
 
     //the veto cone size is 0.07 in the ecal and 0.1 in the hcal 
-    // const reco::IsoDeposit *trckIsoDep = patmu_it->isolationR03().trackerVetoPt;
-    float trckIsoDep = patmu_it->isolationR03().trackerVetoPt;
+    // and 0.01 in the tracker
+    const reco::IsoDeposit *trckIsoDep = patmu_it->trackIsoDeposit();
     const reco::IsoDeposit *ecalIsoDep = patmu_it->ecalIsoDeposit();
     const reco::IsoDeposit *hcalIsoDep = patmu_it->hcalIsoDeposit();
-    
+
     mus_pat_p4          ->push_back( LorentzVector( patmu_it->p4() ) );
-    mus_pat_trackIso    ->push_back( patmu_it->trackIso()       );
-    mus_pat_trckvetoDep ->push_back( trckIsoDep   );
-    mus_pat_vetoDep     ->push_back( ecalIsoDep->candEnergy() + hcalIsoDep->candEnergy() );
-    mus_pat_ecalvetoDep ->push_back( ecalIsoDep->candEnergy()   );
-    mus_pat_hcalvetoDep ->push_back( hcalIsoDep->candEnergy()   );
-    mus_pat_ecalIso     ->push_back( patmu_it->ecalIso()        );
-    mus_pat_hcalIso     ->push_back( patmu_it->hcalIso()        );
-    mus_pat_caloIso     ->push_back( patmu_it->caloIso()        );
-    mus_pat_genID       ->push_back( gen.pdgId()            );
-    mus_pat_genMotherID ->push_back( gen_mom->pdgId()       );
-    mus_pat_flag        ->push_back( patmu_it->status()     );
-    mus_pat_genP4       ->push_back( LorentzVector( gen.p4() )     );
+    mus_pat_trackIso    ->push_back( patmu_it->userIsolation(pat::TrackIso) );
+    mus_pat_caloIso     ->push_back( patmu_it->userIsolation(pat::EcalIso) + patmu_it->userIsolation(pat::HcalIso) );
+    mus_pat_ecalIso     ->push_back( patmu_it->userIsolation(pat::EcalIso)  );
+    mus_pat_hcalIso     ->push_back( patmu_it->userIsolation(pat::HcalIso)  );
+    mus_pat_trckvetoDep ->push_back( trckIsoDep->candEnergy() );
+    mus_pat_calovetoDep ->push_back( ecalIsoDep->candEnergy() + hcalIsoDep->candEnergy() );
+    mus_pat_ecalvetoDep ->push_back( ecalIsoDep->candEnergy() );
+    mus_pat_hcalvetoDep ->push_back( hcalIsoDep->candEnergy() );
+    mus_pat_genID       ->push_back( gen.pdgId()              );
+    mus_pat_genMotherID ->push_back( gen_mom->pdgId()         );
+    mus_pat_flag        ->push_back( patmu_it->status()       );
+    mus_pat_genP4       ->push_back( LorentzVector( gen.p4() )      );
     mus_pat_genMotherP4 ->push_back( LorentzVector( gen_mom->p4() ) );
     
 
@@ -145,13 +145,13 @@ void PATMuonMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
   // put containers into event
   iEvent.put(mus_pat_p4,           "muspatp4"              );
   iEvent.put(mus_pat_trackIso,     "muspattrackIso"        );
-  iEvent.put(mus_pat_trckvetoDep,  "muspattrckvetoDep"     );
-  iEvent.put(mus_pat_vetoDep,      "muspatvetoDep"         );
-  iEvent.put(mus_pat_ecalvetoDep,  "muspatecalvetoDep"     );
-  iEvent.put(mus_pat_hcalvetoDep,  "muspathcalvetoDep"     );
   iEvent.put(mus_pat_caloIso,      "muspatcaloIso"         );
   iEvent.put(mus_pat_ecalIso,      "muspatecalIso"         );
   iEvent.put(mus_pat_hcalIso,      "muspathcalIso"         );
+  iEvent.put(mus_pat_trckvetoDep,  "muspattrckvetoDep"     );
+  iEvent.put(mus_pat_calovetoDep,  "muspatcalovetoDep"     );
+  iEvent.put(mus_pat_ecalvetoDep,  "muspatecalvetoDep"     );
+  iEvent.put(mus_pat_hcalvetoDep,  "muspathcalvetoDep"     );
   
   iEvent.put(mus_pat_genID,        "muspatgenID"           );
   iEvent.put(mus_pat_genMotherID,  "muspatgenMotherID"     );
