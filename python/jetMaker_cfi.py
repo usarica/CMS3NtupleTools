08e55cacdcaa388a9da7484aa3f13c4f58e1ce12
kalavase <kalavase>
2010-04-13 00:00:39 +0000

    switched to using summer09 7 TeV JEC by default

--- python/jetMaker_cfi.py
+++ python/jetMaker_cfi.py
@@ -4,7 +4,7 @@ jetMaker = cms.EDFilter("JetMaker",
            uncorJetsInputTag     = cms.InputTag("prunedUncorrectedCMS2Jets"),
            runningOnReco         = cms.untracked.bool(True),
            correctionLevels      = cms.string("L2:L3"),
-           correctionTags        = cms.string("900GeV_L2Relative_AK5Calo:900GeV_L3Absolute_AK5Calo"),
+           correctionTags        = cms.string("Summer09_7TeV_L2Relative_AK5Calo:Summer09_7TeV_L3Absolute_AK5Calo"),
            AliasPrefix          = cms.string("jets"),
            jetIDIputTag = cms.InputTag("cms2ak5JetID")
 )
--- python/scjetMaker_cfi.py
+++ python/scjetMaker_cfi.py
@@ -5,7 +5,7 @@ scjetMaker = cms.EDFilter("JetMaker",
            runningOnReco         = cms.untracked.bool(True),
            L2L3JetCorrectorName = cms.string("L2L3JetCorrectorSC5Calo"),
            correctionLevels      = cms.string("L2:L3"),
-           correctionTags        = cms.string("900GeV_L2Relative_SC5Calo:900GeV_L3Absolute_SC5Calo"),
+           correctionTags        = cms.string("Summer09_7TeV_L2Relative_SC5Calo:Summer09_7TeV_L3Absolute_SC5Calo"),
            AliasPrefix          = cms.string("scjets"),
            jetIDIputTag = cms.InputTag("cms2sc5JetID")
 )
