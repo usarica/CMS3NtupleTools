2753b6a2e04b67e40948ad03545719c3ab119808
warren <warren>
2010-04-20 02:10:25 +0000

    add threshold (set to 5) and rec hit colls

--- python/photonMaker_cfi.py
+++ python/photonMaker_cfi.py
@@ -2,7 +2,10 @@ import FWCore.ParameterSet.Config as cms
 
 photonMaker = cms.EDFilter("PhotonMaker",
 	aliasPrefix = cms.untracked.string("photons"),
+    minEt       = cms.double(5.), #gev, min to keep
     # Photon collection
     photonsInputTag = cms.InputTag("photons"),
+    ecalRecHitsInputTag_EE = cms.InputTag("ecalRecHit","EcalRecHitsEE"),
+    ecalRecHitsInputTag_EB = cms.InputTag("ecalRecHit","EcalRecHitsEB"),
 )
 
