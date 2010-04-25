bf2495c42505edafdde92f635d12986c4bb77a81
kalavase <kalavase>
2010-04-25 14:04:17 +0000

    made pt cut configurable

--- python/calotauMaker_cfi.py
+++ python/calotauMaker_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 calotauMaker = cms.EDFilter("CaloTauMaker",
 	aliasPrefix = cms.untracked.string("taus_calo"),
-      
+        minleadTrackPt = cms.double(5.),
         # CaloTau collection
         calotausInputTag = cms.InputTag("caloRecoTauProducer")
 )
--- python/pftauMaker_cfi.py
+++ python/pftauMaker_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 pftauMaker = cms.EDFilter("PFTauMaker",
 	aliasPrefix = cms.untracked.string("taus_pf"),
+        minleadPFChargedHadrCandPt = cms.double(5.),
         # PFTau collection
         pftausInputTag = cms.InputTag("fixedConePFTauProducer"),
       
