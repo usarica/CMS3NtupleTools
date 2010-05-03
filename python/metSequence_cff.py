1dbedfbebae6ccfe14b73618487f0689811f1a53
kalavase <kalavase>
2010-05-03 23:38:06 +0000

    update L2 corr corrector name

--- python/metSequence_cff.py
+++ python/metSequence_cff.py
@@ -1,12 +1,12 @@
 import FWCore.ParameterSet.Config as cms
 
-from JetMETCorrections.Configuration.L2L3Corrections_Summer09_cff import *
+
 from JetMETCorrections.Type1MET.MetType1Corrections_cff import metJESCorIC5CaloJet
+from JetMETCorrections.Configuration.JetCorrectionServices_cff import *
 
 metMuonJESCorAK5CMS2                     = metJESCorIC5CaloJet.clone()
 metMuonJESCorAK5CMS2.inputUncorJetsLabel = "prunedUncorrectedCMS2Jets"
-metMuonJESCorAK5CMS2.corrector           = "L2L3JetCorrectorAK5Calo"
+metMuonJESCorAK5CMS2.corrector           = "ak5CaloL2L3"
 metMuonJESCorAK5CMS2.inputUncorMetLabel  = "corMetGlobalMuons"
 
 metCorSequence = cms.Sequence(metMuonJESCorAK5CMS2)
-
