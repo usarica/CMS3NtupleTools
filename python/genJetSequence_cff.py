c1f2115c5ff2395ab167709fc52ce17ba79b366a
kalavase <kalavase>
2010-05-06 20:13:48 +0000

    use existing genparticles for jets

--- python/genJetSequence_cff.py
+++ python/genJetSequence_cff.py
@@ -1,21 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
+from RecoJets.Configuration.GenJetParticles_cff import genParticlesForJetsNoMuNoNu
 from RecoJets.JetProducers.ak5GenJets_cfi import ak5GenJets
 
-genParticlesAllStables =  cms.EDProducer("InputGenJetsParticleSelector",
-          src                      = cms.InputTag("genParticles"),
-          partonicFinalState       = cms.bool(False),
-          excludeResonances        = cms.bool(False),
-          excludeFromResonancePids = cms.vuint32(),
-          tausAsJets               = cms.bool(False),
-          ignoreParticleIDs        = cms.vuint32(
-            1000022, 2000012, 2000014,
-            2000016, 1000039, 5000039,
-            4000012, 9900012, 9900014,
-            9900016, 39, 12, 13, 14, 16)
-)
-
-
-cms2antikt5GenJets = ak5GenJets.clone(src = cms.InputTag("genParticlesAllStables") )
-genJetSequence = cms.Sequence( genParticlesAllStables * cms2antikt5GenJets )
+cms2antikt5GenJets = ak5GenJets.clone(src = cms.InputTag("genParticlesForJetsNoMuNoNu") )
+genJetSequence = cms.Sequence( genParticlesForJetsNoMuNoNu * cms2antikt5GenJets )
 
