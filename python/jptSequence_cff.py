89234ff330a4f1c2f811a0a87baeff7feea46f0e
fgolf <fgolf>
2010-05-06 18:08:06 +0000

    reactivating jptSequence to run on 3_5_7

---
+++ python/jptSequence_cff.py
@@ -0,0 +1,9 @@
+import FWCore.ParameterSet.Config as cms
+
+from JetMETCorrections.Configuration.ZSPJetCorrections332_cff import *
+from JetMETCorrections.Configuration.JetPlusTrackCorrections_cff import *
+from CMS2.NtupleMaker.jptMaker_cfi import jptMaker
+
+ZSPJetCorJetAntiKt5.src = cms.InputTag("prunedUncorrectedCMS2Jets")
+
+JPTCorrections = cms.Sequence(ZSPJetCorrectionsAntiKt5 * JetPlusTrackCorrectionsAntiKt5 * jptMaker)
