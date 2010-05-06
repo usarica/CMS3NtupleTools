9df4831930ded868683fafc2bfaefaa9f13997dd
kalavase <kalavase>
2010-05-06 21:23:03 +0000

    cleaned up

--- test/MCProduction2010_NoFilter_cfg.py
+++ test/MCProduction2010_NoFilter_cfg.py
@@ -5,26 +5,25 @@ process = cms.Process("CMS2")
 from Configuration.EventContent.EventContent_cff import *
 
 process.configurationMetadata = cms.untracked.PSet(
-        version = cms.untracked.string('$Revision: 1.1 $'),
+        version = cms.untracked.string('$Revision: 1.2 $'),
         annotation = cms.untracked.string('CMS2'),
         name = cms.untracked.string('CMS2 test configuration')
 )
 
 # load event level configurations
 process.load("Configuration.StandardSequences.Services_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi")
 process.load("TrackingTools.TrackAssociator.DetIdAssociatorESProducer_cff")
-process.GlobalTag.globaltag = "MC_3XY_V26::All"
 
-# load event level configurations
-process.load("CMS2.NtupleMaker.cms2CoreSequences_cff")
-process.load("CMS2.NtupleMaker.cms2GENSequence_cff")
-process.load("CMS2.NtupleMaker.cms2PATSequence_cff")
-process.load('CMS2.NtupleMaker.pixelDigiMaker_cfi')
-process.load('CMS2.NtupleMaker.beamHaloSequence_cff')
+process.load("RecoJets.Configuration.RecoJPTJets_cff")
+process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
+process.load('Configuration/EventContent/EventContent_cff')
+
+process.GlobalTag.globaltag = "START3X_V26::All"
 
 process.options = cms.untracked.PSet(
     Rethrow = cms.untracked.vstring('ProductNotFound')
@@ -32,7 +31,7 @@ process.options = cms.untracked.PSet(
 
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = ''
-process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
 
 
 
@@ -50,31 +49,33 @@ from PhysicsTools.PatAlgos.tools.muonTools import *
 addMuonUserIsolation.isolationTypes = ['All']
 addMuonUserIsolation.toolCode(process)
 
-
 #change JetID tag
 from PhysicsTools.PatAlgos.tools.jetTools import *
+from PhysicsTools.PatAlgos.tools.cmsswVersionTools import *
+run36xOn35xInput(process)
 addJetID( process, cms.InputTag('prunedUncorrectedCMS2Jets'), "antikt5" )
-switchJetCollection(process, 
+switchJetCollection35X(process, 
                     cms.InputTag('prunedUncorrectedCMS2Jets'),   
                     doJTA            = True,            
                     doBTagging       = True,            
-                    jetCorrLabel     = ('AK5','Calo'),  
+                    jetCorrLabel     = None,
                     doType1MET       = True,
                     genJetCollection = cms.InputTag("cms2antikt5GenJets"),
                     doJetID          = True,
-                    jetIdLabel       = "antikt5"
+                    jetIdLabel       = "cms2ak5"
                     )
 
 # add statement to prevent the PAT from using generator information
-from PhysicsTools.PatAlgos.tools.coreTools import *
-switchJECSet(process,"Summer09_7TeV")
+#from PhysicsTools.PatAlgos.tools.coreTools import *
+#uncomment for data
+#removeMCMatching(process, ['All'])
 
 #-----------------------------------------------------------
 # configure input data files and number of event to process
 #-----------------------------------------------------------
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10)
+    input = cms.untracked.int32(100)
 )
 process.options = cms.untracked.PSet(
     Rethrow = cms.untracked.vstring('ProductNotFound')
@@ -83,54 +84,63 @@ process.options = cms.untracked.PSet(
 process.source = cms.Source("PoolSource",
     skipEvents = cms.untracked.uint32(0),
     fileNames = cms.untracked.vstring(
-       '/store/mc/Spring10/Wmunu/RECO/START3X_V26_S09_preproduction-v2/0008/FA3CF9BB-433F-DF11-BF87-0015178C48FC.root'
+    'file:/store/disk00/jribnik/Spring10_TTbarJets-madgraph_GEN-SIM-RECO_START3X_V26_S09-v1_0005_2AA58B20-AD46-DF11-9274-003048C69032.root'
     ),
 )
-#Should help to increase I/O performance from Castor
+
+
+process.out = cms.OutputModule(
+        "PoolOutputModule",
+        verbose = cms.untracked.bool(True),
+        dropMetaData = cms.untracked.string("NONE"),
+        fileName = cms.untracked.string('ntuple_temp.root')
+)
+
+process.out.outputCommands = cms.untracked.vstring( 'drop *' )
+process.out.outputCommands.extend(cms.untracked.vstring('keep *_*Maker*_*_CMS2*'))
+
+
+# load event level configurations
+process.load("CMS2.NtupleMaker.cms2CoreSequences_cff")
+process.load("CMS2.NtupleMaker.cms2GENSequence_cff")
+process.load("CMS2.NtupleMaker.cms2PATSequence_cff")
+process.load('CMS2.NtupleMaker.pixelDigiMaker_cfi')
+process.load('CMS2.NtupleMaker.beamHaloSequence_cff')
+
+
+# loosen thresholds on collections
+process.hypDilepMaker.TightLepton_PtCut=cms.double(10.0)
+process.hypDilepMaker.LooseLepton_PtCut=cms.double(10.0)
+
+process.RandomNumberGeneratorService.randomConeIsoMaker = cms.PSet( engineName = cms.untracked.string('HepJamesRandom'), 
+        initialSeedSet = cms.untracked.vuint32(4126))
+
+#-------------------------------------------------
+# process paths;
+#-------------------------------------------------
+process.cms2WithEverything             = cms.Sequence( process.cms2CoreSequence
+                                                       * process.cms2GENSequence
+                                                       * process.patDefaultSequence
+                                                       * process.cms2PATSequence)
+
+#since filtering is done in the last step, there is no reason to remove these paths
+#just comment out/remove an output which is not needed
+#process.pWithRecoLepton = cms.Path(process.cms2WithEverything * process.aSkimFilter   )
+process.eventMaker.datasetName = cms.string("")
+process.eventMaker.CMS2tag     = cms.string("")
+
+#stuff to speed up I/O from castor
 process.AdaptorConfig = cms.Service("AdaptorConfig",
                                   stats = cms.untracked.bool(True),
                                   enable = cms.untracked.bool(True),
                                   cacheHint = cms.untracked.string("lazy-download"),
                                   readHint = cms.untracked.string("auto-detect")
                                   )
+
 process.source.noEventSort = cms.untracked.bool(True)
 
-process.out = cms.OutputModule(
-        "PoolOutputModule",
-        verbose = cms.untracked.bool(True),
-        dropMetaData = cms.untracked.string("NONE"),
-        fileName = cms.untracked.string('ntuple.root')
-)
-process.out.outputCommands = cms.untracked.vstring( 'drop *' )
-process.out.outputCommands.extend(cms.untracked.vstring('keep *_*Maker*_*_CMS2*'))
 
 
-
-# don't forget 8e29
-process.hltMakerSequence += process.hlt8e29Maker
-process.l1Maker.fillL1Particles = cms.untracked.bool(False)
-##
-
-
-#Random number generator for the random cone maker
-process.RandomNumberGeneratorService.randomConeIsoMaker = cms.PSet( engineName = cms.untracked.string('HepJamesRandom'), 
-        initialSeedSet = cms.untracked.vuint32(4126))
-
-
-#-------------------------------------------------
-# process paths;
-#-------------------------------------------------
-process.cms2WithEverything             = cms.Sequence( process.coreCMS2Sequence
-                                                       * process.cms2GENSequence
-                                                       * process.cms2beamHaloSequence
-                                                       * process.pixelDigiMaker
-                                                       * process.patDefaultSequence * process.cms2PATSequence)
-
-#since filtering is done in the last step, there is no reason to remove these paths
-#just comment out/remove an output which is not needed
-# loosen thresholds on collections
-process.hypDilepMaker.TightLepton_PtCut=cms.double(5)
-process.hypDilepMaker.LooseLepton_PtCut=cms.double(5)
 process.p = cms.Path(process.cms2WithEverything)
 process.outpath         = cms.EndPath(process.out)
 
