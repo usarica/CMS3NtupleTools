1d24b7aed0231169866b85ed4027223686a17f21
jribnik <jribnik>
2010-05-06 20:37:32 +0000

    36X jpt that works no thanks to Sal

--- python/jptSequence_cff.py
+++ python/jptSequence_cff.py
@@ -1,9 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
+from RecoJets.JetAssociationProducers.trackExtrapolator_cfi import *
+from RecoJets.JetAssociationProducers.ak5JTA_cff import *
+ak5JetTracksAssociatorAtVertex.jets = cms.InputTag("prunedUncorrectedCMS2Jets")
+ak5JetTracksAssociatorAtCaloFace.jets = cms.InputTag("prunedUncorrectedCMS2Jets")
+ak5JetExtender.jets = cms.InputTag("prunedUncorrectedCMS2Jets")
+
 from RecoJets.JetPlusTracks.JetPlusTrackCorrections_cff import *
+JetPlusTrackZSPCorJetAntiKt5.src = cms.InputTag("prunedUncorrectedCMS2Jets")
 
 from CMS2.NtupleMaker.jptMaker_cfi import jptMaker
 
-JetPlusTrackZSPCorJetAntiKt5.src = cms.InputTag("prunedUncorrectedCMS2Jets")
-
-JPTCorrections = cms.Sequence(JetPlusTrackCorrectionsAntiKt5 * jptMaker)
+JPTCorrections = cms.Sequence(trackExtrapolator * ak5JTA * JetPlusTrackCorrectionsAntiKt5 * jptMaker)
