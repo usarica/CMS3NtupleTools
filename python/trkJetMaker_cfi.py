8ca8893f3bae570820a83b47f464be885397b863
fgolf <fgolf>
2010-05-06 14:50:50 +0000

    modiified to run on pruned track jet collection

--- python/trkJetMaker_cfi.py
+++ python/trkJetMaker_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from JetMETCorrections.Configuration.DefaultJEC_cff import *
 
 trkJetMaker = cms.EDProducer("TrkJetMaker", 
-                             trkJetsInputTag = cms.InputTag('ak5TrackJets'),
+                             trkJetsInputTag = cms.InputTag('prunedUncorrectedCMS2TrackJets'),
                              trkJetPtCut     = cms.double(3.0),
                              trkJetCorrectionL2L3 = cms.string("ak5TrackL2L3")
 )
--- python/trkJetSequence_cfi.py
+++ python/trkJetSequence_cfi.py
@@ -1,7 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-from RecoJets.JetProducers.ak5TrackJets_cfi import ak5TrackJets
-from RecoJets.JetProducers.TracksForJets_cff import *
-from CommonTools.RecoAlgos.TrackWithVertexRefSelector_cfi import *
+from CMS2.NtupleMaker.trackJetCollectionPruner_cfi import *
+from CMS2.NtupleMaker.trkJetMaker_cfi import *
 
-cms2TrkJetSequence = cms.Sequence(trackWithVertexRefSelector*trackRefsForJets*ak5TrackJets)
+cms2TrkJetSequence = cms.Sequence(prunedUncorrectedCMS2TrackJets * trkJetMaker)
