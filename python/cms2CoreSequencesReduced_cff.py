15bc52a44d940e8dc22518bee58332307c02761d
warren <warren>
2010-05-10 22:58:01 +0000

    Reduced sequence for met template work. Contains all jets and met, removes tracks, gen, misc stuff. cfg contains jetphoton filter.

---
+++ python/cms2CoreSequencesReduced_cff.py
@@ -0,0 +1,46 @@
+#Contains a reduced set of the core CMS2 makers for met template studies. Does not contain Gen or PAT makers
+import FWCore.ParameterSet.Config as cms
+
+#from CMS2.NtupleMaker.aSkimFilter_cfi import *
+from CMS2.NtupleMaker.beamSpotMaker_cfi import *
+from CMS2.NtupleMaker.beamHaloSequence_cff import *
+#from CMS2.NtupleMaker.bTaggingSequence_cfi import *
+#from CMS2.NtupleMaker.bTaggingTrkSequence_cfi import *
+#from CMS2.NtupleMaker.bTagMaker_cfi import *
+#from CMS2.NtupleMaker.bTagTrkMaker_cfi import *
+from CMS2.NtupleMaker.electronMaker_cfi import *
+from CMS2.NtupleMaker.electronSequence_cfi import *
+from CMS2.NtupleMaker.eventMaker_cfi import *
+from CMS2.NtupleMaker.eventSelectionMaker_cfi import *
+from CMS2.NtupleMaker.hltMaker_cff import *
+from CMS2.NtupleMaker.jetSequence_cff import *
+from CMS2.NtupleMaker.jetMaker_cfi import *
+from CMS2.NtupleMaker.jetToElAssMaker_cfi import *
+from CMS2.NtupleMaker.jetToMuAssMaker_cfi import *
+from CMS2.NtupleMaker.jptSequence_cff import *
+from CMS2.NtupleMaker.jptMaker_cfi import *
+from CMS2.NtupleMaker.jptToCaloJetAssMaker_cfi import *
+from CMS2.NtupleMaker.l1Maker_cfi import *
+from CMS2.NtupleMaker.metSequence_cff import *
+from CMS2.NtupleMaker.metMaker_cfi import *
+from CMS2.NtupleMaker.muonMaker_cfi import *
+from CMS2.NtupleMaker.pfJetMaker_cfi import *
+from CMS2.NtupleMaker.pfmetMaker_cfi import *
+from CMS2.NtupleMaker.photonMaker_cfi import *
+from CMS2.NtupleMaker.scMaker_cfi import *
+from CMS2.NtupleMaker.tcmetSequence_cff import *
+from CMS2.NtupleMaker.trkJetMaker_cfi import *
+from CMS2.NtupleMaker.trkJetSequence_cfi import *
+
+
+CMS2Reco      = cms.Sequence(egammaElectronIDCMS2 * cms2CaloJetSequence * metCorSequence * cms2TrkJetSequence * cms2beamHaloSequence)
+
+eventmakers   = cms.Sequence(beamSpotMaker * eventMaker * eventSelectionMaker * cms2beamHaloSequence)
+
+trigmakers   = cms.Sequence(l1Maker * hltMakerSequence)
+
+makers        = cms.Sequence(muonMaker * scMaker * electronMaker * photonMaker * jetMaker  * JPTCorrections * metMaker * tcmetSequence)
+
+pflowmakers   = cms.Sequence(pfmetMaker * pfJetMaker )
+
+cms2CoreSequence = cms.Sequence(CMS2Reco * eventmakers * trigmakers * makers * pflowmakers)
