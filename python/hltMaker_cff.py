8ff25f0a55a11b267667c6547cd480f6ef4390d7
kalavase <kalavase>
2010-05-10 22:39:44 +0000

    added all hlt trigger menus, 1e31, 8e29 and spring10 specific one

--- python/hltMaker_cff.py
+++ python/hltMaker_cff.py
@@ -1,4 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
 from CMS2.NtupleMaker.hltMaker_cfi import *
-hltMakerSequence = cms.Sequence(hltMaker)
+hltMakerSequence = cms.Sequence(hltMaker * hlt8e29Maker * hlt1E31Maker)
+
--- python/hltMaker_cfi.py
+++ python/hltMaker_cfi.py
@@ -1,7 +1,13 @@
 import FWCore.ParameterSet.Config as cms
 
+#There are three different HLT trigger menus in the Spring10 samples,
+#REDIGI, HLT and HLT8e29. The appropriate one is REDIGI, and this will be
+#the default although we will run all 3 paths
+
+
 hlt8e29Maker = cms.EDProducer("HLTMaker",
     processName = cms.untracked.string("HLT8E29"),
+    aliasPrefix = cms.untracked.string("hlt8e29"),                       
     fillTriggerObjects = cms.untracked.bool(True),
     prunedTriggerNames = cms.untracked.vstring(
         # wildcards
@@ -9,6 +15,8 @@ hlt8e29Maker = cms.EDProducer("HLTMaker",
         "*Ele*",
         # jets
         "*Jet*",
+         #met	
+        "*met*",
         "HLT_Jet50U",
         "HLT_DiJetAve15U",
         "HLT_DiJetAve30U",
@@ -42,8 +50,9 @@ hlt8e29Maker = cms.EDProducer("HLTMaker",
     )
 )
 
-hltMaker = cms.EDProducer("HLTMaker",
+hlt1E31Maker = cms.EDProducer("HLTMaker",
     processName = cms.untracked.string("HLT"),
+    aliasPrefix = cms.untracked.string("hlt1e31"),
     fillTriggerObjects = cms.untracked.bool(True),
     prunedTriggerNames = cms.untracked.vstring(
         # wildcards
@@ -51,6 +60,59 @@ hltMaker = cms.EDProducer("HLTMaker",
         "*Ele*",
         # jets
         "*Jet*",
+        #met	
+        "*met*",
+        "HLT_Jet110",
+        "HLT_DiJetAve15U",
+        "HLT_DiJetAve30U",
+        "HLT_DiJetAve50U",
+        "HLT_DiJetAve70U",
+        "HLT_DiJetAve130U",
+        "HLT_FwdJet40",
+        "HLT_QuadJet30",
+        # met
+        "HLT_MET60",
+        "HLT_MET100",
+        # ht
+        "HLT_HT200",
+        "HLT_HT300_MHT100",
+        # muons
+        #"HLT_L1Mu30",
+        #"HLT_L2Mu11",
+        #"HLT_Mu9",
+        #"HLT_Mu15",
+        #"HLT_DoubleMu0",
+        #"HLT_DoubleMu3",
+        #"HLT_IsoMu9",
+        # electrons
+        #"HLT_Ele15_SW_LooseTrackIso_L1R",
+        #"HLT_Ele15_SW_EleId_L1R",
+        #"HLT_Ele20_SW_L1R",
+        #"HLT_DoubleEle10_SW_L1R",
+        # photons
+        "HLT_Photon25_L1R",
+        "HLT_DoublePhoton15_L1R",
+        "HLT_DoublePhoton15_VeryLooseEcalIso_L1R",
+        # taus
+        "HLT_SingleIsoTau30_Trk5",
+        "HLT_DoubleLooseIsoTau15_Trk5",
+    )
+)
+
+
+
+hltMaker = cms.EDProducer("HLTMaker",
+    processName = cms.untracked.string("REDIGI"),
+    aliasPrefix = cms.untracked.string("hlt"),                       
+    fillTriggerObjects = cms.untracked.bool(True),
+    prunedTriggerNames = cms.untracked.vstring(
+        # wildcards
+        "*Mu*",
+        "*Ele*",
+        # jets
+        "*Jet*",
+        #met	
+        "*met*",
         "HLT_Jet110",
         "HLT_DiJetAve15U",
         "HLT_DiJetAve30U",
