a0626334cc954418871e0f0992bca76e3c25470f
kalavase <kalavase>
2010-05-28 00:37:04 +0000

    removed pt cut since its applied in the jet collection pruner

--- python/jptMaker_cfi.py
+++ python/jptMaker_cfi.py
@@ -5,6 +5,5 @@ from JetMETCorrections.Configuration.DefaultJEC_cff import *
 jptMaker = cms.EDFilter("JPTMaker",
 	aliasPrefix = cms.untracked.string("jpts"),
         jptInputTag = cms.InputTag("prunedUncorrectedCMS2Jets", "jpt"),
-        minUncorPt  = cms.double("5."),
         JPTCorrectorL2L3 = cms.string("ak5JPTL2L3")                        
 )
