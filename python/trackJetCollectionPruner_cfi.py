a0e182dd40f9bf2512c6607c842ec4b55ffab58d
kalavase <kalavase>
2010-05-28 00:19:33 +0000

    don\'t need this anymore

--- python/trackJetCollectionPruner_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-prunedUncorrectedCMS2TrackJets = cms.EDFilter("TrackJetCollectionPruner",
-   inputUncorrectedJetCollection = cms.InputTag("ak5TrackJets"),
-   uncorrectedJetPtCut           = cms.double(3.0) ##cut on the UNCORRECTED reco jets!!!!!
-)
