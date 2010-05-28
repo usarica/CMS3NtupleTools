5dd3f83ca07db1c59b461893d14bb7c56fbd36c6
kalavase <kalavase>
2010-05-28 00:37:46 +0000

    changed input tag name

--- python/pfJetMaker_cfi.py
+++ python/pfJetMaker_cfi.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 pfJetMaker = cms.EDFilter("PFJetMaker",
-             pfJetsInputTag     = cms.InputTag("ak5PFJets"),
+             pfJetsInputTag     = cms.InputTag("prunedUncorrectedCMS2Jets", "pfjet"),
              pfJetPtCut         = cms.double(5.),
              PFJetCorrectorL2L3 = cms.string("ak5PFL2L3")
 )
--- python/trkJetMaker_cfi.py
+++ python/trkJetMaker_cfi.py
@@ -3,8 +3,7 @@ import FWCore.ParameterSet.Config as cms
 from JetMETCorrections.Configuration.DefaultJEC_cff import *
 
 trkJetMaker = cms.EDProducer("TrkJetMaker", 
-                             trkJetsInputTag = cms.InputTag('prunedUncorrectedCMS2TrackJets'),
-                             trkJetPtCut     = cms.double(3.0),
+                             trkJetsInputTag = cms.InputTag('prunedUncorrectedCMS2Jets','trkjet'),                             
                              trkJetCorrectionL2L3 = cms.string("ak5TrackL2L3")
 )
 
