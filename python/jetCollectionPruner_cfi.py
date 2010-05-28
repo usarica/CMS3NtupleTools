1aaed2aecd8e3ac5cfedcf3c69841f68de9c25cd
kalavase <kalavase>
2010-05-28 00:35:31 +0000

    update the pt cuts and the input collections for jets

--- python/jetCollectionPruner_cfi.py
+++ python/jetCollectionPruner_cfi.py
@@ -3,10 +3,14 @@ import FWCore.ParameterSet.Config as cms
 from RecoJets.JetProducers.ak5JetID_cfi import ak5JetID
 
 prunedUncorrectedCMS2Jets = cms.EDFilter("JetCollectionPruner",
-   inputUncorrectedJetCollection = cms.InputTag("JetPlusTrackZSPCorJetAntiKt5"),
-   uncorrectedJetPtCut           = cms.double(8.0), ##cut on uncorrected JPT jets!!!!!
+                                         inputUncorrectedJPTJetCollection = cms.InputTag("JetPlusTrackZSPCorJetAntiKt5"),
+                                         inputUncorrectedPFJetCollection  = cms.InputTag("ak5PFJets"),
+                                         inputUncorrectedTrkJetCollection = cms.InputTag("ak5TrackJets"),
+                                         uncorrectedJPTJetPtCut           = cms.double(8.0), ##cut on uncorrected JPT jets!!!!!
+                                         uncorrectedTrkJetPtCut           = cms.double(5.0),                                                   
+                                         uncorrectedPFJetPtCut            = cms.double(5.0),
 )
-
+                                         
 
 cms2ak5JetID = ak5JetID.clone()
 cms2ak5JetID.src = cms.InputTag("prunedUncorrectedCMS2Jets", "calojet")
