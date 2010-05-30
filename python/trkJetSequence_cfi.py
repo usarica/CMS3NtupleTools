12a7c12cb3fbe8526ed02306630be41b5f07c5ee
kalavase <kalavase>
2010-05-30 12:30:15 +0000

    don\'t need this anymore

--- python/trkJetSequence_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from CMS2.NtupleMaker.trackJetCollectionPruner_cfi import *
-from CMS2.NtupleMaker.trkJetMaker_cfi import *
-
-cms2TrkJetSequence = cms.Sequence(prunedUncorrectedCMS2TrackJets * trkJetMaker)
