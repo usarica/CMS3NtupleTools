cfababb1c4aca721878d3db17dfe88269e71f3f8
kalavase <kalavase>
2010-05-31 07:54:05 +0000

    Do not need this anymore as the collections are aligned

--- python/jptToCaloJetAssMaker_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-jptToCaloJetAssMaker = cms.EDFilter("JPTtoCaloJetAssMaker",
-                               cms2CaloJetInputTag  = cms.InputTag("jetMaker", "jetsp4" ),
-                               jptInputTag = cms.InputTag("jptMaker", "jptsp4")
-)
