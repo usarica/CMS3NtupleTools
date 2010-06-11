f18834a1f7755fb1b1b92edf334905a9ab82742b
fgolf <fgolf>
2010-06-11 17:23:40 +0000

    bug fix for mis-spelled branch

--- interface/PFElectronMaker.h
+++ interface/PFElectronMaker.h
@@ -30,20 +30,22 @@
 
 class PFElectronMaker : public edm::EDProducer {
 public:
-  explicit PFElectronMaker (const edm::ParameterSet&);
-  ~PFElectronMaker();
+     explicit PFElectronMaker (const edm::ParameterSet&);
+     ~PFElectronMaker();
 
 private:
 //  virtual void beginJob() ;
-  virtual void beginJob() ;
-  virtual void beginRun(edm::Run&, const edm::EventSetup&) ;
-  virtual void produce(edm::Event&, const edm::EventSetup&);
-  virtual void endJob() ;
+     virtual void beginJob() ;
+     virtual void beginRun(edm::Run&, const edm::EventSetup&) ;
+     virtual void produce(edm::Event&, const edm::EventSetup&);
+     virtual void endJob() ;
     
-  // ----------member data ---------------------------
-  edm::InputTag pfCandidatesTag_;
-  
-
+     // ----------member data ---------------------------
+     edm::InputTag pfCandidatesTag_;
+     edm::InputTag isoc_vm_tag_;
+     edm::InputTag ison_vm_tag_;
+     edm::InputTag isop_vm_tag_;
+     edm::InputTag pfAllElectrons_tag_;  
 };
 
 #endif
