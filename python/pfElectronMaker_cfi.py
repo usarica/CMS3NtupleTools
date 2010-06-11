0794b70c17782389bfb2f4148ba1925cab3268f5
fgolf <fgolf>
2010-06-11 17:25:16 +0000

    added isolation for particle flow electrons

--- python/pfElectronMaker_cfi.py
+++ python/pfElectronMaker_cfi.py
@@ -1,5 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
 pfElectronMaker = cms.EDFilter("PFElectronMaker",
-                               pfCandidatesTag = cms.InputTag("particleFlow","electrons")
+                               pfCandidatesTag     = cms.InputTag("particleFlow","electrons"),
+                               isoc_vm_tag         = cms.InputTag("CMS2isoValElectronWithCharged"),                                  
+                               ison_vm_tag         = cms.InputTag("CMS2isoValElectronWithNeutral"),       
+                               isop_vm_tag         = cms.InputTag("CMS2isoValElectronWithPhotons"),       
+                               pfAllElectrons_tag  = cms.InputTag("CMS2pfAllElectrons")
 )
