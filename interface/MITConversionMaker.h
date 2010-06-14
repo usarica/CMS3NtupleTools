44e5b075700464c6b8c313fbd6f9b841ab6bb6af
kalavase <kalavase>
2010-06-14 11:56:09 +0000

    header for MIT conversion stff

---
+++ interface/MITConversionMaker.h
@@ -0,0 +1,56 @@
+// -*- C++ -*-
+//
+// Package:    NtupleMaker
+// Class:      MITConversionMaker
+// 
+/**\class MITConversionMaker MITConversionMaker.cc CMS2/NtupleMaker/src/MITConversionMaker.cc
+
+   Description: <one line class summary>
+
+   Implementation:
+   <Notes on implementation>
+*/
+//
+// Original Author:  Puneeth Kalavase
+//         Created:  Thu Jun  10 11:07:38 CDT 2010
+// $Id: MITConversionMaker.h,v 1.2 2010/06/14 11:56:09 kalavase Exp $
+//
+//
+#ifndef NTUPLEMAKER_FLAVORHISTORYPRODUCER_H
+#define NTUPLEMAKER_FLAVORHISTORYPRODUCER_H
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+
+//
+// class decleration
+//
+
+class MITConversionMaker : public edm::EDProducer {
+public:
+  explicit MITConversionMaker (const edm::ParameterSet&);
+  ~MITConversionMaker();
+
+private:
+  virtual void beginJob() ;
+  virtual void produce(edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
+
+
+  // ----------member data ---------------------------
+  edm::InputTag elsInputTag_;
+  edm::InputTag mitConversionsTag_;
+  edm::InputTag ctfTrksInputTag_;
+  
+};
+
+#endif
+
