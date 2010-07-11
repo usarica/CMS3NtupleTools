639e09720772af4809a2db70bde2dc72b3b1e3ef
slava77 <slava77>
2010-07-11 13:16:37 +0000

    drop calotowers, use configs from V03-04-26

--- test/cfgcms2_data_express.py
+++ test/cfgcms2_data_express.py
@@ -5,13 +5,14 @@ process = cms.Process("CMS2")
 from Configuration.EventContent.EventContent_cff import *
 
 process.configurationMetadata = cms.untracked.PSet(
-        version = cms.untracked.string('$Revision: 1.1 $'),
+        version = cms.untracked.string('$Revision: 1.2 $'),
         annotation = cms.untracked.string('CMS2'),
         name = cms.untracked.string('CMS2 test configuration')
 )
 
 # load event level configurations
 process.load("Configuration.StandardSequences.Services_cff")
+process.load("Configuration.StandardSequences.Reconstruction_cff")
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
@@ -24,7 +25,9 @@ process.load('Configuration/EventContent/EventContent_cff')
 
 #process.GlobalTag.globaltag = "GR09_R_35_V2B::All"
 # changed at 16:45 Apr 7
-process.GlobalTag.globaltag = "GR10_E_V5::All"
+#process.GlobalTag.globaltag = "GR10_E_V6::All"
+# changed for runs >=138725 Jun 27
+process.GlobalTag.globaltag = "GR10_E_V7::All"
 
 process.options = cms.untracked.PSet(
     Rethrow = cms.untracked.vstring('ProductNotFound')
@@ -57,23 +60,26 @@ addMuonUserIsolation.toolCode(process)
 #change JetID tag
 from PhysicsTools.PatAlgos.tools.jetTools import *
 from PhysicsTools.PatAlgos.tools.cmsswVersionTools import *
-run36xOn35xInput(process)
-addJetID( process, cms.InputTag('prunedUncorrectedCMS2Jets'), "antikt5" )
-switchJetCollection(process, 
-                    cms.InputTag('prunedUncorrectedCMS2Jets'),   
-                    doJTA            = True,            
-                    doBTagging       = True,            
-                    jetCorrLabel     = ('AK5','Calo'),  
-                    doType1MET       = True,
-                    genJetCollection = cms.InputTag("cms2antikt5GenJets"),
-                    doJetID          = True,
-                    jetIdLabel       = "cms2ak5"
-                    )
+#run36xOn35xInput(process)
+addJetID( process, cms.InputTag('prunedUncorrectedCMS2Jets', "calojet"), "antikt5" )
+switchJetCollection35X(process, 
+                       cms.InputTag('prunedUncorrectedCMS2Jets', "calojet"),   
+                       doJTA            = True,            
+                       doBTagging       = True,            
+                       jetCorrLabel     = ('AK5','Calo'),  
+                       doType1MET       = True,
+                       genJetCollection = cms.InputTag("cms2antikt5GenJets"),
+                       doJetID          = True,
+                       jetIdLabel       = "cms2ak5"
+                       )
 
 # add statement to prevent the PAT from using generator information
 from PhysicsTools.PatAlgos.tools.coreTools import *
 #uncomment for data
 removeMCMatching(process, ['All'])
+
+from JetMETCorrections.Type1MET.MetType1Corrections_cff import *
+metJESCorAK5CaloJet.inputUncorJetsLabel = cms.string("ak5CaloJets")
 
 #-----------------------------------------------------------
 # configure input data files and number of event to process
@@ -100,7 +106,13 @@ process.source = cms.Source("PoolSource",
 # load event level configurations
 process.load("CMS2.NtupleMaker.cms2CoreSequences_cff")
 process.load("CMS2.NtupleMaker.cms2PATSequence_cff")
-process.load("CMS2.NtupleMaker.caloTowerSequence_cff")
+process.load("CMS2.NtupleMaker.cms2EcalCleaningSequence_cff")
+process.load("CMS2.NtupleMaker.cms2HFCleaningSequence_cff")
+process.load("CMS2.NtupleMaker.cms2HcalCleaningSequence_cff")
+#process.load("CMS2.NtupleMaker.sdFilter_cfi")
+process.load("CMS2.NtupleMaker.cms2PFSequence_cff")
+
+#process.filter = cms.Path(process.sdFilter)
 
 process.hltMaker.processName = cms.untracked.string("HLT")
 process.hltMakerSequence = cms.Sequence(process.hltMaker)
@@ -110,10 +122,10 @@ process.hypDilepMaker.TightLepton_PtCut = cms.double(0)
 process.hypDilepMaker.LooseLepton_PtCut = cms.double(0)
 process.hypDilepMaker.useSTAMuon = cms.bool(True)
 process.eventMaker.isData      = cms.bool(True)
-process.eventMaker.datasetName = cms.string("/MinimumBias/BeamCommissioning09-RecoTracks-Mar3rdSkim_v2/RAW-RECO")
-process.eventMaker.CMS2tag     = cms.string("blah")
+process.eventMaker.datasetName = cms.string("/ExpressPhysics/Run2010A-Express-v4/FEVT")
+process.eventMaker.CMS2tag     = cms.string("V01-04-26-01")
 process.l1Maker.fillL1Particles = cms.untracked.bool(True)
-process.l1Maker.l1ParticlesProcessName = cms.untracked.string('EXPRESS')
+process.l1Maker.l1ParticlesProcessName = cms.untracked.string('RECO')
 
 process.RandomNumberGeneratorService.randomConeIsoMaker = cms.PSet( engineName = cms.untracked.string('HepJamesRandom'), 
         initialSeedSet = cms.untracked.vuint32(4126))
@@ -122,10 +134,13 @@ process.RandomNumberGeneratorService.randomConeIsoMaker = cms.PSet( engineName =
 # process paths;
 #-------------------------------------------------
 process.cms2WithEverythingExceptGEN  = cms.Sequence(   process.cms2CoreSequence
-                                                     * process.cms2CaloTowerSequence  
-                                                     * process.patDefaultSequence
-                                                     * process.cms2PATSequence
-                                                     * process.randomConeIsoMaker  )
+                                                       * process.patDefaultSequence
+                                                       * process.cms2PATSequence
+                                                       * process.cms2PFNoTauSequence
+                                                       * process.cms2ECALcleaningSequence
+                                                       * process.cms2HCALcleaningSequence
+                                                       * process.cms2HFcleaningSequence
+                                                       )
 
 #since filtering is done in the last step, there is no reason to remove these paths
 #just comment out/remove an output which is not needed
@@ -146,6 +161,7 @@ process.outNoFilter_CMS2 = cms.OutputModule(
 )
 process.outNoFilter_CMS2.outputCommands = cms.untracked.vstring( 'drop *' )
 process.outNoFilter_CMS2.outputCommands.extend(cms.untracked.vstring('keep *_*Maker*_*_CMS2*'))
+process.outNoFilter_CMS2.outputCommands.extend(cms.untracked.vstring('drop *_cms2towerMaker*_*_CMS2*'))
 
 process.outpath         = cms.EndPath(process.outNoFilter_CMS2)
 
