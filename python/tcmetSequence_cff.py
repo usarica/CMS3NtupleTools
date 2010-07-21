563369e2a0e5675e14be128b2c62e4e3341ba221
fgolf <fgolf>
2010-07-21 12:26:27 +0000

    sequence to calculate tcMET using PF clusters

---
+++ python/tcmetSequence_cff.py
@@ -0,0 +1,19 @@
+import FWCore.ParameterSet.Config as cms
+
+from RecoMET.METProducers.TCMET_cfi import *
+from RecoMET.METAlgorithms.MuonTCMETValueMapProducer_cff import *
+from CMS2.NtupleMaker.tcmetMaker_cfi import *
+
+muonTCMETValueMapProducerNew = muonTCMETValueMapProducer.clone()
+muonTCMETValueMapProducerNew.muonMinValidStaHits = cms.int32(0)
+
+tcMetNew = tcMet.clone()
+tcMetNew.tcmetDepValueMap = cms.InputTag("muonTCMETValueMapProducerNew", "muCorrData")
+tcMetNew.usePFClusters = cms.bool(True)
+
+tcmetMakerNew = tcmetMaker.clone()
+tcmetMakerNew.aliasPrefix = cms.untracked.string("evtnew")
+tcmetMakerNew.tcmet_tag_ = cms.InputTag("tcMetNew")
+tcmetMakerNew.tcmet_vm_tag_ = cms.InputTag("muonTCMETValueMapProducerNew", "muCorrData")
+
+tcmetSequence = cms.Sequence(muonTCMETValueMapProducerNew * tcMetNew * tcmetMakerNew)
