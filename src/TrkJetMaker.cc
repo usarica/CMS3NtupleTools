806dac4929cb8add3845c7d0865d09319b8ff01c
fgolf <fgolf>
2010-08-11 18:41:40 +0000

    added branches for the number of tracks in each trkjet and the primary vertex it is associated to

--- src/TrkJetMaker.cc
+++ src/TrkJetMaker.cc
@@ -5,13 +5,13 @@
 //
 /**\class TrkJetMaker TrkJetMaker.cc CMS2/NtupleMaker/src/TrkJetMaker.cc
 
-Description: Dumps the TrkJet contents into the ntuple
-Implementation:
+   Description: Dumps the TrkJet contents into the ntuple
+   Implementation:
 */
 //
 // Original Author:  Sanjay Padhi
 //         Created:  Mon Jun 23 03:57:47 CEST 2008
-// $Id: TrkJetMaker.cc,v 1.8 2010/05/28 00:30:22 kalavase Exp $
+// $Id: TrkJetMaker.cc,v 1.9 2010/08/11 18:41:40 fgolf Exp $
 //
 
 
@@ -35,10 +35,13 @@ Implementation:
 
 #include "JetMETCorrections/Objects/interface/JetCorrector.h"
 
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
 typedef math::XYZTLorentzVectorF LorentzVector;
 
 bool sortTrkJetsByPt(LorentzVector jet1, LorentzVector jet2) {
-  return jet1.pt() > jet2.pt();
+	 return jet1.pt() > jet2.pt();
 }
 
 //
@@ -46,14 +49,16 @@ bool sortTrkJetsByPt(LorentzVector jet1, LorentzVector jet2) {
 //
 TrkJetMaker::TrkJetMaker(const edm::ParameterSet& iConfig)
 {
-  // product of this EDProducer
-  produces<unsigned int>                ("evtntrkjets" ).setBranchAlias("evt_ntrkjets"  ); // number of jets
-  produces<std::vector<LorentzVector> > ("trkjetsp4"   ).setBranchAlias("trkjets_p4"    ); // p4 of the jet
-  produces<std::vector<float> >         ("trkjetscor"  ).setBranchAlias("trkjets_cor"   );             
+	 // product of this EDProducer
+	 produces<unsigned int>						("evtntrkjets"			).setBranchAlias("evt_ntrkjets"			);
+	 produces<std::vector<LorentzVector> >		("trkjetsp4"			).setBranchAlias("trkjets_p4"			);
+	 produces<std::vector<float> >				("trkjetscor"			).setBranchAlias("trkjets_cor"			);             
+	 produces<std::vector<int> >				("trkjetsntrks"			).setBranchAlias("trkjets_ntrks"		);	 
+	 produces<std::vector<int> >				("trkjetsvtxsidx"		).setBranchAlias("trkjets_vtxs_idx"		);
 
-  // parameters from configuration
-  trkJetsInputTag       = iConfig.getParameter<edm::InputTag>("trkJetsInputTag");
-  trkJetCorrectionL2L3_ = iConfig.getParameter<std::string>  ("trkJetCorrectionL2L3");
+	 // parameters from configuration
+	 trkJetsInputTag       = iConfig.getParameter<edm::InputTag>("trkJetsInputTag");
+	 trkJetCorrectionL2L3_ = iConfig.getParameter<std::string>  ("trkJetCorrectionL2L3");
 
 }
 
@@ -66,32 +71,57 @@ TrkJetMaker::~TrkJetMaker()
 void
 TrkJetMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
-  // get TrkJet collection
-  edm::Handle<edm::View<reco::TrackJet> > trkJets;
-  iEvent.getByLabel(trkJetsInputTag, trkJets);
-  
-  // create containers
-  std::auto_ptr<unsigned int>                evt_ntrkjets       (new unsigned int(trkJets->size())  );
-  std::auto_ptr<std::vector<LorentzVector> > vector_trkjets_p4  (new std::vector<LorentzVector>     );
-  std::auto_ptr<std::vector<float> >         vector_trkjets_cor (new std::vector<float>             );
-  
-  const JetCorrector* correctorL2L3 = JetCorrector::getJetCorrector (trkJetCorrectionL2L3_, iSetup);
-  
-  // loop over jets and fill containers
-  edm::View<reco::TrackJet>::const_iterator jetsEnd = trkJets->end();
-  for ( edm::View<reco::TrackJet>::const_iterator jet = trkJets->begin(); jet != jetsEnd; ++jet) {
+	 // get TrkJet collection
+	 edm::Handle<edm::View<reco::TrackJet> > trkJets;
+	 iEvent.getByLabel(trkJetsInputTag, trkJets);
+	 
+	 edm::Handle<std::vector<LorentzVector> > vtxs_h;
+	 iEvent.getByLabel("vtxsposition", vtxs_h);
 
-    double cor = correctorL2L3->correction(jet->p4());
-    vector_trkjets_cor ->push_back( cor                        );
-    vector_trkjets_p4  ->push_back( LorentzVector( jet->p4() ) );
-  }
+	 // create containers
+	 std::auto_ptr<unsigned int>                evt_ntrkjets			(new unsigned int(trkJets->size())		);
+	 std::auto_ptr<std::vector<LorentzVector> > vector_trkjets_p4		(new std::vector<LorentzVector>			);
+	 std::auto_ptr<std::vector<float> >         vector_trkjets_cor		(new std::vector<float>					);
+	 std::auto_ptr<std::vector<int> >			vector_trkjets_ntrks	(new std::vector<int>					);
+	 std::auto_ptr<std::vector<int> >			vector_trkjets_vtxs_idx	(new std::vector<int>					);	 
 
-  std::sort( vector_trkjets_p4->begin(), vector_trkjets_p4->end(), sortTrkJetsByPt );
+	 const JetCorrector* correctorL2L3 = JetCorrector::getJetCorrector (trkJetCorrectionL2L3_, iSetup);
+  
+	 // loop over jets and fill containers
+	 edm::View<reco::TrackJet>::const_iterator jetsEnd = trkJets->end();
+	 for ( edm::View<reco::TrackJet>::const_iterator jet = trkJets->begin(); jet != jetsEnd; ++jet) {
 
-  // put containers into event
-  iEvent.put(evt_ntrkjets,       "evtntrkjets"  );
-  iEvent.put(vector_trkjets_p4,  "trkjetsp4"    );
-  iEvent.put(vector_trkjets_cor, "trkjetscor"   );
+		  double cor = correctorL2L3	->correction(jet->p4()					);
+		  vector_trkjets_cor			->push_back( cor                        );
+		  vector_trkjets_p4				->push_back( LorentzVector( jet->p4() ) );
+		  vector_trkjets_ntrks			->push_back( jet->numberOfTracks()		);
+
+		  reco::VertexRef refvtx = jet->primaryVertex();
+		  float trkjets_vtx_x = (*refvtx).x();
+		  float trkjets_vtx_y = (*refvtx).y();
+
+		  int vtx_idx = -1;
+		  int nVtxs = vtxs_h->size();
+		  for (int i_vtx = 0; i_vtx < nVtxs; i_vtx++)
+		  {
+			   float vtxs_x = vtxs_h->at(i_vtx).x();
+			   float vtxs_y = vtxs_h->at(i_vtx).y();
+
+			   if (fabs(trkjets_vtx_x - vtxs_x) < 0.001 && fabs(trkjets_vtx_y - vtxs_y) < 0.001)
+					vtx_idx = i_vtx;
+		  }
+
+		  vector_trkjets_vtxs_idx->push_back(vtx_idx);
+	 }
+
+//	 std::sort( vector_trkjets_p4->begin(), vector_trkjets_p4->end(), sortTrkJetsByPt );
+
+	 // put containers into event
+	 iEvent.put(evt_ntrkjets			, "evtntrkjets"			);
+	 iEvent.put(vector_trkjets_p4		,  "trkjetsp4"			);
+	 iEvent.put(vector_trkjets_cor		, "trkjetscor"			);
+	 iEvent.put(vector_trkjets_ntrks	, "trkjetsntrks"		);
+	 iEvent.put(vector_trkjets_vtxs_idx	, "trkjetsvtxsidx"		);
 
 }
 
