91322d106280033ebdb8ae90b342a3fdab5debef
dbarge <dbarge>
2010-10-07 19:03:23 +0000

    EDProducer -> EDFilter

--- python/aSkimFilter_cfi.py
+++ python/aSkimFilter_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-aSkimFilter = cms.EDProducer("ASkimFilter",
+aSkimFilter = cms.EDFilter("ASkimFilter",
                            filterExpressions = cms.VInputTag(cms.InputTag("electronMaker","elsp4"),
                                                              cms.InputTag("muonMaker","musp4")),
                            filterPtCut  = cms.double(10.0)
--- python/monolepGenFilter_cfi.py
+++ python/monolepGenFilter_cfi.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 
-monolepGenFilter = cms.EDProducer("LepGenFilter",
+monolepGenFilter = cms.EDFilter("LepGenFilter",
 	nGenLepsRequired = cms.int32(1)
 )
