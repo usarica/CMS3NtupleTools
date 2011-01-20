b3128e1c0e1a6d671ea508574a954bbf297ef7c6
fgolf <fgolf>
2011-01-20 22:09:25 +0000

    need to run towerMakerWithHO and metHO on our own in 3_9 and 3_10 because they were inadvertently dropped; won't have to do this from 3_11 onward when it actually matters

--- python/metSequence_cff.py
+++ python/metSequence_cff.py
@@ -1,8 +1,9 @@
 import FWCore.ParameterSet.Config as cms
 
-
 from JetMETCorrections.Type1MET.MetType1Corrections_cff import metJESCorIC5CaloJet
 from JetMETCorrections.Configuration.JetCorrectionServices_cff import *
+from RecoJets.JetProducers.CaloTowerSchemeBWithHO_cfi import towerMakerWithHO
+from RecoMET.METProducers.CaloMET_cfi import metHO
 
 metMuonJESCorAK5CMS2                     = metJESCorIC5CaloJet.clone()
 metMuonJESCorAK5CMS2.inputUncorJetsLabel = "ak5CaloJets"
@@ -12,4 +13,5 @@ metMuonJESCorAK5CMS2.inputUncorMetLabel  = "corMetGlobalMuons"
 from CommonTools.RecoAlgos.HBHENoiseFilterResultProducer_cfi import *
 cms2HBHENoiseFilterResultProducer = HBHENoiseFilterResultProducer.clone()
 
-metCorSequence = cms.Sequence(metMuonJESCorAK5CMS2 * cms2HBHENoiseFilterResultProducer)
+#metCorSequence = cms.Sequence(metMuonJESCorAK5CMS2 * cms2HBHENoiseFilterResultProducer)
+metCorSequence = cms.Sequence(metMuonJESCorAK5CMS2 * towerMakerWithHO * metHO * cms2HBHENoiseFilterResultProducer)
