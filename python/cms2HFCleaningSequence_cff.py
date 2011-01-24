9ed0c463dac9159a4fa33d3ae20a7fe39a4df3f9
dbarge <dbarge>
2011-01-24 19:45:01 +0000

    Updated cms2HFCleaningSequence_cff.py, cms2HcalCleaningSequence_cff.py

--- python/cms2HFCleaningSequence_cff.py
+++ python/cms2HFCleaningSequence_cff.py
@@ -4,43 +4,43 @@ isMC = False
 version = 10
 
 
-# HF RecHit reflagger -- specify type of HF cleaning to use
-from JetMETAnalysis.HcalReflagging.HFrechitreflaggerJETMET_cff import *
-if version==1:
-    cms2hfrecoReflagged = HFrechitreflaggerJETMETv1.clone()
-elif version==2:
-    cms2hfrecoReflagged = HFrechitreflaggerJETMETv2.clone()
-elif version==3:
-    cms2hfrecoReflagged = HFrechitreflaggerJETMETv3.clone()
-elif version==4: 
-    if (isMC==False):
-        cms2hfrecoReflagged = HFrechitreflaggerJETMETv4.clone()
-    else:
-        cms2hfrecoReflagged = HFrechitreflaggerJETMETv2.clone()  
-elif version==5:
-    if (isMC==False):
-        cms2hfrecoReflagged = HFrechitreflaggerJETMETv5.clone()
-    else:
-        cms2hfrecoReflagged = HFrechitreflaggerJETMETv3.clone()  
+## HF RecHit reflagger -- specify type of HF cleaning to use
+#from JetMETAnalysis.HcalReflagging.HFrechitreflaggerJETMET_cff import *
+#if version==1:
+#    cms2hfrecoReflagged = HFrechitreflaggerJETMETv1.clone()
+#elif version==2:
+#    cms2hfrecoReflagged = HFrechitreflaggerJETMETv2.clone()
+#elif version==3:
+#    cms2hfrecoReflagged = HFrechitreflaggerJETMETv3.clone()
+#elif version==4: 
+#    if (isMC==False):
+#        cms2hfrecoReflagged = HFrechitreflaggerJETMETv4.clone()
+#    else:
+#        cms2hfrecoReflagged = HFrechitreflaggerJETMETv2.clone()  
+#elif version==5:
+#    if (isMC==False):
+#        cms2hfrecoReflagged = HFrechitreflaggerJETMETv5.clone()
+#    else:
+#        cms2hfrecoReflagged = HFrechitreflaggerJETMETv3.clone()  
+#
+## CURRENT RECOMMENDATION
+#elif version==10:
+#    cms2hfrecoReflagged = HFrechitreflaggerJETMETv10.clone()
+#    if (isMC==False):  # V10 cleaning uses results of prior flags when setting new flags; this is the current recommendation as of 21 July 2010 
+#        cms2hfrecoReflagged.PETstat.flagsToSkip =string.atoi('10',2)
+#        cms2hfrecoReflagged.S8S1stat.flagsToSkip=string.atoi('10010',2)
+#        cms2hfrecoReflagged.S9S1stat.flagsToSkip=string.atoi('11010',2)
+#        # Flag ordering
+#        cms2hfrecoReflagged.FlagsToSet=(4,3,0)  # set flag 4 (HFPET -- also sets HFLongShort), then flag 3 (HFS8S1 -- also sets HFLongShort), then flag 0 (HFLongShort -- set directly via S9S1)
 
-# CURRENT RECOMMENDATION
-elif version==10:
-    cms2hfrecoReflagged = HFrechitreflaggerJETMETv10.clone()
-    if (isMC==False):  # V10 cleaning uses results of prior flags when setting new flags; this is the current recommendation as of 21 July 2010 
-        cms2hfrecoReflagged.PETstat.flagsToSkip =string.atoi('10',2)
-        cms2hfrecoReflagged.S8S1stat.flagsToSkip=string.atoi('10010',2)
-        cms2hfrecoReflagged.S9S1stat.flagsToSkip=string.atoi('11010',2)
-        # Flag ordering
-        cms2hfrecoReflagged.FlagsToSet=(4,3,0)  # set flag 4 (HFPET -- also sets HFLongShort), then flag 3 (HFS8S1 -- also sets HFLongShort), then flag 0 (HFLongShort -- set directly via S9S1)
 
 
-
-import JetMETAnalysis.HcalReflagging.RemoveAddSevLevel as cms2RemoveAddSevLevel
+#import JetMETAnalysis.HcalReflagging.RemoveAddSevLevel as cms2RemoveAddSevLevel
 from RecoLocalCalo.HcalRecAlgos.hcalRecAlgoESProd_cfi import *
-hcalRecAlgos=cms2RemoveAddSevLevel.AddFlag(hcalRecAlgos,"HFLongShort",11)
+#hcalRecAlgos=cms2RemoveAddSevLevel.AddFlag(hcalRecAlgos,"HFLongShort",11)
 
-if (isMC==False):  # Don't use HFDigiTime on MC !
-    hcalRecAlgos=cms2RemoveAddSevLevel.AddFlag(hcalRecAlgos,"HFDigiTime",11)
+#if (isMC==False):  # Don't use HFDigiTime on MC !
+#    hcalRecAlgos=cms2RemoveAddSevLevel.AddFlag(hcalRecAlgos,"HFDigiTime",11)
 
 
 
@@ -60,5 +60,6 @@ deltaMETMakerHF = deltaMETMaker.clone()
 deltaMETMakerHF.aliasPrefix = cms.untracked.string("evthf")
 deltaMETMakerHF.metInputTag_ = cms.InputTag("cms2metHF")
 
-cms2HFcleaningSequence   = cms.Sequence(cms2hfrecoReflagged * cms2towerMakerHF * cms2metHF * deltaMETMakerHF)
+#cms2HFcleaningSequence   = cms.Sequence(cms2hfrecoReflagged * cms2towerMakerHF * cms2metHF * deltaMETMakerHF)
+cms2HFcleaningSequence   = cms.Sequence(cms2towerMakerHF * cms2metHF * deltaMETMakerHF)
 
--- python/cms2HcalCleaningSequence_cff.py
+++ python/cms2HcalCleaningSequence_cff.py
@@ -1,16 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 # New SeverityLevelComputer that forces RecHits with UserDefinedBit0 set to be excluded from new rechit collection
-import JetMETAnalysis.HcalReflagging.RemoveAddSevLevel as cms2RemoveAddSevLevel
+#import JetMETAnalysis.HcalReflagging.RemoveAddSevLevel as cms2RemoveAddSevLevel
 from RecoLocalCalo.HcalRecAlgos.hcalRecAlgoESProd_cfi import *
-from JetMETAnalysis.HcalReflagging.hbherechitreflaggerJETMET_cfi import *
+#from JetMETAnalysis.HcalReflagging.hbherechitreflaggerJETMET_cfi import *
 
 
-hcalRecAlgos= cms2RemoveAddSevLevel.AddFlag(hcalRecAlgos,"UserDefinedBit0",10)
+#hcalRecAlgos= cms2RemoveAddSevLevel.AddFlag(hcalRecAlgos,"UserDefinedBit0",10)
 
 # HBHE RecHit reflagger
-cms2hbherecoReflagged = hbherechitreflaggerJETMET.clone()
-cms2hbherecoReflagged.debug=0
+#cms2hbherecoReflagged = hbherechitreflaggerJETMET.clone()
+#cms2hbherecoReflagged.debug=0
 
 
 # Use the reflagged HF RecHits to make the CaloTowers
@@ -28,4 +28,5 @@ deltaMETMakerHCAL.aliasPrefix = cms.untracked.string("evthcal")
 deltaMETMakerHCAL.metInputTag_ = cms.InputTag("cms2metHCAL")
 
 
-cms2HCALcleaningSequence = cms.Sequence(cms2hbherecoReflagged * cms2towerMakerHCAL * cms2metHCAL * deltaMETMakerHCAL)
+#cms2HCALcleaningSequence = cms.Sequence(cms2hbherecoReflagged * cms2towerMakerHCAL * cms2metHCAL * deltaMETMakerHCAL)
+cms2HCALcleaningSequence = cms.Sequence(cms2towerMakerHCAL * cms2metHCAL * deltaMETMakerHCAL)
