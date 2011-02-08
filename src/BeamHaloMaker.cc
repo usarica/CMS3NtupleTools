017beb83d99192bf0672809a08d7c81ca48a340e
kalavase <kalavase>
2011-02-08 18:36:16 +0000

    Fixed in order to run on AOD

--- src/BeamHaloMaker.cc
+++ src/BeamHaloMaker.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Puneeth Kalavase
 //         Created:  Fri Jun  6 11:07:38 CDT 2008
-// $Id: BeamHaloMaker.cc,v 1.6 2010/05/26 12:09:44 fgolf Exp $
+// $Id: BeamHaloMaker.cc,v 1.7 2011/02/08 18:36:16 kalavase Exp $
 //
 //
 
@@ -103,25 +103,43 @@ void BeamHaloMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
      edm::Handle<reco::CSCHaloData> cscHalo_h;
      iEvent.getByLabel(cscHaloInputTag, cscHalo_h);
 
-     *evt_nHaloTriggerCandidates = cscHalo_h->NumberOfHaloTriggers();
-     *evt_nHaloLikeTracks        = cscHalo_h->NumberOfHaloTracks();
-
+     *evt_nHaloTriggerCandidates = cscHalo_h.isValid() ? cscHalo_h->NumberOfHaloTriggers() : -9999;
+     *evt_nHaloLikeTracks        = cscHalo_h.isValid() ? cscHalo_h->NumberOfHaloTracks() : -9999;
+     
      *evt_eventHasHalo           = (*evt_nHaloTriggerCandidates && *evt_nHaloLikeTracks);
 
-     *evt_ecalLooseHaloId        = beamHalo_h->EcalLooseHaloId()      ;
-     *evt_ecalTightHaloId        = beamHalo_h->EcalTightHaloId()      ;
-     *evt_hcalLooseHaloId        = beamHalo_h->HcalLooseHaloId()      ;
-     *evt_hcalTightHaloId        = beamHalo_h->HcalTightHaloId()      ;
-     *evt_cscLooseHaloId         = beamHalo_h->CSCLooseHaloId()       ;
-     *evt_cscTightHaloId         = beamHalo_h->CSCTightHaloId()       ;
-     *evt_globalLooseHaloId      = beamHalo_h->GlobalLooseHaloId()    ;
-     *evt_globalTightHaloId      = beamHalo_h->GlobalTightHaloId()    ;
-     *evt_looseHaloId            = beamHalo_h->LooseId()              ;
-     *evt_tightHaloId            = beamHalo_h->TightId()              ;
-     *evt_extremeTightHaloId     = beamHalo_h->ExtremeTightId()       ;
-     *evt_ecaliPhiSuspects       = beamHalo_h->GetEcaliPhiSuspects()  ;
-     *evt_hcaliPhiSuspects       = beamHalo_h->GetHcaliPhiSuspects()  ;
-     *evt_globaliPhiSuspects     = beamHalo_h->GetGlobaliPhiSuspects();
+     if(beamHalo_h.isValid()) {
+       *evt_ecalLooseHaloId        = beamHalo_h->EcalLooseHaloId()      ;
+       *evt_ecalTightHaloId        = beamHalo_h->EcalTightHaloId()      ;
+       *evt_hcalLooseHaloId        = beamHalo_h->HcalLooseHaloId()      ;
+       *evt_hcalTightHaloId        = beamHalo_h->HcalTightHaloId()      ;
+       *evt_cscLooseHaloId         = beamHalo_h->CSCLooseHaloId()       ;
+       *evt_cscTightHaloId         = beamHalo_h->CSCTightHaloId()       ;
+       *evt_globalLooseHaloId      = beamHalo_h->GlobalLooseHaloId()    ;
+       *evt_globalTightHaloId      = beamHalo_h->GlobalTightHaloId()    ;
+       *evt_looseHaloId            = beamHalo_h->LooseId()              ;
+       *evt_tightHaloId            = beamHalo_h->TightId()              ;
+       *evt_extremeTightHaloId     = beamHalo_h->ExtremeTightId()       ;
+       *evt_ecaliPhiSuspects       = beamHalo_h->GetEcaliPhiSuspects()  ;
+       *evt_hcaliPhiSuspects       = beamHalo_h->GetHcaliPhiSuspects()  ;
+       *evt_globaliPhiSuspects     = beamHalo_h->GetGlobaliPhiSuspects();
+     } else {
+       *evt_ecalLooseHaloId        = -9999;
+       *evt_ecalTightHaloId        = -9999;
+       *evt_hcalLooseHaloId        = -9999;
+       *evt_hcalTightHaloId        = -9999;
+       *evt_cscLooseHaloId         = -9999;
+       *evt_cscTightHaloId         = -9999;
+       *evt_globalLooseHaloId      = -9999;
+       *evt_globalTightHaloId      = -9999;
+       *evt_looseHaloId            = -9999;
+       *evt_tightHaloId            = -9999;
+       *evt_extremeTightHaloId     = -9999;
+       vector<int> temp;
+       *evt_ecaliPhiSuspects        = temp;
+       *evt_hcaliPhiSuspects        = temp;
+       *evt_globaliPhiSuspects      = temp;
+     }
   
      iEvent.put(evt_eventHasHalo, "evteventHasHalo");
      iEvent.put(evt_nHaloLikeTracks, "evtnHaloLikeTracks");
