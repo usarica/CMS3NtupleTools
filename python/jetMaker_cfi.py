9ee94b6f5734a492bad36ca23a0462887e84848e
kalavase <kalavase>
2011-02-08 22:23:58 +0000

    updated to run on AOD

--- python/jetMaker_cfi.py
+++ python/jetMaker_cfi.py
@@ -4,10 +4,9 @@ from JetMETCorrections.Configuration.DefaultJEC_cff import *
 
 jetMaker = cms.EDProducer("JetMaker",
            uncorJetsInputTag     = cms.InputTag("prunedUncorrectedCMS2Jets", "calojet"),
+           AliasPrefix           = cms.string("jets"),
+           CaloJetCorrectorL2L3 = cms.string('ak5CaloL2L3')
            runningOnReco         = cms.untracked.bool(True),
-           AliasPrefix          = cms.string("jets"),
-           jetIDInputTag = cms.InputTag("cms2ak5JetID"),
-           CaloJetCorrectorL2L3 = cms.string('ak5CaloL2L3'),
            CaloJetCorrectorL1L2L3 = cms.string('ak5CaloL1L2L3'),
            CaloJetCorrectorL1FastL2L3 = cms.string('ak5CaloL1FastL2L3')
 )
