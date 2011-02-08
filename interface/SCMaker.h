14504dbae852902106a1e263ebe9ada7148ead49
kalavase <kalavase>
2011-02-08 22:02:33 +0000

    *** empty log message ***

--- interface/SCMaker.h
+++ interface/SCMaker.h
@@ -5,11 +5,11 @@
 // 
 /**\class SCMaker SCMaker.cc CMS2/NtupleMaker/src/SCMaker.cc
 
-Description: <one line class summary>
+   Description: <one line class summary>
 
-Implementation:
-<Notes on implementation>
- */
+   Implementation:
+   <Notes on implementation>
+*/
 //
 //
 #ifndef CMS2_SCMAKER_H
@@ -43,57 +43,59 @@ Implementation:
 //
 
 class SCMaker : public edm::EDProducer {
-	public:
-		explicit SCMaker (const edm::ParameterSet&);
+public:
+  explicit SCMaker (const edm::ParameterSet&);
 
-	private:
-		virtual void beginRun(edm::Run&, const edm::EventSetup&);
-		virtual void beginJob() ;
-		virtual void produce(edm::Event&, const edm::EventSetup&);
-		virtual void endJob() ;
+private:
+  virtual void beginRun(edm::Run&, const edm::EventSetup&);
+  virtual void beginJob() ;
+  virtual void produce(edm::Event&, const edm::EventSetup&);
+  virtual void endJob() ;
 
-		// used by mc debugging
-        	void closestMCParticle(const HepMC::GenEvent *genEvent, const reco::SuperCluster &sc,
-                                      double &dRClosest, double &energyClosest);
-	        float ecalEta(float EtaParticle , float Zvertex, float plane_Radius);
-		//
+  // used by mc debugging
+  void closestMCParticle(const HepMC::GenEvent *genEvent, const reco::SuperCluster &sc,
+			 double &dRClosest, double &energyClosest);
+  float ecalEta(float EtaParticle , float Zvertex, float plane_Radius);
+  //
 
-		math::XYZTLorentzVectorF initP4(const math::XYZPoint &pvPos,                 
-				const reco::SuperCluster &sc);
+  math::XYZTLorentzVectorF initP4(const math::XYZPoint &pvPos,                 
+				  const reco::SuperCluster &sc);
 
-		// ----------member data ---------------------------
+  // ----------member data ---------------------------
 
-		// mc debugging
-		bool debug_;
-	        edm::InputTag MCTruthCollection_;
+  // mc debugging
+  bool debug_;
+  edm::InputTag MCTruthCollection_;
 
-		// preselection cuts
-		double scEtMin_;
+  // preselection cuts
+  double scEtMin_;
 
-		// supercluster input collections
-		edm::InputTag scInputTag_EE_;
-		edm::InputTag scInputTag_EB_;
-		std::vector<edm::InputTag> scInputTags_;
-		std::vector<edm::InputTag> hitInputTags_;
+  // supercluster input collections
+  edm::InputTag scInputTag_EE_;
+  edm::InputTag scInputTag_EB_;
+  std::vector<edm::InputTag> scInputTags_;
+  std::vector<edm::InputTag> hitInputTags_;
 
-		// rechit input collections
-		edm::InputTag hcalRecHitsInputTag_HBHE_;
-		edm::InputTag ecalRecHitsInputTag_EE_;
-		edm::InputTag ecalRecHitsInputTag_EB_;
+  // rechit input collections
+  edm::InputTag hcalRecHitsInputTag_HBHE_;
+  edm::InputTag ecalRecHitsInputTag_EE_;
+  edm::InputTag ecalRecHitsInputTag_EB_;
 
-		// primary vertex collection
-		edm::InputTag primaryVertexInputTag_;
+  // primary vertex collection
+  edm::InputTag primaryVertexInputTag_;
 
-		// electrons
-		edm::InputTag electronsInputTag_;
+  // electrons
+  edm::InputTag electronsInputTag_;
 
-		// access to geometry
-		unsigned long long cachedCaloGeometryID_;
-		edm::ESHandle<CaloGeometry> caloGeometry_;
-		const   EcalChannelStatus *channelStatus_;
+  // access to geometry
+  unsigned long long cachedCaloGeometryID_;
+  edm::ESHandle<CaloGeometry> caloGeometry_;
+  const   EcalChannelStatus *channelStatus_;
+
+  EcalClusterLazyTools* clusterTools_;
 
 
-	std::string aliasprefix_;
+  std::string aliasprefix_;
 };
 
 #endif
