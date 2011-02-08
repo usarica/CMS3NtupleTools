ba205d1131d24c13b0e991993aa1dd21c801c473
kalavase <kalavase>
2011-02-08 22:04:31 +0000

    update for AOD

--- src/JetCollectionPruner.cc
+++ src/JetCollectionPruner.cc
@@ -24,7 +24,6 @@
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "DataFormats/JetReco/interface/CaloJet.h"
 #include "DataFormats/JetReco/interface/CaloJetCollection.h"
 #include "DataFormats/JetReco/interface/JPTJet.h"
 #include "DataFormats/JetReco/interface/JPTJetCollection.h"
@@ -34,6 +33,7 @@
 #include "DataFormats/JetReco/interface/TrackJetCollection.h"
 #include "CMS2/NtupleMaker/interface/JetCollectionPruner.h"
 #include "JetMETCorrections/Objects/interface/JetCorrector.h"
+
 
 bool sortJPTJetsByPt(reco::JPTJet jet1, reco::JPTJet jet2) {
   return jet1.pt() > jet2.pt();
@@ -59,11 +59,11 @@ JetCollectionPruner::JetCollectionPruner(const edm::ParameterSet& iConfig)
   produces<reco::CaloJetCollection>	("calojet"	);
   produces<reco::PFJetCollection>	("pfjet"	);
   produces<reco::TrackJetCollection>	("trkjet"	);
-
+  
   // get the input collection of electrons
   inputUncorrectedJPTJetCollection_	= iConfig.getParameter<edm::InputTag>	("inputUncorrectedJPTJetCollection"   	);
   inputUncorrectedPFJetCollection_	= iConfig.getParameter<edm::InputTag>	("inputUncorrectedPFJetCollection" 	);
-  inputUncorrectedTrkJetCollection_     = iConfig.getParameter<edm::InputTag>	("inputUncorrectedTrkJetCollection"	); 
+  inputUncorrectedTrkJetCollection_     = iConfig.getParameter<edm::InputTag>	("inputUncorrectedTrkJetCollection"	);  
   uncorrectedJPTJetPtCut_       	= iConfig.getParameter<double>       	("uncorrectedJPTJetPtCut"            	);
   uncorrectedPFJetPtCut_        	= iConfig.getParameter<double>       	("uncorrectedPFJetPtCut"          	);
   uncorrectedTrkJetPtCut_       	= iConfig.getParameter<double>    	("uncorrectedTrkJetPtCut"		);
@@ -81,15 +81,15 @@ void JetCollectionPruner::produce(edm::Event& iEvent, const edm::EventSetup& iSe
 
   edm::Handle<reco::PFJetCollection>    pfjetsHandle;
   edm::Handle<reco::TrackJetCollection> trkjetsHandle;
-  iEvent.getByLabel(inputUncorrectedPFJetCollection_, pfjetsHandle);
-  iEvent.getByLabel(inputUncorrectedTrkJetCollection_, trkjetsHandle);
-  
-     
+
+  iEvent.getByLabel(inputUncorrectedPFJetCollection_,  pfjetsHandle	);
+  iEvent.getByLabel(inputUncorrectedTrkJetCollection_, trkjetsHandle	);
+    
   // new collection to put in the event after cleaning
-  std::auto_ptr<reco::JPTJetCollection>		outJPTcollection	(new reco::JPTJetCollection);
-  std::auto_ptr<reco::CaloJetCollection>	outJETcollection	(new reco::CaloJetCollection);
-  std::auto_ptr<reco::PFJetCollection>		outPFJetCollection	(new reco::PFJetCollection);
-  std::auto_ptr<reco::TrackJetCollection> 	outTrkJetCollection 	(new reco::TrackJetCollection);
+  std::auto_ptr<reco::JPTJetCollection>		outJPTcollection	(new reco::JPTJetCollection	);
+  std::auto_ptr<reco::CaloJetCollection>	outJETcollection	(new reco::CaloJetCollection	);
+  std::auto_ptr<reco::PFJetCollection>		outPFJetCollection	(new reco::PFJetCollection	);
+  std::auto_ptr<reco::TrackJetCollection> 	outTrkJetCollection 	(new reco::TrackJetCollection	);
 
   for (reco::JPTJetCollection::const_iterator jet_it = jetsHandle->begin(); jet_it != jetsHandle->end(); jet_it++)
     {
@@ -100,12 +100,12 @@ void JetCollectionPruner::produce(edm::Event& iEvent, const edm::EventSetup& iSe
   //sort the jets using the above predicate.
   //they should be sorted, but being paranoid
   std::sort(outJPTcollection->begin(), outJPTcollection->end(), sortJPTJetsByPt);
-
-  for (reco::JPTJetCollection::const_iterator jet_it = outJPTcollection->begin(); jet_it != outJPTcollection->end(); jet_it++)
-    {
+  for (reco::JPTJetCollection::const_iterator jet_it = outJPTcollection->begin(); jet_it != outJPTcollection->end(); jet_it++)   {
+    
       const reco::CaloJet& tmp_calojet = dynamic_cast<const reco::CaloJet&>(*(jet_it->getCaloJetRef()));
       outJETcollection->push_back(tmp_calojet);
-    }
+      
+  }
 
 
   for(reco::PFJetCollection::const_iterator jet_it = pfjetsHandle->begin();
@@ -132,17 +132,16 @@ void JetCollectionPruner::produce(edm::Event& iEvent, const edm::EventSetup& iSe
   iEvent.put(outJETcollection,		"calojet"	);
   iEvent.put(outPFJetCollection,	"pfjet"		);
   iEvent.put(outTrkJetCollection,       "trkjet"        );
+
 }
 
 // ------------ method called once each job just before starting event loop  ------------
-void JetCollectionPruner::beginJob()
-{
-}
+void JetCollectionPruner::beginJob() { }
 
 // ------------ method called once each job just after ending the event loop  ------------
-void JetCollectionPruner::endJob() 
-{
-}
+void JetCollectionPruner::endJob()  { }
+
+
 
 //define this as a plug-in
 DEFINE_FWK_MODULE(JetCollectionPruner);
