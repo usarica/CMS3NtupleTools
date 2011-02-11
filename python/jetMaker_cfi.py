04cafe4bdb167d3fa7699df066ccae3993dc7191
kalavase <kalavase>
2011-02-11 19:10:15 +0000

    upated input tags

--- python/jetMaker_cfi.py
+++ python/jetMaker_cfi.py
@@ -2,11 +2,13 @@ import FWCore.ParameterSet.Config as cms
 
 from JetMETCorrections.Configuration.DefaultJEC_cff import *
 
+
 jetMaker = cms.EDProducer("JetMaker",
            uncorJetsInputTag     = cms.InputTag("prunedUncorrectedCMS2Jets", "calojet"),
-           AliasPrefix           = cms.string("jets"),
-           CaloJetCorrectorL2L3 = cms.string('ak5CaloL2L3')
-           runningOnReco         = cms.untracked.bool(True),
+           unprunedJetsTag       = cms.InputTag("ak5CaloJets"),
+           AliasPrefix          = cms.string("jets"),
+           jetIDInputTag = cms.InputTag("ak5JetID"),
+           CaloJetCorrectorL2L3 = cms.string('ak5CaloL2L3'),
            CaloJetCorrectorL1L2L3 = cms.string('ak5CaloL1L2L3'),
            CaloJetCorrectorL1FastL2L3 = cms.string('ak5CaloL1FastL2L3')
 )
