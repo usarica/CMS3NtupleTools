deb473564f6b1353902b1d499864bacf66b275ad
dbarge <dbarge>
2011-02-15 20:31:20 +0000

    updated for 398

--- test/MCProduction2010_SingleLeptonFilter_cfg.py
+++ test/MCProduction2010_SingleLeptonFilter_cfg.py
@@ -5,7 +5,7 @@ process = cms.Process("CMS2")
 from Configuration.EventContent.EventContent_cff import *
 
 process.configurationMetadata = cms.untracked.PSet(
-        version = cms.untracked.string('$Revision: 1.7 $'),
+        version = cms.untracked.string('$Revision: 1.8 $'),
         annotation = cms.untracked.string('CMS2'),
         name = cms.untracked.string('CMS2 test configuration')
 )
@@ -18,57 +18,30 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi")
 process.load("TrackingTools.TrackAssociator.DetIdAssociatorESProducer_cff")
-
 process.load("RecoJets.Configuration.RecoJPTJets_cff")
 process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
+process.ak5CaloL1Offset.useCondDB = False
+process.ak5CaloL1Fastjet.useCondDB = False
+
+
+##-------------------- Import the Jet RECO modules -----------------------
+process.load('RecoJets.Configuration.RecoPFJets_cff')
+##-------------------- Turn-on the FastJet density calculation -----------------------
+process.kt6PFJets.doRhoFastjet = True
+process.kt6PFJets.Rho_EtaMax = cms.double(4.5)
+##-------------------- Turn-on the FastJet jet area calculation for your favorite algorithm -----------------------
+process.ak5PFJets.doAreaFastjet = True
+process.ak5PFJets.Rho_EtaMax = cms.double(4.5)
+
+
 process.load('Configuration/EventContent/EventContent_cff')
-
-process.GlobalTag.globaltag = "MC_38Y_V9::All"
-
+process.GlobalTag.globaltag = "START39_V8::All"
 process.options = cms.untracked.PSet(
     Rethrow = cms.untracked.vstring('ProductNotFound')
 )
-
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = ''
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
-
-
-
-#-------------------------------------------------
-# PAT configuration
-#-------------------------------------------------
-process.load("PhysicsTools.PatAlgos.patSequences_cff")
-process.patDefaultSequence = cms.Sequence(
-    process.patCandidates *
-    process.selectedPatCandidates
-)
-
-#add muon isolation
-from PhysicsTools.PatAlgos.tools.muonTools import *
-addMuonUserIsolation.isolationTypes = ['All']
-addMuonUserIsolation.toolCode(process)
-
-#change JetID tag
-from PhysicsTools.PatAlgos.tools.jetTools import *
-from PhysicsTools.PatAlgos.tools.cmsswVersionTools import *
-run36xOn35xInput(process)
-addJetID( process, cms.InputTag('prunedUncorrectedCMS2Jets','calojet'), "antikt5" )
-switchJetCollection35X(process, 
-                    cms.InputTag('prunedUncorrectedCMS2Jets','calojet'),   
-                    doJTA            = True,            
-                    doBTagging       = True,            
-                    jetCorrLabel     = ('AK5', 'Calo'),
-                    doType1MET       = True,
-                    genJetCollection = cms.InputTag("cms2antikt5GenJets"),
-                    doJetID          = True,
-                    jetIdLabel       = "cms2ak5"
-                    )
-
-# add statement to prevent the PAT from using generator information
-#from PhysicsTools.PatAlgos.tools.coreTools import *
-#uncomment for data
-#removeMCMatching(process, ['All'])
 
 #
 from JetMETCorrections.Type1MET.MetType1Corrections_cff import *
@@ -79,30 +52,33 @@ metJESCorAK5CaloJet.inputUncorJetsLabel = cms.string("ak5CaloJets")
 #-----------------------------------------------------------
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(10)
 )
 process.options = cms.untracked.PSet(
     Rethrow = cms.untracked.vstring('ProductNotFound')
 )
 
 process.source = cms.Source("PoolSource",
+    dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
     skipEvents = cms.untracked.uint32(0),
     fileNames = cms.untracked.vstring(
-    'file:/store/disk00/jribnik/Spring10_TTbarJets-madgraph_GEN-SIM-RECO_START3X_V26_S09-v1_0005_2AA58B20-AD46-DF11-9274-003048C69032.root'
+      'file:../../../../../F0684341-9A0F-E011-93AF-001BFCDBD1BC.root'
     ),
+    # Uncomment to emulate AOD with RECO
+    #inputCommands = process.AODEventContent.outputCommands,
 )
 
 #single lepton filter
 process.EventSelectionSingleFilt = cms.PSet(
-    SelectEvents = cms.untracked.PSet(
+  SelectEvents = cms.untracked.PSet(
     SelectEvents = cms.vstring('pWithRecoLepton', 'pWithGenLepton')
-                )
-        )
+  )
+)
 
 process.out = cms.OutputModule(
         "PoolOutputModule",
         process.EventSelectionSingleFilt,
-        verbose = cms.untracked.bool(True),
+        #verbose = cms.untracked.bool(True),
         dropMetaData = cms.untracked.string("NONE"),
         fileName = cms.untracked.string('ntuple.root')
 )
@@ -110,11 +86,11 @@ process.out = cms.OutputModule(
 process.out.outputCommands = cms.untracked.vstring( 'drop *' )
 process.out.outputCommands.extend(cms.untracked.vstring('keep *_*Maker*_*_CMS2*'))
 process.out.outputCommands.extend(cms.untracked.vstring('drop *_cms2towerMaker*_*_CMS2*'))
+process.out.outputCommands.extend(cms.untracked.vstring('drop CaloTowers*_*_*_CMS2*'))
 
 # load event level configurations    
 process.load("CMS2.NtupleMaker.cms2CoreSequences_cff")
 process.load("CMS2.NtupleMaker.cms2GENSequence_cff")
-process.load("CMS2.NtupleMaker.cms2PATSequence_cff")
 process.load('CMS2.NtupleMaker.pixelDigiMaker_cfi')
 process.load("CMS2.NtupleMaker.cms2HFCleaningSequence_cff")
 process.load("CMS2.NtupleMaker.cms2HcalCleaningSequence_cff")
@@ -129,13 +105,10 @@ process.hypDilepMaker.LooseLepton_PtCut=cms.double(10.0)
 #-------------------------------------------------
 # process paths;
 #-------------------------------------------------
-process.cms2WithEverything             = cms.Sequence( process.cms2CoreSequence
+process.cms2WithEverything             = cms.Sequence( process.kt6PFJets * process.cms2CoreSequence
                                                        * process.cms2PFNoTauSequence
                                                        * process.cms2GENSequence
-                                                       * process.patDefaultSequence
-                                                       * process.cms2PATSequence
-                                                       * process.cms2HCALcleaningSequence
-                                                       * process.cms2HFcleaningSequence)
+                                                     )
 
 #since filtering is done in the last step, there is no reason to remove these paths
 #just comment out/remove an output which is not needed
