721bc44a9dff4a244dd85258f0d5fcbc7414e564
fgolf <fgolf>
2011-03-14 23:08:53 +0000

    run sequence to produce and store branches for ecal dead cells

---
+++ python/ecalAnomalousEventMaker_cff.py
@@ -0,0 +1,6 @@
+import FWCore.ParameterSet.Config as cms
+
+from PhysicsTools.EcalAnomalousEventFilter.ecalanomalouseventfilter_cfi import EcalAnomalousEventFilter
+from CMS2.NtupleMaker.ecalAnomalousEventMaker_cfi import ecalAnomalousEventMaker
+
+ecalAnomalousEventFilterSequence = cms.Sequence(EcalAnomalousEventFilter * ecalAnomalousEventMaker)
