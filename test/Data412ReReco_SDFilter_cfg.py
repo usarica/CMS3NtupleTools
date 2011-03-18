72cc7ceca294054214ca98d63d70276923768c99
benhoob <benhoob>
2011-03-18 17:41:11 +0000

    Initial commit

---
+++ test/Data412ReReco_SDFilter_cfg.py
@@ -0,0 +1,42 @@
+from CMS2.NtupleMaker.RecoConfiguration2011_cfg import *
+
+# Global Tag
+process.GlobalTag.globaltag = "GR_R_311_V2::All"
+
+# Load Filters
+process.load("CMS2.NtupleMaker.sdFilter_cfi")
+process.filter = cms.Path(process.sdFilter)
+if "DoubleElectron" in str(process.source.fileNames):
+    process.sdFilter.filterName_=cms.string("doubleElectron")
+elif "DoubleMu" in str(process.source.fileNames):
+    process.sdFilter.filterName_=cms.string("doubleMu")
+elif "MuEG" in  str(process.source.fileNames):
+    process.sdFilter.filterName_=cms.string("MuEG")
+elif "SingleMu" in str(process.source.fileNames):
+    process.sdFilter.filterName_=cms.string("SingleMu")
+elif "Photon" in str(process.source.fileNames):
+    process.sdFilter.filterName_=cms.string("Photon")
+else:
+    print 'filterName missing!'
+# Output
+process.out = cms.OutputModule(
+        "PoolOutputModule",
+        SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring('filter') ),
+        fileName     = cms.untracked.string('ntuple.root'),
+        dropMetaData = cms.untracked.string("NONE")
+)
+process.outpath      = cms.EndPath(process.out)
+
+# Branches
+process.out.outputCommands = cms.untracked.vstring( 'drop *' )
+process.out.outputCommands.extend(cms.untracked.vstring('keep *_*Maker*_*_CMS2*'))
+process.out.outputCommands.extend(cms.untracked.vstring('drop *_cms2towerMaker*_*_CMS2*'))
+process.out.outputCommands.extend(cms.untracked.vstring('drop CaloTowers*_*_*_CMS2*'))
+
+#
+process.cms2WithEverything = cms.Sequence( process.sdFilter * process.ak5PFJets * process.kt6PFJets * process.cms2CoreSequence * process.cms2PFNoTauSequence )
+process.p                  = cms.Path( process.cms2WithEverything )
+
+#
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.eventMaker.isData                        = cms.bool(True)
