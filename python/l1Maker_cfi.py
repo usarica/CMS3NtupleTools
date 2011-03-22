b9bc99219bbec0debe789e4143c8a26ec555aa69
fgolf <fgolf>
2011-03-22 16:16:14 +0000

    removed RECO process name to avoid a potential product not found error in the future per dima's recommendation

--- python/l1Maker_cfi.py
+++ python/l1Maker_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 l1Maker = cms.EDProducer("L1Maker",
 	aliasPrefix = cms.untracked.string("l1"),
     fillL1Particles = cms.untracked.bool(True),
-    l1ParticlesProcessName = cms.untracked.string("RECO"),
+    l1ParticlesProcessName = cms.untracked.string(""),
     l1GlobalTriggerReadoutRecordInputTag = cms.InputTag("gtDigis"),
     l1GlobalTriggerRecordInputTag = cms.InputTag("l1GtRecord"),
     l1extraParticlesInputTag             = cms.InputTag("l1extraParticles")                   
