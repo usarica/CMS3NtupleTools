f0ff28958eeb2ac6ee117c0cf85d6ad65cd89361
dmytro <dmytro>
2011-03-23 14:37:39 +0000

    switched ot pfjets for hypothesis block; increased eta to 5.0

--- python/hypDilepMaker_cfi.py
+++ python/hypDilepMaker_cfi.py
@@ -1,10 +1,8 @@
+
 import FWCore.ParameterSet.Config as cms
 
 hypDilepMaker = cms.EDProducer("HypDilepMaker",
 	aliasPrefix = cms.untracked.string("hyp"),
-    #are we using pat jets in the JetMaker?
-    #need to know if the jets were corrected
-    usingCorrectedJets = cms.bool(True),
     # met collection
     tcmetInputTag = cms.InputTag("tcmetMaker"),
     metInputTag = cms.InputTag("metMaker"),
@@ -12,9 +10,9 @@ hypDilepMaker = cms.EDProducer("HypDilepMaker",
     muToGenInputTag = cms.InputTag("muToGenAssMaker"),
     # electrons collection
     electronsInputTag = cms.InputTag("electronMaker"),
-    hypJetMaxEtaCut = cms.double(2.4),
+    hypJetMaxEtaCut = cms.double(5.0),
     #jet collection
-    jetsInputTag = cms.InputTag("jptMaker"),
+    jetsInputTag = cms.InputTag("pfJetMaker","pfjetsp4"),
     hypJetMinPtCut = cms.double(30.0), ##this is a corrected pt cut!
 
     #tight lepton pt cut
