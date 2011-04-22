476c343c82cfd73ddc0df4a421ec41ba0031e3dc
warren <warren>
2011-04-22 01:23:07 +0000

    Filter for MC photons--same as SD filter for photons for data but no trig reqmnts

---
+++ test/MCProduction2011_PhotonFilter_cfg.py
@@ -0,0 +1,41 @@
+from CMS2.NtupleMaker.RecoConfiguration2011_cfg import *
+
+# Global Tag
+process.GlobalTag.globaltag = "MC_311_V2::All"
+
+# Load Filters
+process.load("CMS2.NtupleMaker.sdFilter_cfi")
+process.filter = cms.Path(process.sdFilter)
+
+process.sdFilter.filterName_=cms.string("Photon")
+process.sdFilter.photonJet_dotrig_=cms.bool(False)
+
+# Photon Filter from SDFilter
+process.EventSelectionPhotonFilt = cms.PSet(
+  SelectEvents = cms.untracked.PSet(
+    SelectEvents = cms.vstring('filter')
+  )
+)
+
+# Output
+process.out = cms.OutputModule(
+        "PoolOutputModule",
+        process.EventSelectionPhotonFilt,
+        fileName     = cms.untracked.string('ntuple.root'),
+        dropMetaData = cms.untracked.string("NONE")
+)
+process.outpath      = cms.EndPath(process.out)
+
+# Branches
+process.out.outputCommands = cms.untracked.vstring( 'drop *' )
+process.out.outputCommands.extend(cms.untracked.vstring('keep *_*Maker*_*_CMS2*'))
+process.out.outputCommands.extend(cms.untracked.vstring('drop *_cms2towerMaker*_*_CMS2*'))
+process.out.outputCommands.extend(cms.untracked.vstring('drop CaloTowers*_*_*_CMS2*'))
+
+#
+process.cms2WithEverything = cms.Sequence( process.sdFilter * process.ak5PFJets * process.kt6PFJets * process.cms2CoreSequence * process.cms2PFNoTauSequence * process.cms2GENSequence )
+process.p                  = cms.Path( process.cms2WithEverything )
+
+#
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+process.eventMaker.isData                        = cms.bool(False)
