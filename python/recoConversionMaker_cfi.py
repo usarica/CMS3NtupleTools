e80b7f9c9b4aeca3384b064265ac68ad04e8f6bb
benhoob <benhoob>
2011-04-28 17:49:16 +0000

    Update collection name

--- python/recoConversionMaker_cfi.py
+++ python/recoConversionMaker_cfi.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
 
 recoConversionMaker = cms.EDProducer("RecoConversionMaker",
-                            recoConversionInputTag = cms.InputTag("trackerOnlyConversions"),
-                            beamSpotInputTag       = cms.InputTag("offlineBeamSpot")
+                                     recoConversionInputTag = cms.InputTag("allConversions"),
+                                     beamSpotInputTag       = cms.InputTag("offlineBeamSpot")
 )
 
 
