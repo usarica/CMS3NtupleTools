33057bc76a092fcb92189b847752da539fef46f8
dmytro <dmytro>
2011-05-04 19:52:49 +0000

    ww skim in use

--- test/MC41X_wwfilter_cfg.py
+++ test/MC41X_wwfilter_cfg.py
@@ -1,15 +1,14 @@
 from CMS2.NtupleMaker.RecoConfiguration2011_cfg import *
 
 # Global Tag
-process.GlobalTag.globaltag = "GR_R_311_V2::All"
+process.GlobalTag.globaltag = "MC_311_V2::All"
 
 #
-process.cms2WithEverything = cms.Sequence( process.ak5PFJets * process.kt6PFJets * process.cms2CoreSequence * process.cms2PFNoTauSequence )
+process.cms2WithEverything = cms.Sequence( process.ak5PFJets * process.kt6PFJets * process.cms2CoreSequence * process.cms2PFNoTauSequence * process.cms2GENSequence )
 
 #
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
-process.eventMaker.isData                        = cms.bool(True)
-
+process.eventMaker.isData                        = cms.bool(False)
 
 # Load Filters
 process.load("CMS2.NtupleMaker.wwfilter_cfi")
@@ -22,7 +21,6 @@ process.out = cms.OutputModule(
         SelectEvents = cms.untracked.PSet( SelectEvents = cms.vstring('CMS2_Ele10Mu10_IsoIdMET',
                                                                       'CMS2_Ele10Mu10_IsoId') ),
         fileName     = cms.untracked.string('ntuple.root'),
-        # dropMetaData = cms.untracked.string("NONE")
 )
 process.outpath      = cms.EndPath(process.out)
 
@@ -32,9 +30,3 @@ process.out.outputCommands.extend(cms.untracked.vstring('keep *_*Maker*_*_CMS2*'
 process.out.outputCommands.extend(cms.untracked.vstring('keep *_ele10*_*_CMS2*'))
 process.out.outputCommands.extend(cms.untracked.vstring('drop *_cms2towerMaker*_*_CMS2*'))
 process.out.outputCommands.extend(cms.untracked.vstring('drop CaloTowers*_*_*_CMS2*'))
-
-# process.source = cms.Source("PoolSource",
-#      fileNames = cms.untracked.vstring( 'file:/tas/dmytro/tmp/DYToEE_M-20_CT10_TuneZ2_7TeV-powheg-pythia_Spring11-PU_S1_START311_V1G1-v1_AODSIM_7840115C-5D4F-E011-BD85-001E0B48D9A4.root' )
-# )
-             
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
