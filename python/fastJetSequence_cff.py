0e9f9801fd211330bdd4f7153647a284fd5280e9
kalavase <kalavase>
2011-05-24 15:59:34 +0000

    fixed duplicate fast jet module

--- python/fastJetSequence_cff.py
+++ python/fastJetSequence_cff.py
@@ -3,15 +3,6 @@ import FWCore.ParameterSet.Config as cms
 from RecoJets.JetProducers.kt4PFJets_cfi import *
 from CMS2.NtupleMaker.fastJetMaker_cfi import *
 
-kt6PFJetsDeterministicJEC = kt4PFJets.clone(
-    rParam = 0.6,
-    doAreaFastjet = True,
-    doRhoFastjet = True,
-    voronoiRfact = 0.9,
-    Rho_EtaMax = 2.5,
-    Ghost_EtaMax = 2.5
-)
-
 
 kt6PFJetsDeterministicJEC = kt4PFJets.clone(
     rParam = 0.6,
@@ -27,8 +18,8 @@ kt6PFJetsDeterministicIso = kt4PFJets.clone(
     doAreaFastjet = True,
     doRhoFastjet = True,
     voronoiRfact = 0.9,
-    Rho_EtaMax = 5.0,
-    Ghost_EtaMax = 5.0
+    Rho_EtaMax = 2.5,
+    Ghost_EtaMax = 2.5
 )
 
 
