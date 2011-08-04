1854cd41bd09942ebb7cca1f720986a9339014e7
dbarge <dbarge>
2011-08-04 21:58:42 +0000

    added bool for PFMuonAlgo::isIsolatedMuon

--- src/PFCandidateMaker.cc
+++ src/PFCandidateMaker.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  Puneeth Kalavase
 //         Created:  Fri Jun  6 11:07:38 CDT 2008
-// $Id: PFCandidateMaker.cc,v 1.5 2011/05/24 18:19:41 cerati Exp $
+// $Id: PFCandidateMaker.cc,v 1.6 2011/08/04 21:58:42 dbarge Exp $
 //
 //
 
@@ -29,6 +29,8 @@
 #include "FWCore/Framework/interface/ESHandle.h"
 
 #include "DataFormats/ParticleFlowCandidate/interface/PFCandidate.h"
+#include "DataFormats/ParticleFlowCandidate/interface/PFCandidateFwd.h"
+#include "RecoParticleFlow/PFProducer/interface/PFMuonAlgo.h"
 #include "DataFormats/ParticleFlowReco/interface/PFBlock.h"
 #include "DataFormats/Common/interface/ValueMap.h"
 
@@ -57,76 +59,65 @@ PFCandidateMaker::PFCandidateMaker(const edm::ParameterSet& iConfig) {
      tracksInputTag_            = iConfig.getParameter<InputTag>        ("tracksInputTag");
      minDR_electron_            = iConfig.getParameter<double>          ("minDRelectron");
 
-     produces<vector<LorentzVector>	> ("pfcandsp4"                  ).setBranchAlias("pfcands_p4"			);
-     produces<vector<LorentzVector>	> ("pfcandsposAtEcalp4"		).setBranchAlias("pfcands_posAtEcal_p4"		);
-     produces<vector<float>		> ("pfcandsecalE"		).setBranchAlias("pfcands_ecalE"		);
-     produces<vector<float>		> ("pfcandshcalE"		).setBranchAlias("pfcands_hcalE"		);
-     produces<vector<float>		> ("pfcandsrawEcalE"		).setBranchAlias("pfcands_rawEcalE"		);
-     produces<vector<float>		> ("pfcandsrawHcalE"		).setBranchAlias("pfcands_rawHcalE"		);
-     produces<vector<float>		> ("pfcandspS1E"		).setBranchAlias("pfcands_pS1E"			);
-     produces<vector<float>		> ("pfcandspS2E"		).setBranchAlias("pfcands_pS2E"			);
-     produces<vector<float>		> ("pfcandsdeltaP"		).setBranchAlias("pfcands_deltaP"		);
-     produces<vector<float>		> ("pfcandsmvaepi"		).setBranchAlias("pfcands_mva_epi"		);
-     produces<vector<float>		> ("pfcandsmvaemu"		).setBranchAlias("pfcands_mva_emu"		);
-     produces<vector<float>		> ("pfcandsmvapimu"		).setBranchAlias("pfcands_mva_pimu"		);
-     produces<vector<float>		> ("pfcandsmvanothinggamma"	).setBranchAlias("pfcands_mva_nothing_gamma"	);
-     produces<vector<float>		> ("pfcandsmvanothingnh"	).setBranchAlias("pfcands_mva_nothing_nh"	);
-     produces<vector<int>		> ("pfcandscharge"		).setBranchAlias("pfcands_charge"		);
-     produces<vector<int>		> ("pfcandsparticleId"		).setBranchAlias("pfcands_particleId"		);
-     produces<vector<int>  		> ("pfcandsflag"		).setBranchAlias("pfcands_flag"			);
-     produces<vector<int>  		> ("pfcandstrkidx"		).setBranchAlias("pfcands_trkidx"		);
-     produces<vector<int>  		> ("pfcandspfmusidx"		).setBranchAlias("pfcands_pfmusidx"		);
-     produces<vector<int>  		> ("pfcandspfelsidx"		).setBranchAlias("pfcands_pfelsidx"		);
+     produces<vector<LorentzVector>	> ("pfcandsp4"              ).setBranchAlias("pfcands_p4"			          );
+     produces<vector<LorentzVector>	> ("pfcandsposAtEcalp4"		  ).setBranchAlias("pfcands_posAtEcal_p4"		  );
+     produces<vector<bool> >          ("pfcandisMuIso"          ).setBranchAlias("pfcands_isMuIso"	        );
+     produces<vector<float>	>         ("pfcandsecalE"	  	      ).setBranchAlias("pfcands_ecalE"		        );
+     produces<vector<float>	>         ("pfcandshcalE"		        ).setBranchAlias("pfcands_hcalE"		        );
+     produces<vector<float>	>         ("pfcandsrawEcalE"		    ).setBranchAlias("pfcands_rawEcalE"		      );
+     produces<vector<float>	>         ("pfcandsrawHcalE"		    ).setBranchAlias("pfcands_rawHcalE"		      );
+     produces<vector<float>	>         ("pfcandspS1E"		        ).setBranchAlias("pfcands_pS1E"			        );
+     produces<vector<float>	>         ("pfcandspS2E"		        ).setBranchAlias("pfcands_pS2E"			        );
+     produces<vector<float>	>         ("pfcandsdeltaP"		      ).setBranchAlias("pfcands_deltaP"		        );
+     produces<vector<float>	>         ("pfcandsmvaepi"		      ).setBranchAlias("pfcands_mva_epi"		      );
+     produces<vector<float>	>         ("pfcandsmvaemu"		      ).setBranchAlias("pfcands_mva_emu"		      );
+     produces<vector<float>	>         ("pfcandsmvapimu"		      ).setBranchAlias("pfcands_mva_pimu"		      );
+     produces<vector<float>	>         ("pfcandsmvanothinggamma"	).setBranchAlias("pfcands_mva_nothing_gamma");
+     produces<vector<float>	>         ("pfcandsmvanothingnh"	  ).setBranchAlias("pfcands_mva_nothing_nh"	  );
+     produces<vector<int>	>           ("pfcandscharge"		      ).setBranchAlias("pfcands_charge"		        );
+     produces<vector<int> >           ("pfcandsparticleId"		  ).setBranchAlias("pfcands_particleId"		    );
+     produces<vector<int>	>           ("pfcandsflag"		        ).setBranchAlias("pfcands_flag"			        );
+     produces<vector<int>	>           ("pfcandstrkidx"		      ).setBranchAlias("pfcands_trkidx"		        );
+     produces<vector<int>	>           ("pfcandspfmusidx"		    ).setBranchAlias("pfcands_pfmusidx"		      );
+     produces<vector<int>	>           ("pfcandspfelsidx"		    ).setBranchAlias("pfcands_pfelsidx"		      );
 
-     produces<float>                      ("evtfixgridrhoctr"           ).setBranchAlias("evt_fixgrid_rho_ctr"          );
-     produces<float>                      ("evtfixgridrhofwd"           ).setBranchAlias("evt_fixgrid_rho_fwd"          );
-     produces<float>                      ("evtfixgridrhoall"           ).setBranchAlias("evt_fixgrid_rho_all"          );
+     produces<float>                  ("evtfixgridrhoctr"       ).setBranchAlias("evt_fixgrid_rho_ctr"      );
+     produces<float>                  ("evtfixgridrhofwd"       ).setBranchAlias("evt_fixgrid_rho_fwd"      );
+     produces<float>                  ("evtfixgridrhoall"       ).setBranchAlias("evt_fixgrid_rho_all"      );
 }
 
-PFCandidateMaker::~PFCandidateMaker() {
-
-}
-
-
-void  PFCandidateMaker::beginRun(edm::Run&, const edm::EventSetup& es) {
-     
-
-}
-
-void PFCandidateMaker::beginJob() {
-
-
-}
-
-void PFCandidateMaker::endJob() {
-}
+PFCandidateMaker::~PFCandidateMaker() {}
+void  PFCandidateMaker::beginRun(edm::Run&, const edm::EventSetup& es) {}
+void PFCandidateMaker::beginJob() {}
+void PFCandidateMaker::endJob()   {}
 
 // ------------ method called to produce the data  ------------
 void PFCandidateMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup) {
 
-     auto_ptr<vector<LorentzVector> >	pfcands_p4		   (new vector<LorentzVector>	);
-     auto_ptr<vector<LorentzVector> >	pfcands_posAtEcal_p4	   (new vector<LorentzVector>	);
-     auto_ptr<vector<float> >		pfcands_ecalE		   (new vector<float>		);
-     auto_ptr<vector<float> >		pfcands_hcalE		   (new vector<float>		);
-     auto_ptr<vector<float> >		pfcands_rawEcalE	   (new vector<float>		);
-     auto_ptr<vector<float> >		pfcands_rawHcalE	   (new vector<float>		);
-     auto_ptr<vector<float> >		pfcands_pS1E		   (new vector<float>		);
-     auto_ptr<vector<float> >		pfcands_pS2E		   (new vector<float>		);
-     auto_ptr<vector<float> >		pfcands_deltaP		   (new vector<float>		);
-     auto_ptr<vector<float> >		pfcands_mva_epi		   (new vector<float>		);
-     auto_ptr<vector<float> >		pfcands_mva_emu		   (new vector<float>		);
-     auto_ptr<vector<float> >		pfcands_mva_pimu	   (new vector<float>		);
-     auto_ptr<vector<float> >		pfcands_mva_nothing_gamma  (new vector<float>		);
-     auto_ptr<vector<float> >		pfcands_mva_nothing_nh	   (new vector<float>		);
-     auto_ptr<vector<int> >		pfcands_charge		   (new vector<int>		);
-     auto_ptr<vector<int> >		pfcands_particleId	   (new vector<int>		);
-     auto_ptr<vector<int> >	        pfcands_flag		   (new vector<int>        	);
-     auto_ptr<vector<int> >	        pfcands_trkidx		   (new vector<int>        	);
-     auto_ptr<vector<int> >	        pfcands_pfmusidx	   (new vector<int>        	);    
-     auto_ptr<vector<int> >	        pfcands_pfelsidx	   (new vector<int>        	);
-     auto_ptr<float >	                evt_fixgrid_rho_ctr 	   (new float           	);
-     auto_ptr<float >	                evt_fixgrid_rho_fwd 	   (new float           	);
-     auto_ptr<float >	                evt_fixgrid_rho_all 	   (new float           	);
+     auto_ptr<vector<LorentzVector> >	pfcands_p4		            (new vector<LorentzVector>  );
+     auto_ptr<vector<LorentzVector> >	pfcands_posAtEcal_p4	    (new vector<LorentzVector>	);
+     auto_ptr<vector<bool> >		      pfcands_isMuIso	          (new vector<bool> 		      );
+     auto_ptr<vector<float> >		      pfcands_ecalE		          (new vector<float>		      );
+     auto_ptr<vector<float> >		      pfcands_hcalE		          (new vector<float>		      );
+     auto_ptr<vector<float> >		      pfcands_rawEcalE	        (new vector<float>		      );
+     auto_ptr<vector<float> >		      pfcands_rawHcalE	        (new vector<float>		      );
+     auto_ptr<vector<float> >		      pfcands_pS1E		          (new vector<float>		      );
+     auto_ptr<vector<float> >		      pfcands_pS2E		          (new vector<float>		      );
+     auto_ptr<vector<float> >		      pfcands_deltaP		        (new vector<float>		      );
+     auto_ptr<vector<float> >		      pfcands_mva_epi		        (new vector<float>		      );
+     auto_ptr<vector<float> >		      pfcands_mva_emu		        (new vector<float>		      );
+     auto_ptr<vector<float> >		      pfcands_mva_pimu	        (new vector<float>		      );
+     auto_ptr<vector<float> >		      pfcands_mva_nothing_gamma (new vector<float>		      );
+     auto_ptr<vector<float> >		      pfcands_mva_nothing_nh	  (new vector<float>		      );
+     auto_ptr<vector<int> >		        pfcands_charge		        (new vector<int>		        );
+     auto_ptr<vector<int> >		        pfcands_particleId	      (new vector<int>		        );
+     auto_ptr<vector<int> >	          pfcands_flag		          (new vector<int>        	  );
+     auto_ptr<vector<int> >	          pfcands_trkidx		        (new vector<int>        	  );
+     auto_ptr<vector<int> >	          pfcands_pfmusidx	        (new vector<int>        	  );    
+     auto_ptr<vector<int> >	          pfcands_pfelsidx	        (new vector<int>        	  );
+     auto_ptr<float >	                evt_fixgrid_rho_ctr 	    (new float           	      );
+     auto_ptr<float >	                evt_fixgrid_rho_fwd 	    (new float           	      );
+     auto_ptr<float >	                evt_fixgrid_rho_all 	    (new float           	      );
     
      //get pfcandidates
      Handle<PFCandidateCollection> pfCandidatesHandle;
@@ -144,37 +135,43 @@ void PFCandidateMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup
      iEvent.getByLabel(tracksInputTag_, track_h);
 
      int npfmus = 0;
+  
 
-     for(PFCandidateCollection::const_iterator pf_it = pfCandidates->begin(); pf_it != pfCandidates->end(); pf_it++)
-     {
-	  int pfflags = 0;
-	  for(unsigned int i = 0; i < 17; i++)
-	  {
-	       if(pf_it->flag((PFCandidate::Flags)i))
-		    pfflags |= (1<<i);
-	  }
+     
+       
+      int iCand = 0;
+      for( PFCandidateCollection::const_iterator pf_it = pfCandidates->begin(); pf_it != pfCandidates->end(); pf_it++ ) {
+  
+        //
+	      int pfflags = 0;
+	      for( unsigned int i = 0; i < 17; i++ ) {
+          if(pf_it->flag((PFCandidate::Flags)i)) pfflags |= (1<<i);
+	      }
 
-	  pfcands_p4			->push_back(LorentzVector(pf_it->px(), pf_it->py(), pf_it->pz(), pf_it->p())	);
-	  pfcands_posAtEcal_p4		->push_back(LorentzVector(pf_it->positionAtECALEntrance().x(),
-								  pf_it->positionAtECALEntrance().y(),
-								  pf_it->positionAtECALEntrance().z(),
-								  0.0)							);
+        //
+        bool isMuIso = false;
+        const reco::PFCandidate &cand = (*pfCandidatesHandle)[iCand];
+        if( cand.particleId() == 3 ) isMuIso = PFMuonAlgo::isIsolatedMuon( cand.muonRef() );
+        iCand++;
 
-	  pfcands_ecalE			->push_back( isfinite( pf_it->ecalEnergy() ) ? pf_it->ecalEnergy() : -9999.     );
-	  pfcands_hcalE			->push_back(pf_it->hcalEnergy()							);
-	  pfcands_rawEcalE		->push_back(pf_it->rawEcalEnergy()						);
-	  pfcands_rawHcalE		->push_back(pf_it->rawHcalEnergy()						);
-	  pfcands_pS1E			->push_back(pf_it->pS1Energy()							);
-	  pfcands_pS2E			->push_back(pf_it->pS2Energy()							);
-	  pfcands_deltaP		->push_back(pf_it->deltaP()							);
-	  pfcands_mva_epi		->push_back(pf_it->mva_e_pi()							);
-	  pfcands_mva_emu		->push_back(pf_it->mva_e_mu()							);
-	  pfcands_mva_pimu		->push_back(pf_it->mva_pi_mu()							);
-	  pfcands_mva_nothing_gamma	->push_back(pf_it->mva_nothing_gamma()						);
-	  pfcands_mva_nothing_nh	->push_back(pf_it->mva_nothing_nh()						);
-	  pfcands_charge		->push_back(pf_it->charge()							);
-	  pfcands_particleId		->push_back(pf_it->translateTypeToPdgId(pf_it->particleId())			);
-	  pfcands_flag			->push_back(pfflags                                                             ); 
+  	    pfcands_p4			          ->push_back( LorentzVector(pf_it->px(), pf_it->py(), pf_it->pz(), pf_it->p())	  );
+  	    pfcands_posAtEcal_p4		  ->push_back( LorentzVector(pf_it->positionAtECALEntrance().x(), pf_it->positionAtECALEntrance().y(), pf_it->positionAtECALEntrance().z(), 0.0) );
+  	    pfcands_isMuIso		        ->push_back( isMuIso                                                            );
+  	    pfcands_ecalE			        ->push_back( isfinite( pf_it->ecalEnergy() ) ? pf_it->ecalEnergy() : -9999.     );
+  	    pfcands_hcalE			        ->push_back( pf_it->hcalEnergy()				                                        );
+  	    pfcands_rawEcalE		      ->push_back( pf_it->rawEcalEnergy()			                                        );
+  	    pfcands_rawHcalE		      ->push_back( pf_it->rawHcalEnergy()			                                        );
+  	    pfcands_pS1E			        ->push_back( pf_it->pS1Energy()					                                        );
+  	    pfcands_pS2E			        ->push_back( pf_it->pS2Energy()					                                        );
+  	    pfcands_deltaP		        ->push_back( pf_it->deltaP()						                                        );
+  	    pfcands_mva_epi		        ->push_back( pf_it->mva_e_pi()					                                        );
+  	    pfcands_mva_emu		        ->push_back( pf_it->mva_e_mu()					                                        );
+  	    pfcands_mva_pimu		      ->push_back( pf_it->mva_pi_mu()				                                          );
+  	    pfcands_mva_nothing_gamma	->push_back( pf_it->mva_nothing_gamma()                                         );
+  	    pfcands_mva_nothing_nh	  ->push_back( pf_it->mva_nothing_nh()		                                        );
+  	    pfcands_charge		        ->push_back( pf_it->charge()						                                        );
+  	    pfcands_particleId		    ->push_back( pf_it->translateTypeToPdgId(pf_it->particleId())	                  );
+  	    pfcands_flag			        ->push_back( pfflags                                                            ); 
 
 
           //for charged pfcandidates, find corresponding track index
@@ -270,30 +267,31 @@ void PFCandidateMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup
      *evt_fixgrid_rho_fwd = getFixGridRho(etabins_fwd,phibins);
      *evt_fixgrid_rho_all = getFixGridRho(etabins_all,phibins);
 
-
-     iEvent.put(pfcands_p4,			"pfcandsp4"		    );
-     iEvent.put(pfcands_posAtEcal_p4,		"pfcandsposAtEcalp4"	    );
-     iEvent.put(pfcands_ecalE,			"pfcandsecalE"		    );
-     iEvent.put(pfcands_hcalE,			"pfcandshcalE"		    );
-     iEvent.put(pfcands_rawEcalE,		"pfcandsrawEcalE"	    );
-     iEvent.put(pfcands_rawHcalE,		"pfcandsrawHcalE"	    );
-     iEvent.put(pfcands_pS1E,			"pfcandspS1E"		    );
-     iEvent.put(pfcands_pS2E,			"pfcandspS2E"		    );
-     iEvent.put(pfcands_deltaP,			"pfcandsdeltaP"		    );
-     iEvent.put(pfcands_mva_epi,		"pfcandsmvaepi"		    );
-     iEvent.put(pfcands_mva_emu,		"pfcandsmvaemu"		    );
-     iEvent.put(pfcands_mva_pimu,		"pfcandsmvapimu"	    );
-     iEvent.put(pfcands_mva_nothing_gamma,	"pfcandsmvanothinggamma"    );
-     iEvent.put(pfcands_mva_nothing_nh,		"pfcandsmvanothingnh"	    );
-     iEvent.put(pfcands_charge,			"pfcandscharge"		    );
-     iEvent.put(pfcands_particleId,		"pfcandsparticleId"	    );
-     iEvent.put(pfcands_flag,			"pfcandsflag"		    );
-     iEvent.put(pfcands_trkidx,			"pfcandstrkidx"		    );
-     iEvent.put(pfcands_pfmusidx,		"pfcandspfmusidx"	    );
-     iEvent.put(pfcands_pfelsidx,		"pfcandspfelsidx"	    );
-     iEvent.put(evt_fixgrid_rho_ctr,		"evtfixgridrhoctr"	    );
-     iEvent.put(evt_fixgrid_rho_fwd,		"evtfixgridrhofwd"	    );
-     iEvent.put(evt_fixgrid_rho_all,		"evtfixgridrhoall"	    );
+     //
+     iEvent.put(pfcands_p4,			            "pfcandsp4"		          );
+     iEvent.put(pfcands_posAtEcal_p4,		    "pfcandsposAtEcalp4"	  );
+     iEvent.put(pfcands_isMuIso,	          "pfcandsisMuIso"	      );
+     iEvent.put(pfcands_ecalE,			        "pfcandsecalE"		      );
+     iEvent.put(pfcands_hcalE,			        "pfcandshcalE"		      );
+     iEvent.put(pfcands_rawEcalE,		        "pfcandsrawEcalE"	      );
+     iEvent.put(pfcands_rawHcalE,		        "pfcandsrawHcalE"	      );
+     iEvent.put(pfcands_pS1E,			          "pfcandspS1E"		        );
+     iEvent.put(pfcands_pS2E,			          "pfcandspS2E"		        );
+     iEvent.put(pfcands_deltaP,			        "pfcandsdeltaP"		      );
+     iEvent.put(pfcands_mva_epi,		        "pfcandsmvaepi"		      );
+     iEvent.put(pfcands_mva_emu,		        "pfcandsmvaemu"		      );
+     iEvent.put(pfcands_mva_pimu,	  	      "pfcandsmvapimu"	      );
+     iEvent.put(pfcands_mva_nothing_gamma,  "pfcandsmvanothinggamma");
+     iEvent.put(pfcands_mva_nothing_nh,		  "pfcandsmvanothingnh"	  );
+     iEvent.put(pfcands_charge,			        "pfcandscharge"		      );
+     iEvent.put(pfcands_particleId,		      "pfcandsparticleId"	    );
+     iEvent.put(pfcands_flag,			          "pfcandsflag"		        );
+     iEvent.put(pfcands_trkidx,			        "pfcandstrkidx"		      );
+     iEvent.put(pfcands_pfmusidx,		        "pfcandspfmusidx"	      );
+     iEvent.put(pfcands_pfelsidx,		        "pfcandspfelsidx"	      );
+     iEvent.put(evt_fixgrid_rho_ctr,		    "evtfixgridrhoctr"	    );
+     iEvent.put(evt_fixgrid_rho_fwd,		    "evtfixgridrhofwd"	    );
+     iEvent.put(evt_fixgrid_rho_all,		    "evtfixgridrhoall"	    );
  
 }
 
