a3bbb608c35457be0b7aeabe1b01d76f367a7698
slava77 <slava77>
2011-09-14 17:39:19 +0000

    changed the name of the ele clusters

--- python/elCaloIsoSequence_cff.py
+++ python/elCaloIsoSequence_cff.py
@@ -3,7 +3,8 @@ import FWCore.ParameterSet.Config as cms
 from CMS2.NtupleMaker.elCaloIsoMaker_cfi import *
 from RecoEgamma.EgammaIsolationAlgos.egammaBasicClusterMerger_cfi import *
 
+
 egammaBasicClusterMerger.src = cms.VInputTag(cms.InputTag('hybridSuperClusters',  'hybridBarrelBasicClusters'), 
-                                             cms.InputTag('multi5x5BasicClusters','multi5x5EndcapBasicClusters')
+                                             cms.InputTag('multi5x5SuperClusters','multi5x5EndcapBasicClusters')
 )
 elCaloIsoSequence = cms.Sequence(egammaBasicClusterMerger*elCaloIsoMaker)
