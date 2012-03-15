3846203ed54783dcbac39595664cc129c2db2a48
dbarge <dbarge>
2012-03-15 20:39:04 +0000

    changed formatting

--- python/vertexMaker_cfi.py
+++ python/vertexMaker_cfi.py
@@ -1,18 +1,21 @@
 import FWCore.ParameterSet.Config as cms
 
-vertexMaker = cms.EDProducer("VertexMaker",
-	aliasPrefix = cms.untracked.string("vtxs"),
-                           primaryVertexInputTag = cms.InputTag("offlinePrimaryVertices"),
+vertexMaker = cms.EDProducer(
+  "VertexMaker",
+  aliasPrefix           = cms.untracked.string("vtxs"),
+  primaryVertexInputTag = cms.InputTag("offlinePrimaryVertices"),
 )
 
 #D.A. vertices will become default at some point.... (4_2_X???)
 from CMS2.NtupleMaker.daVtxSequence_cff import *
-davertexMaker = cms.EDProducer("VertexMaker",
-	aliasPrefix = cms.untracked.string("davtxs"),
-                           primaryVertexInputTag = cms.InputTag("offlinePrimaryVerticesDA"),
+davertexMaker = cms.EDProducer(
+  "VertexMaker",
+  aliasPrefix           = cms.untracked.string("davtxs"),
+  primaryVertexInputTag = cms.InputTag("offlinePrimaryVerticesDA"),
 )
 
-vertexMakerWithBS = cms.EDProducer("VertexMaker",
-                                   aliasPrefix = cms.untracked.string("bsvtxs"),
-                                   primaryVertexInputTag = cms.InputTag("offlinePrimaryVerticesWithBS"),
+vertexMakerWithBS = cms.EDProducer(
+  "VertexMaker",
+  aliasPrefix           = cms.untracked.string("bsvtxs"),
+  primaryVertexInputTag = cms.InputTag("offlinePrimaryVerticesWithBS"),
 )
