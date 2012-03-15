84235182c28d5d206bb4a75a929662e42fc09708
dbarge <dbarge>
2012-03-15 22:38:40 +0000

    updated for 52X

---
+++ test/MCProduction2012_NoFilter_cfg.py
@@ -0,0 +1,28 @@
+from CMS2.NtupleMaker.RecoConfiguration2012_cfg import *
+
+# Global Tag
+process.GlobalTag.globaltag = "START52_V4A::All"
+
+# Load Filters
+
+# Output
+process.out = cms.OutputModule(
+        "PoolOutputModule",
+        fileName     = cms.untracked.string('ntuple.root'),
+        dropMetaData = cms.untracked.string("NONE")
+)
+process.outpath      = cms.EndPath(process.out)
+
+# Branches
+process.out.outputCommands = cms.untracked.vstring( 'drop *' )
+process.out.outputCommands.extend(cms.untracked.vstring('keep *_*Maker*_*_CMS2*'))
+process.out.outputCommands.extend(cms.untracked.vstring('drop *_cms2towerMaker*_*_CMS2*'))
+process.out.outputCommands.extend(cms.untracked.vstring('drop CaloTowers*_*_*_CMS2*'))
+
+#
+process.cms2WithEverything = cms.Sequence( process.ak5PFJets * process.kt6PFJets * process.cms2CoreSequence * process.cms2PFNoTauSequence * process.cms2GENSequence )
+process.p                  = cms.Path( process.cms2WithEverything )
+
+#
+process.MessageLogger.cerr.FwkReport.reportEvery = 1000
+process.eventMaker.isData                        = cms.bool(False)
