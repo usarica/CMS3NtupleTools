ef4fc31c88f36234a576282bffc27b3923500522
dbarge <dbarge>
2012-03-15 20:15:09 +0000

    changed formatting

--- python/l1Maker_cfi.py
+++ python/l1Maker_cfi.py
@@ -1,10 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
-l1Maker = cms.EDProducer("L1Maker",
-	aliasPrefix = cms.untracked.string("l1"),
-    fillL1Particles = cms.untracked.bool(True),
-    l1ParticlesProcessName = cms.untracked.string(""),
-    l1GlobalTriggerReadoutRecordInputTag = cms.InputTag("gtDigis"),
-    l1GlobalTriggerRecordInputTag = cms.InputTag("l1GtRecord"),
-    l1extraParticlesInputTag             = cms.InputTag("l1extraParticles")                   
+l1Maker = cms.EDProducer(
+  "L1Maker",
+  aliasPrefix                          = cms.untracked.string("l1"),
+  fillL1Particles                      = cms.untracked.bool(True),
+  l1ParticlesProcessName               = cms.untracked.string(""),
+  l1GlobalTriggerReadoutRecordInputTag = cms.InputTag("gtDigis"),
+  l1GlobalTriggerRecordInputTag        = cms.InputTag("l1GtRecord"),
+  l1extraParticlesInputTag             = cms.InputTag("l1extraParticles")                   
 )
--- python/luminosityMaker_cfi.py
+++ python/luminosityMaker_cfi.py
@@ -1,8 +1,8 @@
 import FWCore.ParameterSet.Config as cms
+from RecoLuminosity.LumiProducer.lumiProducer_cfi import *
 
-luminosityMaker = cms.EDProducer("LuminosityMaker",
-                                 aliasPrefix = cms.untracked.string("ls"),
-                                 lumiSummaryInputTag = cms.InputTag("lumiProducer")
-                                 )
-
-
+luminosityMaker = cms.EDProducer(
+  "LuminosityMaker",
+  aliasPrefix = cms.untracked.string("ls"),
+  lumiSummaryInputTag = cms.InputTag("lumiProducer")
+)
