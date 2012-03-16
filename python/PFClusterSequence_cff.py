bd131a8d2ef1fb1c309f809d22f75f5d3ab132b1
dbarge <dbarge>
2012-03-16 19:53:57 +0000

    retired

--- python/PFClusterSequence_cff.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from CMS2.NtupleMaker.PFClusterMaker_cfi import *
-
-PFClusterMakerSequence      = cms.Sequence( PFClusterMaker)
-
-
--- python/patMETMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-patMETMaker = cms.EDProducer("PATMETMaker",
-	aliasPrefix = cms.untracked.string("met_pat"),
-                           patMETsInputTag = cms.InputTag("patMETs")
-)
-
-
