04959f653f715a57eec05f3583e3bb11f666449e
dalfonso <dalfonso>
2013-02-18 20:08:53 +0000

    fix post Alias

--- python/genJetMaker_cfi.py
+++ python/genJetMaker_cfi.py
@@ -3,13 +3,13 @@ import FWCore.ParameterSet.Config as cms
 genJetMaker = cms.EDProducer("GenJetMaker", 
    genJetsInputTag = cms.InputTag("cms2antikt5GenJets"),
    genJetMinPtCut  = cms.double(10.0),
-   aliasPostfix = cms.untracked.string("")
+   aliasPostfix = cms.untracked.string("NoMuNoNu")
 )
 
 
 genPFJetMaker = cms.EDProducer("GenJetMaker", 
    genJetsInputTag = cms.InputTag("cms2antikt5PFGenJets"),
    genJetMinPtCut  = cms.double(10.0),
-   aliasPostfix = cms.untracked.string("NoNu")
+   aliasPostfix = cms.untracked.string("")
 
 )
