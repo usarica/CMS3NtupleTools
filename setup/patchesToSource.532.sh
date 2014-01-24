e68f0d98fc85c1daea576b6aa9f506b121aea532
jgran <jgran@physics.ucsb.edu>
2014-01-23 23:57:37 -0800

    now all packages are checked out from git

--- setup/patchesToSource.532.sh
+++ setup/patchesToSource.532.sh
@@ -4,33 +4,57 @@
 # MET Filters 2012 #
 ####################
 
-git cms-cvs-history import V00-00-10 RecoMET/METFilters
-touch RecoMET/METFilters/data/dummy.txt
+git clone https://github.com/cms-cvs-history/RecoMET-METFilters $CMSSW_BASE/src/RecoMET/METFilters
+pushd $CMSSW_BASE/src/RecoMET/METFilters
+git checkout RecoMET-METFilters-V00-00-10
+popd
+touch $CMSSW_BASE/src/RecoMET/METFilters/data/dummy.txt
 sed '3 s/EDFilter/EDProducer/' <$CMSSW_BASE/src/RecoMET/METFilters/python/EcalDeadCellDeltaRFilter_cfi.py > blah
 mv blah $CMSSW_BASE/src/RecoMET/METFilters/python/EcalDeadCellDeltaRFilter_cfi.py
 
-# not working -- tag not migrated (using cvs):
-# git cms-cvs-history import V00-00-08 RecoMET/METAnalyzers
-cvs co -r V00-00-08 RecoMET/METAnalyzers
 
-git cms-cvs-history import V00-03-23 CommonTools/RecoAlgos  
+git clone https://github.com/cms-cvs-dump/RecoMET_METAnalyzers $CMSSW_BASE/src/RecoMET/METAnalyzers
+pushd $CMSSW_BASE/src/RecoMET/METAnalyzers
+git checkout V00-00-08
+popd
+
+
+
+git clone https://github.com/cms-cvs-history/CommonTools-RecoAlgos $CMSSW_BASE/src/CommonTools/RecoAlgos
+pushd $CMSSW_BASE/src/CommonTools/RecoAlgos
+git checkout CommonTools-RecoAlgos-V00-03-23
+popd
+
 
 #############
 # MVA JetId #
 #############
  
-# not working -- tag not migrated: git cms-cvs-history import V00-03-01 -d CMGTools/External UserCode/CMG/CMGTools/External
-cvs co -r V00-03-01 -d CMGTools/External UserCode/CMG/CMGTools/External
+git clone https://github.com/latinos/UserCode-CMG-CMGTools-External $CMSSW_BASE/src/CMGTools/External
+pushd $CMSSW_BASE/src/CMGTools/External
+git checkout V00-03-01
+popd
+
 
 #############################
 # do stuff with PF candidates
 #############################
 
-git cms-cvs-history import V15-02-06 RecoParticleFlow/PFProducer
+git clone https://github.com/cms-cvs-history/RecoParticleFlow-PFProducer $CMSSW_BASE/src/RecoParticleFlow/PFProducer
+pushd $CMSSW_BASE/src/RecoParticleFlow/PFProducer
+git checkout RecoParticleFlow-PFProducer-V15-02-06
+popd
 
-# use new recommended tags (keep the original PFProducer)
-git cms-cvs-history import V00-03-15 CommonTools/ParticleFlow
-git cms-cvs-history import V08-09-21 PhysicsTools/PatAlgos
+git clone https://github.com/cms-cvs-history/CommonTools-ParticleFlow $CMSSW_BASE/src/CommonTools/ParticleFlow
+pushd $CMSSW_BASE/src/CommonTools/ParticleFlow
+git checkout CommonTools-ParticleFlow-V00-03-15
+popd
+
+git clone https://github.com/cms-cvs-history/PhysicsTools-PatAlgos $CMSSW_BASE/src/PhysicsTools/PatAlgos
+pushd $CMSSW_BASE/src/PhysicsTools/PatAlgos
+git checkout PhysicsTools-PatAlgos-V08-09-21
+popd
+
 
 ####################
 # 2012 Electron ID #
@@ -41,21 +65,25 @@ pushd $CMSSW_BASE/src/EGamma/EGammaAnalysisTools
 git checkout CutBasedId_V00-00-03 
 popd
 
+
 ########################
 # Type-I Corrected MET #
 ########################
 
-# not working -- tag not migrated (using cvs): git cms-cvs-history
-#git cms-cvs-history import V04-06-09 JetMETCorrections/Type1MET 
-cvs co -r V04-06-09 JetMETCorrections/Type1MET 
+git clone https://github.com/cms-cvs-history/JetMETCorrections-Type1MET $CMSSW_BASE/src/JetMETCorrections/Type1MET
+pushd $CMSSW_BASE/src/JetMETCorrections/Type1MET
+git checkout JetMETCorrections-Type1MET-V04-06-09
+popd
+
 
 #######################
 # MC Jet Flavor Truth #
 #######################
 
-# not working -- tag not migrated (using cvs): git cms-cvs-history
-# git cms-cvs-history import V00-07-04 PhysicsTools/JetMCAlgos
-cvs co -r V00-07-04 PhysicsTools/JetMCAlgos
+git clone https://github.com/cms-cvs-history/PhysicsTools-JetMCAlgos $CMSSW_BASE/src/PhysicsTools/JetMCAlgos
+pushd $CMSSW_BASE/src/PhysicsTools/JetMCAlgos
+git checkout PhysicsTools-JetMCAlgos-V00-07-04
+popd
 
 sed '13 s/ParameterSet/Utilities/' <$CMSSW_BASE/src/PhysicsTools/JetMCAlgos/plugins/CandOneToManyDeltaRMatcher.cc >blah
 mv blah $CMSSW_BASE/src/PhysicsTools/JetMCAlgos/plugins/CandOneToManyDeltaRMatcher.cc
@@ -78,19 +106,26 @@ mv blah $CMSSW_BASE/src/PhysicsTools/JetMCAlgos/plugins/PartonSelector.cc
 sed '32 s/GenParticleCandidate/GenParticle/' <$CMSSW_BASE/src/PhysicsTools/JetMCAlgos/plugins/GenJetBCEnergyRatio.cc >blah
 mv blah $CMSSW_BASE/src/PhysicsTools/JetMCAlgos/plugins/GenJetBCEnergyRatio.cc
 
+
 #######################
 # TAUs
 #######################
 
-git cms-cvs-history import V01-04-23 RecoTauTag/RecoTau #HCP + new discriminants
+git clone https://github.com/cms-cvs-history/RecoTauTag-RecoTau $CMSSW_BASE/src/RecoTauTag/RecoTau
+pushd $CMSSW_BASE/src/RecoTauTag/RecoTau
+git checkout RecoTauTag-RecoTau-V01-04-23
+popd
 
-# not working -- tag not migrated (using cvs): git cms-cvs-history
-# git cms-cvs-history import V01-04-10 RecoTauTag/Configuration
-cvs co -r V01-04-10 RecoTauTag/Configuration
 
-# not working -- tag not migrated (using cvs): git cms-cvs-history
-# git cms-cvs-history import V00-04-00 CondFormats/EgammaObjects
-cvs co -r V00-04-00 CondFormats/EgammaObjects
+git clone https://github.com/cms-cvs-history/RecoTauTag-Configuration $CMSSW_BASE/src/RecoTauTag/Configuration
+pushd $CMSSW_BASE/src/RecoTauTag/Configuration
+git checkout RecoTauTag-Configuration-V01-04-10
+popd
+
+git clone https://github.com/cms-cvs-history/CondFormats-EgammaObjects $CMSSW_BASE/src/CondFormats/EgammaObjects
+pushd $CMSSW_BASE/src/CondFormats/EgammaObjects
+git checkout CondFormats-EgammaObjects-V00-04-00
+popd
 
 #######################
 
