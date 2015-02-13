b15ce34e5f6bfc7c5482b72f01d75484b4108aef
Christopher A. George <george@physics.ucsb.edu>
2015-02-13 15:54:35 -0800

    fix install

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@
 GlobalTag=PHYS14_25_V2::All
 CMS3Tag=CMS3_V07-02-05
 
-curl https://raw.githubusercontent.com/cmstas/NtupleTools/master/AutoTuple/setup.sh > setup.sh
+curl https://raw.githubusercontent.com/cmstas/NtupleTools/master/AutoTuple/setup.sh > setup2.sh
 
-sed -i '2,6d' setup.sh
-sed -i "3s/.*/gtag=$GlobalTag/" setup.sh
-sed -i "4s/.*/tag=$CMS3Tag/" setup.sh
-sed -i '$d' setup.sh
-sed -i '$d' setup.sh
+sed -i '2,6d' setup2.sh
+sed -i "3s/.*/gtag=$GlobalTag/" setup2.sh
+sed -i "4s/.*/tag=$CMS3Tag/" setup2.sh
+
+head -n -20 setup2.sh > setup.sh
+rm setup2.sh
+sed -i 's,git clone git@github.com:cmstas/NtupleMaker.git CMS3/NtupleMaker,mv * CMSSW_7_2_0/src/,' setup.sh
