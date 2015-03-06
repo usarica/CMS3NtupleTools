f16a858c0cd80e6414853b2fd21a5ab4f4383639
jgran <jgran@physics.ucsb.edu>
2015-03-05 16:38:06 -0800

    update install script

--- install.sh
+++ install.sh
@@ -3,10 +3,13 @@
 #USER INPUTS
 CMS3Tag=master
 
+export CMS_PATH=/cvmfs/cms.cern.ch
 export SCRAM_ARCH=slc6_amd64_gcc481
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+source /cvmfs/cms.cern.ch/slc6_amd64_gcc481/lcg/root/5.34.18/bin/thisroot.sh
 scramv1 p -n CMSSW_7_2_0 CMSSW CMSSW_7_2_0
 cd CMSSW_7_2_0/src
-cmsenv
+eval `scramv1 runtime -sh`
 git clone git@github.com:cmstas/NtupleMaker.git CMS3/NtupleMaker
 cd CMS3/NtupleMaker
 git checkout $CMS3Tag
