69c66b51ff1b5d55854c2785c3ec946708da0f82
Christopher A. George <george@physics.ucsb.edu>
2015-06-02 00:09:08 -0700

    update

--- setup/patchesToSource.sh
+++ setup/patchesToSource.sh
@@ -7,18 +7,18 @@
 cd $CMSSW_BASE/src/CMS3/NtupleMaker
 
 #Hard-code lepton ID
-git update-index --assume-unchanged setup/GsfEleFull5x5SigmaIEtaIEtaCut72X.cc 
+#git update-index --assume-unchanged setup/GsfEleFull5x5SigmaIEtaIEtaCut72X.cc 
 git update-index --assume-unchanged setup/cutBasedElectronID_PHYS14_PU20bx25_V2_miniAOD_cff.py 
-mkdir $CMSSW_BASE/bullshit  
-mv $CMSSW_BASE/src/* $CMSSW_BASE/bullshit/
-git cms-addpkg RecoEgamma/ElectronIdentification 
-mv $CMSSW_BASE/bullshit/CMS3/NtupleMaker/setup/GsfEleFull5x5SigmaIEtaIEtaCut72X.cc $CMSSW_BASE/src/RecoEgamma/ElectronIdentification/plugins/cuts/
-mv $CMSSW_BASE/bullshit/CMS3/NtupleMaker/setup/cutBasedElectronID_PHYS14_PU20bx25_V2_miniAOD_cff.py $CMSSW_BASE/src/RecoEgamma/ElectronIdentification/python/Identification
+git cms-merge-topic ikrav:egm_id_74X_v0
+#mkdir $CMSSW_BASE/bullshit  
+#mv $CMSSW_BASE/src/* $CMSSW_BASE/bullshit/
+#git cms-addpkg RecoEgamma/ElectronIdentification 
+#mv $CMSSW_BASE/bullshit/CMS3/NtupleMaker/setup/GsfEleFull5x5SigmaIEtaIEtaCut72X.cc $CMSSW_BASE/src/RecoEgamma/ElectronIdentification/plugins/cuts/
 pushd $CMSSW_BASE/src/
 scram b -j 20 
 popd
-mv $CMSSW_BASE/bullshit/* $CMSSW_BASE/src/
-rmdir $CMSSW_BASE/bullshit
+#mv $CMSSW_BASE/bullshit/* $CMSSW_BASE/src/
+#rmdir $CMSSW_BASE/bullshit
 
 
 #############
