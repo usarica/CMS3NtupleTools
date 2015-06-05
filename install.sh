3bf5f5a972f6ffb49bb536328d07b44ce4200c00
Alex George <george@physics.ucsb.edu>
2015-06-05 13:00:38 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 #USER INPUTS
-CMS3Tag=CMS3_V07-04-01
+CMS3Tag=master
 CMSSW_release=7_4_1_patch1
+CMSSW_release_name=    #don't forget the underscore!
 export SCRAM_ARCH=slc6_amd64_gcc491
 
 #--Here there be dragons----
 export CMS_PATH=/cvmfs/cms.cern.ch
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-scramv1 p -n CMSSW_$CMSSW_release CMSSW CMSSW_$CMSSW_release
-cd CMSSW_$CMSSW_release/src
+scramv1 p -n CMSSW_${CMSSW_release}${CMSSW_release_name} CMSSW CMSSW_$CMSSW_release
+cd CMSSW_${CMSSW_release}_${CMSSW_release_name}/src
 eval `scramv1 runtime -sh`
 git clone git@github.com:cmstas/NtupleMaker.git CMS3/NtupleMaker
 cd CMS3/NtupleMaker
 git checkout $CMS3Tag
 source setup/patchesToSource.sh
 cd $CMSSW_BASE/src
-scram b -j 10
+scram b -j 20
 cd ..
