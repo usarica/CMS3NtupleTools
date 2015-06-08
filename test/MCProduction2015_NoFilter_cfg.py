bfb28db90f4ae0a1422bddb7c5e488a2c9e0a978
Alex George <george@physics.ucsb.edu>
2015-06-08 14:54:25 -0700

    clean up

--- test/MCProduction2015_NoFilter_cfg.py
+++ test/MCProduction2015_NoFilter_cfg.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
-from Configuration.EventContent.EventContent_cff        import *
+from Configuration.EventContent.EventContent_cff   import *
 
 # CMS3
 process = cms.Process("CMS3")
@@ -11,22 +11,16 @@ process.configurationMetadata = cms.untracked.PSet(
         name       = cms.untracked.string('CMS3 test configuration')
 )
 
-
 # load event level configurations
 process.load('Configuration/EventContent/EventContent_cff')
 process.load("Configuration.StandardSequences.Services_cff")
-#process.load("Configuration.StandardSequences.Reconstruction_cff")
 process.load('Configuration.Geometry.GeometryRecoDB_cff')
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff")
-#process.load("TrackPropagation.SteppingHelixPropagator.SteppingHelixPropagatorAny_cfi")
-#process.load("TrackingTools.TrackAssociator.DetIdAssociatorESProducer_cff")
 process.load("Configuration.StandardSequences.GeometryRecoDB_cff")
 
 # services
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
-#process.GlobalTag.globaltag = "PHYS14_25_V2::All"
-#process.GlobalTag.globaltag = "MCRUN2_74_V9::All"
 process.GlobalTag.globaltag = "MCRUN2_74_V9A"
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.MessageLogger.cerr.threshold  = ''
@@ -44,9 +38,8 @@ process.out.outputCommands = cms.untracked.vstring( 'drop *' )
 process.out.outputCommands.extend(cms.untracked.vstring('keep *_*Maker*_*_CMS3*'))
 process.out.outputCommands.extend(cms.untracked.vstring('drop *_cms2towerMaker*_*_CMS3*'))
 process.out.outputCommands.extend(cms.untracked.vstring('drop CaloTowers*_*_*_CMS3*'))
-#####################################
+
 #load cff and third party tools
-#####################################
 from JetMETCorrections.Configuration.DefaultJEC_cff import *
 from JetMETCorrections.Configuration.JetCorrectionServices_cff import *
 from JMEAnalysis.JetToolbox.jetToolbox_cff import *
@@ -58,62 +51,39 @@ process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
 from RecoJets.JetProducers.fixedGridRhoProducerFastjet_cfi import *
 process.fixedGridRhoFastjetAll = fixedGridRhoFastjetAll.clone(pfCandidatesTag = 'packedPFCandidates')
 
-#####################################
-#Electron Identification for PHYS 14#
-#####################################
-
-from PhysicsTools.SelectorUtils.tools.vid_id_tools import *       #maybe we need these?
+#Electron Identification for PHYS 14
+from PhysicsTools.SelectorUtils.tools.vid_id_tools import *  
 from PhysicsTools.SelectorUtils.centralIDRegistry import central_id_registry
-
 process.load("RecoEgamma.ElectronIdentification.egmGsfElectronIDs_cfi")
 process.egmGsfElectronIDs.physicsObjectSrc = cms.InputTag('slimmedElectrons',"","PAT")
 process.egmGsfElectronIDSequence = cms.Sequence(process.egmGsfElectronIDs)
 my_id_modules = ['RecoEgamma.ElectronIdentification.Identification.cutBasedElectronID_PHYS14_PU20bx25_V2_cff']
 for idmod in my_id_modules:
     setupAllVIDIdsInModule(process,idmod,setupVIDElectronSelection)
-### added these.
 
 #process.globalPixelSeeds.OrderedHitsFactoryPSet.maxElement = cms.uint32(100000)
 #process.gsfElectrons.MaxElePtForOnlyMVA = cms.double(50.0)
-##############################
-#### Load Ntuple producer cff#####
-##############################
+
+# Load Ntuple producer cff
 process.load("CMS3.NtupleMaker.cms3CoreSequences_cff")
 process.load("CMS3.NtupleMaker.cms3GENSequence_cff")
 process.load("CMS3.NtupleMaker.cms3PFSequence_cff")
+
 # Hypothesis cuts
 process.hypDilepMaker.TightLepton_PtCut  = cms.double(10.0)
 process.hypDilepMaker.LooseLepton_PtCut  = cms.double(10.0)
 
-###################
-#Options for Input#
-###################
+#Options for Input
 process.source = cms.Source("PoolSource",
-#   fileNames = cms.untracked.vstring('file:/hadoop/cms/phedex/store/mc/Phys14DR/SMS-T2tt_2J_mStop-850_mLSP-100_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU20bx25_tsg_PHYS14_25_V1-v1/00000/563CD412-C16B-E411-ACE1-C4346BC8E730.root')
 fileNames = cms.untracked.vstring('file:///home/users/gzevi/ntupling/CMSSW_7_4_1/src/CMS3/NtupleMaker/QCD_Pt_1400to1800_Asympt50ns_MCRUN2_74_V9A-v1.root')
 )
-process.source.noEventSort            = cms.untracked.bool( True )
+process.source.noEventSort = cms.untracked.bool( True )
+
 #Max Events
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
-###############################
-##### Run jet tool box#########
-###############################
+#Run jet tool box
 jetToolbox( process, 'ak4', 'ak4JetSubs', 'out',PUMethod='',miniAOD=True,JETCorrLevels=['L1FastJet','L2Relative', 'L3Absolute'])
-#jetToolbox( process, 'ca10', 'ca10JetSubs', 'out', 
-#            PUMethod='',
-             #addPrunedSubjets=True, 
-             #addSoftDropSubjets=True,
-#            addTrimming=True,
-#            addPruning=True,
-#            addSoftDrop=True,
-#            addFiltering=True,
-#            addMassDrop=True,
-#            addCMSTopTagger=False,
-#            addNsub=True,
-#            miniAOD=True,
-#            JETCorrLevels=['L1FastJet','L2Relative', 'L3Absolute'] ) 
-#process.load('CMS3.NtupleMaker.ca12subJetMaker_cfi')
 
 process.p = cms.Path( 
   process.metFilterMaker *
