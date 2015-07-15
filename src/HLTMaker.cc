6e541920e1818e10fe4875e79bf09c64ba6c3142
Christopher A. George <george@physics.ucsb.edu>
2015-07-15 16:52:22 -0700

    delete outdated comments

--- src/HLTMaker.cc
+++ src/HLTMaker.cc
@@ -24,8 +24,7 @@ void PrintTriggerObjectInfo( ofstream& outfile, int id, const ROOT::Math::Lorent
 }
 */
 
-HLTMaker::HLTMaker(const edm::ParameterSet& iConfig)
-{
+HLTMaker::HLTMaker(const edm::ParameterSet& iConfig){
   processName_        = iConfig.getUntrackedParameter<string>         ("processName"       );
   fillTriggerObjects_ = iConfig.getUntrackedParameter<bool>           ("fillTriggerObjects");
   prunedTriggerNames_ = iConfig.getUntrackedParameter<vector<string> >("prunedTriggerNames");
@@ -45,8 +44,7 @@ HLTMaker::HLTMaker(const edm::ParameterSet& iConfig)
   
 }
 
-void HLTMaker::beginRun(const edm::Run& iRun, const edm::EventSetup& iSetup)
-{
+void HLTMaker::beginRun(const edm::Run& iRun, const edm::EventSetup& iSetup){
 
   // In the case that we are choosing the process name
   // automatically, i.e. the processName_ parameter is
@@ -59,39 +57,20 @@ void HLTMaker::beginRun(const edm::Run& iRun, const edm::EventSetup& iSetup)
   if (processName_ != "") {
 	bool changed(true);
 	if (hltConfig_.init(iRun,iSetup,"*",changed)) {
-	} else 
-	  throw cms::Exception("HLTMaker::beginRun: config extraction failure with process name " + processName_);
+	} 
+    else throw cms::Exception("HLTMaker::beginRun: config extraction failure with process name " + processName_);
   }
 }
 
-void HLTMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
-{
+void HLTMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup){
   // If the process name is not specified retrieve the  latest
   // TriggerEvent object and the corresponding TriggerResults.
   // We should only have to do this once though, the next time
   // produce is called processName_ should be set.
-//AOD  if (processName_ == "") {
-//AOD    iEvent.getByLabel(edm::InputTag("hltTriggerSummaryAOD", ""), triggerEventH_);
-//AOD    if (! triggerEventH_.isValid()  )
-//AOD      throw cms::Exception("HLTMaker::produce: error getting TriggerEvent product from Event!"  );
-//AOD    // This line is important as it makes sure it is never called
-//AOD    // again! A self-terminating code snippet...
-//AOD    processName_ = triggerEventH_.provenance()->processName();
-//AOD    // This is the once and only once bit described in beginRun
-//AOD    bool changed(true);
-//AOD    if (hltConfig_.init(iEvent.getRun(),iSetup,processName_,changed)) {
-//AOD    } else 
-//AOD      throw cms::Exception("HLTMaker::produce: config extraction failure with process name " + processName_);
-//AOD  } else {
-//AOD    iEvent.getByLabel(edm::InputTag("hltTriggerSummaryAOD", "", processName_), triggerEventH_  );
-//AOD    if (! triggerEventH_.isValid()  )
-//AOD      throw cms::Exception("HLTMaker::produce: error getting TriggerEvent product from Event!"  );
-//AOD  }
 
-// Now using a single processName_ (set to "HLT" in the configuration file). Is this OK? Do we need the flexibility we had before?
+  //Now using a single processName_ (set to "HLT" in the configuration file). Is this OK? Do we need the flexibility we had before?
   iEvent.getByLabel(edm::InputTag("TriggerResults",       "", processName_), triggerResultsH_);
-  if (! triggerResultsH_.isValid())
-    throw cms::Exception("HLTMaker::produce: error getting TriggerResults product from Event!");
+  if (! triggerResultsH_.isValid()) throw cms::Exception("HLTMaker::produce: error getting TriggerResults product from Event!");
   
   triggerNames_ = iEvent.triggerNames(*triggerResultsH_); // Does this have to be done for every event?
 
@@ -151,10 +130,8 @@ void HLTMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   trigObjsid->reserve(nTriggers);
   trigObjsp4->reserve(nTriggers);
 
-  for(unsigned int i = 0; i < nTriggers; ++i)
-    {
-      // Create now because must exist regardless
-      // of the accept
+  for(unsigned int i = 0; i < nTriggers; ++i){
+      // Create now because must exist regardless of the accept
       vector<LorentzVector> p4V;
       vector<int> idV;
 
@@ -167,15 +144,9 @@ void HLTMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
       // prescales->push_back( triggerPrescalesH_.isValid() ? triggerPrescalesH_->getPrescaleForIndex(i) : -1 );
 	  bool isdata = iEvent.isRealData();
 
-	  if( isdata ){
+	  if(isdata){
 
 		//get prescale info from hltConfig_
-		// pair
-		//   -vector
-		//     -pair
-		//       -string
-		// 	     -int
-		//   -int
 		std::pair<std::vector<std::pair<std::string,int> >,int> detailedPrescaleInfo = hltConfig_.prescaleValuesInDetail(iEvent, iSetup, name);	 
 		prescales->push_back( triggerPrescalesH_.isValid() ? detailedPrescaleInfo.second : -1 );
 	  
@@ -190,18 +161,18 @@ void HLTMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 		size_t minind = std::distance(std::begin(l1prescalevals), result);
 		// sometimes there are no L1s associated with a HLT. In that case, this branch stores -1 for the l1prescale
 		l1prescales->push_back( minind < l1prescalevals.size() ? l1prescalevals.at(minind) : -1 );
-	  }else{
+	  }
+      else {
 	  	prescales   -> push_back( hltConfig_.prescaleValue(iEvent, iSetup, name) );
 	  	l1prescales -> push_back( 1 );
 	  }
 	    
 	  // Passed... F+
-	  if (triggerResultsH_->accept(i)) {
+	  if (triggerResultsH_->accept(i)){
 		bits->SetBitNumber(i);
 
 		// Collect desired trigger objects 
-		if (fillTriggerObjects_ && doPruneTriggerName(name))
-		  fillTriggerObjectInfo(i, idV, p4V);
+		if (fillTriggerObjects_ && doPruneTriggerName(name)) fillTriggerObjectInfo(i, idV, p4V);
 	  }
 	  
       trigObjsid->push_back(idV);
@@ -277,214 +248,6 @@ void HLTMaker::fillTriggerObjectInfo(unsigned int triggerIndex, vector<int>& idV
 
 
   // End of Triggers from miniAOD
-
-//AOD  // trigger name
-//AOD  //const string& trigName = hltConfig_.triggerName(triggerIndex);
-//AOD
-//AOD  // debug 
-//AOD  //string fileName = Form("%s.txt", trigName.c_str() );
-//AOD  //std::ofstream outfile( fileName.c_str(), ios::out | ios::app );
-//AOD
-//AOD  //
-//AOD  const trigger::TriggerObjectCollection& triggerObjects = triggerEventH_->getObjects();  // trigger objects
-//AOD  if (triggerObjects.size() == 0) return;                                                 //
-//AOD  const vector<string>& moduleLabels = hltConfig_.moduleLabels(triggerIndex);             // modules on this trigger path
-//AOD  const unsigned int    moduleIndex  = triggerResultsH_->index(triggerIndex);             // index (slot position) of module giving the decision of the path
-//AOD  unsigned int          nFilters     = triggerEventH_->sizeFilters();                     // number of filters
-//AOD
-//AOD/////////////////////////////////////
-//AOD// Show all stored trigger objects //
-//AOD/////////////////////////////////////
-//AOD
-//AOD  // CMSSW_4_2x
-//AOD  // We want to store the filter index corresponding to the last filter for each distinct trigger object type ( the trigger id & p4 can be accessed given the filter index )
-//AOD  // For Electrons: take the filter index for the last trigger id of either 82 or 92, we don't consider 82 and 92 distinct trigger objects
-//AOD  // This assumes that each filter will have the same trigger ids... this is *NOT* true for overlap triggers, PFTau triggers for instance
-//AOD  // If we find that any filter has mixed trigger ids, we store the objects for the last filter only ( this is what was done before and should be right )
-//AOD  // Yes this is kludged, and yes a universal solution is preferred
-//AOD
-//AOD  // map ( trigger id => filter index )
-//AOD  map< int, unsigned int > trigObjsToStore;
-//AOD
-//AOD  // True if different trigger id's are found in the same filter
-//AOD  bool mixedTrigIds = false;
-//AOD
-//AOD  // loop over trigger modules
-//AOD  unsigned int lastFilterIndex = nFilters;
-//AOD  for(unsigned int j = 0; j <= moduleIndex; ++j) {
-//AOD
-//AOD    // get module name & filter index
-//AOD    const string&      moduleLabel = moduleLabels[j];
-//AOD    const unsigned int filterIndex = triggerEventH_->filterIndex(InputTag(moduleLabel, "", processName_));
-//AOD
-//AOD    // debug
-//AOD    //if( j == 0 ){
-//AOD    //  outfile << endl << "--------------------------------------------------------------------------------------------------------------" << endl << endl;
-//AOD    //  outfile << trigName << endl << endl;
-//AOD    //  outfile << "-> Everything:" << endl << endl;
-//AOD    //}
-//AOD      
-//AOD    // these are the filters with trigger objects filled
-//AOD    if ( filterIndex < nFilters ){ 
-//AOD     
-//AOD      //
-//AOD      lastFilterIndex = filterIndex;
-//AOD   
-//AOD      // get trigger objects & ids
-//AOD      //unsigned int lastEleFilterIndex = 0;
-//AOD      const trigger::Vids& triggerIds  = triggerEventH_->filterIds(filterIndex);
-//AOD      const trigger::Keys& triggerKeys = triggerEventH_->filterKeys(filterIndex);
-//AOD      assert( triggerIds.size() == triggerKeys.size() );
-//AOD
-//AOD      // True if a filter has hlt objects ( objects with positive trigger id )
-//AOD      bool hlt = false;   
-//AOD      
-//AOD      // Trigger object id for filters associated with a unique trigger object id
-//AOD      int filterId = -1;
-//AOD
-//AOD      // loop on trigger objects
-//AOD      for( unsigned int k = 0; k < triggerKeys.size(); k++){
-//AOD
-//AOD        // trigger object id
-//AOD        int id = triggerIds.at(k);
-//AOD
-//AOD        // trigger p4, p4id
-//AOD        //const trigger::TriggerObject& triggerObject = triggerObjects[ triggerKeys[k] ];   
-//AOD
-//AOD        // hlt objects
-//AOD        if( id > 0 ){
-//AOD          hlt = true;                                 // True if a filter has hlt objects ( objects with positive trigger id )
-//AOD          if( k == 0 ){                               // Assuming all trigger ids are the same for this filter
-//AOD            filterId = id;                            // Filter id for all the objects in this filter
-//AOD            if( filterId == 82 || filterId == 92 ){   // Remember the index of the last filter with all objects having a trigger id of 82 or 92 
-//AOD              //lastEleFilterIndex = filterIndex;
-//AOD            }
-//AOD          }
-//AOD          else {
-//AOD            if( id != filterId ){
-//AOD              mixedTrigIds = true;                    // True if different trigger id's are found in the same filter
-//AOD              //cout << endl << endl << trigName << ", " << moduleLabel << ":\t ERROR in HLTMaker.cc: different trigger object ids found in the same filter... Exiting." << endl << endl;
-//AOD              //exit(1);
-//AOD            }
-//AOD          }
-//AOD        } // end if( id > 0 )
-//AOD
-//AOD        // debug
-//AOD        //if( k == 0 ) outfile << "\t" << filterIndex << ": " << moduleLabel << endl << endl;
-//AOD        //PrintTriggerObjectInfo( outfile, id, triggerObject.particle().p4() );
-//AOD
-//AOD      } // end loop on trigger objects
-//AOD
-//AOD      //
-//AOD      if( hlt ){                                        // only store hlt objects
-//AOD        assert( filterId != -1 );                       // sanity
-//AOD        trigObjsToStore[filterId] = filterIndex;        // Store the filter Index ( used to get trigger objects ) for each different trigger type ( filterId )
-//AOD        if( filterId == 82 ) trigObjsToStore.erase(92); // If this is an electron trigger ( filterId 82 or 92 ) we only want the last one ( that is either 82 or 92 )
-//AOD        if( filterId == 92 ) trigObjsToStore.erase(82); // 
-//AOD      }
-//AOD
-//AOD    } // end if(filterIndex < nFilters)
-//AOD  }   // end loop over trigger modules
-//AOD 
-//AOD//////////////////////////////////////////////
-//AOD// Show the trigger objects we want to save //
-//AOD//////////////////////////////////////////////
-//AOD
-//AOD  // debug
-//AOD  //outfile << endl << endl << "-> What we want to store";
-//AOD  //if(mixedTrigIds) outfile << " ( mixed ids... storing last filter only )";
-//AOD  //outfile << ":" << endl << endl;
-//AOD
-//AOD  // If we find different trigger ids under any filter we only store the objects for the last filter
-//AOD  if( mixedTrigIds ){
-//AOD
-//AOD    // get trigger objects & ids
-//AOD    const trigger::Vids& triggerIds  = triggerEventH_->filterIds ( lastFilterIndex );
-//AOD    const trigger::Keys& triggerKeys = triggerEventH_->filterKeys( lastFilterIndex );
-//AOD    assert( triggerIds.size() == triggerKeys.size() );
-//AOD
-//AOD    // loop on trigger objects
-//AOD    for( unsigned int k = 0; k < triggerKeys.size(); k++){
-//AOD
-//AOD      // trigger object id
-//AOD      int id = triggerIds.at(k);                                                        
-//AOD
-//AOD      // trigger p4, p4id
-//AOD      const trigger::TriggerObject& triggerObject = triggerObjects[ triggerKeys[k] ];
-//AOD  
-//AOD      // store trigger id, trigger p4, & trigger object id
-//AOD      p4V.push_back( LorentzVector( triggerObject.particle().p4() ) );
-//AOD      idV.push_back( id );
-//AOD
-//AOD      // debug
-//AOD      //PrintTriggerObjectInfo( outfile, id, triggerObject.particle().p4() );
-//AOD    }
-//AOD 
-//AOD  }
-//AOD  // If each filter has trigger objects with the same trigger id, we store the trigger 
-//AOD  // objects for the last filter of each distinct trigger object type
-//AOD  else {
-//AOD
-//AOD    map<int,unsigned int>::iterator it;
-//AOD    for( it = trigObjsToStore.begin() ; it != trigObjsToStore.end(); it++ ){
-//AOD    
-//AOD      // get trigger objects & ids
-//AOD      const unsigned int filterIndex   = (*it).second;
-//AOD      const trigger::Vids& triggerIds  = triggerEventH_->filterIds ( filterIndex );
-//AOD      const trigger::Keys& triggerKeys = triggerEventH_->filterKeys( filterIndex );
-//AOD      assert( triggerIds.size() == triggerKeys.size() );
-//AOD    
-//AOD      // loop on trigger objects
-//AOD      for( unsigned int k = 0; k < triggerKeys.size(); k++){
-//AOD
-//AOD        // trigger object id
-//AOD        int id = triggerIds.at(k);                                                        
-//AOD
-//AOD        // trigger p4, p4id
-//AOD        const trigger::TriggerObject& triggerObject = triggerObjects[ triggerKeys[k] ];
-//AOD    
-//AOD        // store trigger id, trigger p4, & trigger object id
-//AOD        p4V.push_back( LorentzVector( triggerObject.particle().p4() ) );
-//AOD        idV.push_back( id );
-//AOD
-//AOD        // debug
-//AOD        //PrintTriggerObjectInfo( outfile, id, triggerObject.particle().p4() );
-//AOD      } 
-//AOD
-//AOD    }
-//AOD  }
-
-/*
-/////////////////////////////////
-// Show what was stored (2010) //
-/////////////////////////////////
-
-  // these are the filters with trigger objects filled
-  if (lastFilterIndex < nFilters) {
-
-    // debug
-    outfile << endl << endl << "-> What is stored now:" << endl << endl;
-
-    // get trigger objects & ids
-    const trigger::Vids& triggerIds = triggerEventH_->filterIds(lastFilterIndex);
-    const trigger::Keys& triggerKeys = triggerEventH_->filterKeys(lastFilterIndex);
-    assert( triggerIds.size() == triggerKeys.size() );
-
-    // loop on trigger objects
-    for(unsigned int j = 0; j < triggerKeys.size(); ++j) {
-
-      // trigger p4, p4id
-      const trigger::TriggerObject& triggerObject = triggerObjects[triggerKeys[j]];
-
-      // store trigger id, trigger p4, & trigger object id
-      //p4V.push_back( LorentzVector( triggerObject.particle().p4() ) );
-      //idV.push_back( triggerObject.id() );
-
-      // debug
-      PrintTriggerObjectInfo( outfile, 0, triggerObject.particle().p4() );
-    }
-  }
-*/
 
 } // end HLTMaker::fillTriggerObjectInfo()
 
