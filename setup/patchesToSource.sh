5eeebcd0fe8394f6722924ba4cba97e0764c02c7
cwelke <cwelke@cern.ch>
2015-08-17 02:54:38 -0700

    updates

--- setup/patchesToSource.sh
+++ setup/patchesToSource.sh
@@ -6,16 +6,17 @@
 #cd
 cd $CMSSW_BASE/src/CMS3/NtupleMaker
 
-##Hard-code lepton ID
-#mkdir $CMSSW_BASE/bullshit  
-#mv $CMSSW_BASE/src/* $CMSSW_BASE/bullshit/
-#pushd $CMSSW_BASE/src/
-#git cms-merge-topic ikrav:egm_id_74X_v0
-#git add EgammaAnalysis/PhotonTools/data/PHYS14/effAreaPhotons_cone03_pfPhotons.txt 
-#git commit -m "blah"
-#mv $CMSSW_BASE/bullshit/* $CMSSW_BASE/src/
-#popd
-#rmdir $CMSSW_BASE/bullshit
+# #############
+# # MET tools #
+# #############
+
+mkdir $CMSSW_BASE/previous_code
+mv $CMSSW_BASE/src/* $CMSSW_BASE/previous_code/
+pushd $CMSSW_BASE/src/
+git cms-merge-topic -u cms-met:METCorUnc74X
+mv $CMSSW_BASE/previous_code/* $CMSSW_BASE/src/
+popd
+rmdir $CMSSW_BASE/previous_code
 
 #############
 # MVA JetId #
