8d8ee939d5e29e2cc8cf95016d6a61e5d965a376
cwelke <cwelke@cern.ch>
2015-08-18 02:55:27 -0700

    use rho calculation from METtools to get OTF MET

--- python/fastJetSequence_cff.py
+++ python/fastJetSequence_cff.py
@@ -1,4 +1,4 @@
 import FWCore.ParameterSet.Config as cms
 from CMS3.NtupleMaker.fastJetMaker_cfi import *
 
-miniAODrhoSequence = cms.Sequence ( fixedGridRhoAllMaker * fixedGridRhoFastJetAllMaker * fixedGridRhoFastJetAllCaloMaker * fixedGridRhoFastJetCentralCaloMaker * fixedGridRhoFastJetCentralChargedPileUpMaker * fixedGridRhoFastJetCentralNeutralMaker )
+miniAODrhoSequence = cms.Sequence ( fixedGridRhoAllMaker * fixedGridRhoFastJetAllMakerMETTools * fixedGridRhoFastJetAllMaker * fixedGridRhoFastJetAllCaloMaker * fixedGridRhoFastJetCentralCaloMaker * fixedGridRhoFastJetCentralChargedPileUpMaker * fixedGridRhoFastJetCentralNeutralMaker )
