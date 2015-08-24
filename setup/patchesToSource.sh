05bf247296dbe473892eac2d9b7ca1a0494069c4
wsicheng <sicheng@physics.ucsb.edu>
2015-08-24 13:38:03 -0700

    2nd temp

--- setup/patchesToSource.sh
+++ setup/patchesToSource.sh
@@ -13,7 +13,7 @@ cd $CMSSW_BASE/src/CMS3/NtupleMaker
 mkdir $CMSSW_BASE/previous_code
 mv $CMSSW_BASE/src/* $CMSSW_BASE/previous_code/
 pushd $CMSSW_BASE/src/
-git cms-merge-topic -u cms-met:METCorDev75X-0751514
+git cms-merge-topic -u cms-met:METCorDev75X-071514
 mv $CMSSW_BASE/previous_code/* $CMSSW_BASE/src/
 popd
 rmdir $CMSSW_BASE/previous_code
@@ -22,54 +22,54 @@ rmdir $CMSSW_BASE/previous_code
 # MVA JetId #
 #############
  
-# git clone https://github.com/latinos/UserCode-CMG-CMGTools-External $CMSSW_BASE/src/CMGTools/External
-# pushd $CMSSW_BASE/src/CMGTools/External
-# git checkout V00-03-01
-# rm plugins/PileupJetIdProducer.cc
-# popd
+git clone https://github.com/latinos/UserCode-CMG-CMGTools-External $CMSSW_BASE/src/CMGTools/External
+pushd $CMSSW_BASE/src/CMGTools/External
+git checkout V00-03-01
+rm plugins/PileupJetIdProducer.cc
+popd
 
 
 # #######################
 # # LCG dictionaries #
 # #######################
 
-# git clone https://github.com/cmstas/Dictionaries $CMSSW_BASE/src/CMS3/Dictionaries
+git clone https://github.com/cmstas/Dictionaries $CMSSW_BASE/src/CMS3/Dictionaries
 
 # ####################
 # # jet tool box     #
 # ####################
 
-# git clone https://github.com/cms-jet/JetToolbox $CMSSW_BASE/src/JMEAnalysis/JetToolbox -b jetToolbox_74X
-# pushd $CMSSW_BASE/src/JMEAnalysis/JetToolbox
-# git checkout a80553163684d718e65e44d9b385a4aa11475659
-# popd
+git clone https://github.com/cms-jet/JetToolbox $CMSSW_BASE/src/JMEAnalysis/JetToolbox -b jetToolbox_74X
+pushd $CMSSW_BASE/src/JMEAnalysis/JetToolbox
+git checkout a80553163684d718e65e44d9b385a4aa11475659
+popd
 
-# mkdir $CMSSW_BASE/bullshit  
-# mv $CMSSW_BASE/src/* $CMSSW_BASE/bullshit/
-# git cms-addpkg   RecoEcal/EgammaClusterProducers
-# mv $CMSSW_BASE/bullshit/* $CMSSW_BASE/src/
-# rmdir $CMSSW_BASE/bullshit
+mkdir $CMSSW_BASE/bullshit  
+mv $CMSSW_BASE/src/* $CMSSW_BASE/bullshit/
+git cms-addpkg   RecoEcal/EgammaClusterProducers
+mv $CMSSW_BASE/bullshit/* $CMSSW_BASE/src/
+rmdir $CMSSW_BASE/bullshit
 
-# ####### line needs to be added ###############
-# inputfile="$CMSSW_BASE/src/RecoEcal/EgammaClusterProducers/src/PFECALSuperClusterProducer.cc"
-# grep "desc.setAllowAnything();" $inputfile 2>&1 > /dev/null
-# doNothing=$?
-# if [ ! $doNothing = "0" ]; 
-# then
-#   echo "line does not exist. Adding now."
-#   sed -i 's/edm::ParameterSetDescription desc;/edm::ParameterSetDescription desc;\n  desc.setAllowAnything();/' $inputfile
-# else
-#   echo "line already exists. File $inputfile will be unchanged."
-# fi
+####### line needs to be added ###############
+inputfile="$CMSSW_BASE/src/RecoEcal/EgammaClusterProducers/src/PFECALSuperClusterProducer.cc"
+grep "desc.setAllowAnything();" $inputfile 2>&1 > /dev/null
+doNothing=$?
+if [ ! $doNothing = "0" ]; 
+then
+  echo "line does not exist. Adding now."
+  sed -i 's/edm::ParameterSetDescription desc;/edm::ParameterSetDescription desc;\n  desc.setAllowAnything();/' $inputfile
+else
+  echo "line already exists. File $inputfile will be unchanged."
+fi
 
-# ####################
+####################
 
-# #######################
+#######################
 
-# # run checkdeps
-# printf "\nchecking deps:\n"
-# git cms-checkdeps -a
+# run checkdeps
+printf "\nchecking deps:\n"
+git cms-checkdeps -a
 
-# # compile
-# cd $CMSSW_BASE/src
-# scram b -j 20
+# compile
+cd $CMSSW_BASE/src
+scram b -j 20
