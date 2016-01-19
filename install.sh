99adaf3d8d4a481741ca1c806cb250d087b2de56
mderdzinski <mark.derdzinski@gmail.com>
2016-01-19 09:11:23 -0800

    update CMS3 tag & CMSSW version

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #USER INPUTS
-CMS3Tag=master
-CMSSW_release=CMSSW_7_6_1
+CMS3Tag=76X
+CMSSW_release=CMSSW_7_6_3
 CMSSW_release_name=    #Leave this blank if you don't know what it is.  It's just a marker in case you have multiple identical directories.  Don't forget the underscore!
 export SCRAM_ARCH=slc6_amd64_gcc493
 
