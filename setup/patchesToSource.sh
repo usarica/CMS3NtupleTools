e1e7705a95929704a9549c21e759a67c2188cbe6
Nick Amin <amin.nj@gmail.com>
2017-01-19 08:06:59 -0800

    redo patchestosource

--- setup/patchesToSource.sh
+++ setup/patchesToSource.sh
@@ -10,11 +10,11 @@ cd $CMSSW_BASE/src/CMS3/NtupleMaker
 ## MVA JetId #
 ##############
  
-git clone https://github.com/latinos/UserCode-CMG-CMGTools-External $CMSSW_BASE/src/CMGTools/External
-pushd $CMSSW_BASE/src/CMGTools/External
-git checkout V00-03-01
-rm plugins/PileupJetIdProducer.cc
-popd
+# git clone https://github.com/latinos/UserCode-CMG-CMGTools-External $CMSSW_BASE/src/CMGTools/External
+# pushd $CMSSW_BASE/src/CMGTools/External
+# git checkout V00-03-01
+# rm plugins/PileupJetIdProducer.cc
+# popd
 
 
 # #######################
@@ -27,10 +27,10 @@ git clone https://github.com/cmstas/Dictionaries $CMSSW_BASE/src/CMS3/Dictionari
 # # jet tool box     #
 # ####################
 
-git clone https://github.com/cms-jet/JetToolbox $CMSSW_BASE/src/JMEAnalysis/JetToolbox -b jetToolbox_74X
-pushd $CMSSW_BASE/src/JMEAnalysis/JetToolbox
-git checkout a80553163684d718e65e44d9b385a4aa11475659
-popd
+# git clone https://github.com/cms-jet/JetToolbox $CMSSW_BASE/src/JMEAnalysis/JetToolbox -b jetToolbox_74X
+# pushd $CMSSW_BASE/src/JMEAnalysis/JetToolbox
+# git checkout a80553163684d718e65e44d9b385a4aa11475659
+# popd
 
 mkdir $CMSSW_BASE/bullshit  
 mv $CMSSW_BASE/src/* $CMSSW_BASE/bullshit/
@@ -38,25 +38,38 @@ git cms-addpkg   RecoEcal/EgammaClusterProducers
 mv $CMSSW_BASE/bullshit/* $CMSSW_BASE/src/
 rmdir $CMSSW_BASE/bullshit
 
-####### line needs to be added ###############
-inputfile="$CMSSW_BASE/src/RecoEcal/EgammaClusterProducers/src/PFECALSuperClusterProducer.cc"
-grep "desc.setAllowAnything();" $inputfile 2>&1 > /dev/null
-doNothing=$?
-if [ ! $doNothing = "0" ]; 
-then
-  echo "line does not exist. Adding now."
-  sed -i 's/edm::ParameterSetDescription desc;/edm::ParameterSetDescription desc;\n  desc.setAllowAnything();/' $inputfile
-else
-  echo "line already exists. File $inputfile will be unchanged."
-fi
+# ####### line needs to be added ###############
+# inputfile="$CMSSW_BASE/src/RecoEcal/EgammaClusterProducers/src/PFECALSuperClusterProducer.cc"
+# grep "desc.setAllowAnything();" $inputfile 2>&1 > /dev/null
+# doNothing=$?
+# if [ ! $doNothing = "0" ]; 
+# then
+#   echo "line does not exist. Adding now."
+#   sed -i 's/edm::ParameterSetDescription desc;/edm::ParameterSetDescription desc;\n  desc.setAllowAnything();/' $inputfile
+# else
+#   echo "line already exists. File $inputfile will be unchanged."
+# fi
+
+######################
+## # DeepFlavour # ###
+######################
+#  Btagging DeepFlavour recipe for 80X (from https://twiki.cern.ch/twiki/bin/viewauth/CMS/DeepFlavour)
+pushd $CMSSW_BASE/src
+git cms-merge-topic -u mverzett:DeepFlavour-from-CMSSW_8_0_21
+scram b -j 20
+mkdir -p RecoBTag/DeepFlavour/data/
+cd RecoBTag/DeepFlavour/data/
+wget http://home.fnal.gov/~verzetti//DeepFlavour/training/DeepFlavourNoSL.json
+popd
+### END of DeepFlavour ###
 
 ####################
 
 #######################
 
-# run checkdeps
-printf "\nchecking deps:\n"
-git cms-checkdeps -a
+# # run checkdeps
+# printf "\nchecking deps:\n"
+# git cms-checkdeps -a
 
 ########################
 #  EGM MVA ID 80X (must be run after checkdeps to avoid checking out useless packages)
@@ -81,18 +94,6 @@ cd $CMSSW_BASE/src
 
 ### End of EGM MVA ID 80X ###
 
-######################
-## # DeepFlavour # ###
-######################
-#  Btagging DeepFlavour recipe for 80X (from https://twiki.cern.ch/twiki/bin/viewauth/CMS/DeepFlavour)
-pushd $CMSSW_BASE/src
-git cms-merge-topic -u mverzett:DeepFlavour-from-CMSSW_8_0_21
-scram b -j 20
-mkdir -p RecoBTag/DeepFlavour/data/
-cd RecoBTag/DeepFlavour/data/
-wget http://home.fnal.gov/~verzetti//DeepFlavour/training/DeepFlavourNoSL.json
-popd
-### END of DeepFlavour ###
 
 
 # compile
