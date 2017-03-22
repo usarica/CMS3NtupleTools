6655336be7db938b4a41820435cd38c1f616a4c4
fgolf <frank.golf@gmail.com>
2017-03-21 17:06:05 -0700

    remove unneeded configs

--- python/fastJetExtraSequence_cff.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from CMS3.NtupleMaker.fastJetMaker_cfi import *
-
-
-miniAODrhoSequence = cms.Sequence ( fixedGridRhoAllMaker * fixedGridRhoFastJetAllMaker * fixedGridRhoFastJetCentralCaloMaker * fixedGridRhoFastJetCentralChargedPileUpMaker * fixedGridRhoFastJetCentralNeutralMaker )
