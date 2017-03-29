a71782d82556e8a0252d1c1f1360d04c1d7c99f6
Nick Amin <amin.nj@gmail.com>
2017-03-29 15:35:38 -0700

    optimizations from sicheng

--- src/PFCandidateMaker.cc
+++ src/PFCandidateMaker.cc
@@ -75,12 +75,17 @@ void PFCandidateMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup
   auto_ptr<vector<float>>         pfcands_trackIso            (new vector<float>         );
   auto_ptr<vector<float>>         pfcands_miniTrackIso        (new vector<float>         );
 
+  auto_ptr<vector<float>>         pfcands_helperPhi           (new vector<float>         );
+  auto_ptr<vector<bool>>          pfcands_helperPVinfo        (new vector<bool>         );
+
   //get pfcandidates
   Handle<pat::PackedCandidateCollection> pfCandidatesHandle;
   iEvent.getByToken(pfCandidatesToken, pfCandidatesHandle);
   pfCandidates  = pfCandidatesHandle.product();
 
   for (pat::PackedCandidateCollection::const_iterator pf_it = pfCandidates->begin(); pf_it != pfCandidates->end(); pf_it++) {
+    pfcands_helperPhi    -> push_back( pf_it->p4().Phi() );
+    pfcands_helperPVinfo -> push_back( (!pf_it->vertexRef().isNull() && fabs(pf_it->dz()) < 0.1) || pf_it->fromPV() > 1 );
     if (pf_it->p4().pt() < minPt_ && abs(pf_it->pdgId()) != 11 && abs(pf_it->pdgId()) != 13) continue;
     pfcands_p4   -> push_back( LorentzVector(pf_it->p4()) );
     pfcands_mass -> push_back( pf_it->mass()              );
@@ -104,26 +109,39 @@ void PFCandidateMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup
     pfcands_puppiWeightNoLep -> push_back( pf_it->puppiWeightNoLep() );
     pfcands_isStandAloneMuon -> push_back( pf_it->isStandAloneMuon() );
     pfcands_isGlobalMuon     -> push_back( pf_it->isGlobalMuon()     );
+  }//loop over candidate collection
 
+  for (auto ibegin = pfCandidates->begin(), pf_it = ibegin; pf_it != pfCandidates->end(); pf_it++) {
+    if (pf_it->p4().pt() < minPt_ && abs(pf_it->pdgId()) != 11 && abs(pf_it->pdgId()) != 13) continue;
     // before Isolation are included in miniAOD, do all the iso calculations in 1 loop here
     float absIso = 0.0;
     float miniTkIso = 0.0;
     float miniDR = (pf_it->pt() > 50)? (pf_it->pt() > 200)? 0.05 : 10./pf_it->pt() : 0.2; // compact version of miniDR calculation
-    for (auto itpf = pfCandidates->begin(); itpf != pfCandidates->end(); itpf++) {
+    float baseDR = max(0.3F, miniDR);
+    float thisPhi = pf_it->p4().Phi();
+    float thisEta = pf_it->p4().Eta();
+    for (auto itpf = ibegin; itpf != pfCandidates->end(); itpf++) {
       if (itpf == pf_it) continue;
       // if (itpf->charge() == 0) continue; // skip neutrals
       if (abs(itpf->pdgId()) != 211) continue; // not considering leptons
-      float dr = ROOT::Math::VectorUtil::DeltaR( itpf->p4(), pf_it->p4() );
+      float deltaPhi = fabs((*pfcands_helperPhi)[itpf-ibegin] - thisPhi);
+      if (deltaPhi > M_PI) deltaPhi = fabs(deltaPhi - 2.0*M_PI);
+      if (deltaPhi > baseDR) continue;
+      float deltaEta = fabs(itpf->p4().Eta() - thisEta);
+      if (deltaEta > baseDR) continue;
+      float dr = sqrt(deltaPhi*deltaPhi + deltaEta*deltaEta);
+      if (dr > baseDR) continue;
+
       // use dz acceptance at 0.1, and accept if from PV
-      if (itpf->pt() >= 0.0 && ( itpf->fromPV() > 1 || (!pf_it->vertexRef().isNull() && fabs(itpf->dz()) < 0.1))) {
+      if (itpf->pt() >= 0.0 && (*pfcands_helperPVinfo)[itpf-ibegin]) {
+        // float dr = ROOT::Math::VectorUtil::DeltaR( itpf->p4(), pf_it->p4() );
         if (dr < 0.3) absIso += itpf->pt();            // normal trackIso with cone size 0.3
         if (dr < miniDR) miniTkIso += itpf->pt();      // miniTrackIso
       }
     }
-
     pfcands_trackIso     -> push_back( absIso    );
     pfcands_miniTrackIso -> push_back( miniTkIso );
-  }//loop over candidate collection
+  }
 
   //define the phi bins
   vector<float> phibins;
