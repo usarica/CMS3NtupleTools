833c1378a4e52f1b940c68db591b90d10f8e3619
mderdzinski <mark.derdzinski@gmail.com>
2017-06-01 14:15:10 -0700

    add fixedGridRhoFastJetAllCentralMaker to miniAODrhoSequence

--- python/fastJetSequence_cff.py
+++ python/fastJetSequence_cff.py
@@ -1,5 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 from CMS3.NtupleMaker.fastJetMaker_cfi import *
 
-miniAODrhoSequence = cms.Sequence ( fixedGridRhoAllMaker * fixedGridRhoFastJetAllMaker * fixedGridRhoFastJetAllCaloMaker * fixedGridRhoFastJetCentralCaloMaker * fixedGridRhoFastJetCentralChargedPileUpMaker * fixedGridRhoFastJetCentralNeutralMaker )
+miniAODrhoSequence = cms.Sequence ( fixedGridRhoAllMaker * fixedGridRhoFastJetAllMaker * fixedGridRhoFastJetAllCaloMaker * fixedGridRhoFastJetCentralCaloMaker * fixedGridRhoFastJetCentralChargedPileUpMaker * fixedGridRhoFastJetCentralNeutralMaker * fixedGridRhoFastJetAllCentralMaker)
 
