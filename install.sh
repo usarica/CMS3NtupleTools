9f44bc9a94fba84d53f92fcd0b1e6d6f31cd776d
Nick Amin <amin.nj@gmail.com>
2017-06-26 14:31:34 -0700

    more CORES and better CMSSW

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 #USER INPUTS
 CMS3Tag=diet
-CMSSW_release=CMSSW_9_2_0_patch2
+CMSSW_release=CMSSW_9_2_4
 CMSSW_release_name=    #Leave this blank if you don't know what it is.  It's just a marker in case you have multiple identical directories.  Don't forget the underscore!
 export SCRAM_ARCH=slc6_amd64_gcc530
 
@@ -18,7 +18,7 @@ cd CMS3/NtupleMaker
 git checkout $CMS3Tag
 source setup/patchesToSource.sh
 cd $CMSSW_BASE/src
-scram b -j 20
+scram b -j 25
 cd ..
 
 # see comment in patchesToSource.sh
