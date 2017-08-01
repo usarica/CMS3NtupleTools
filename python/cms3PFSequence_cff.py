02e9a47d72fd058c6d0faaf964d5a9108787e7db
Bennett Marsh <bmarsh@physics.ucsb.edu>
2017-08-01 10:00:18 -0700

    uncommenting isotracks line in config file

--- python/cms3PFSequence_cff.py
+++ python/cms3PFSequence_cff.py
@@ -4,5 +4,5 @@ from CMS3.NtupleMaker.pfJetMaker_cfi import *
 from CMS3.NtupleMaker.pfmetMaker_cfi import *
 from CMS3.NtupleMaker.pftauMaker_cfi import *
 from CMS3.NtupleMaker.pfCandidateMaker_cfi import *
-# from CMS3.NtupleMaker.isoTrackMaker_cfi import *
+from CMS3.NtupleMaker.isoTrackMaker_cfi import *
 # from CMS3.NtupleMaker.T1CHSMETJets_cfi  import *
