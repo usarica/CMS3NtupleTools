aa8051dee8bf21cef3be6d36de1eb8cfd589bf70
Ulascan Sarica <ulascan90@gmail.com>
2019-10-30 19:30:49 -0700

    Remove archives

--- python/archive/recoErrorLogMaker_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-recoErrorLogMaker = cms.EDProducer("RECOErrorLogMaker",
-                              errorSummaryCollInputTag = cms.InputTag("logErrorHarvester"),
-                              #supported severity is warning, error
-                              #recommended is error to keep the event size down
-                              minSeverity             = cms.string("error"), 
-)
