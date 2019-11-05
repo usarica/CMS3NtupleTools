ab2111f8840fe6f863edcb7b85fa0bce572c25f5
Ulascan Sarica <ulascan90@gmail.com>
2019-11-05 12:07:15 -0800

    Add pfjetmaker changes

--- plugins/PFJetMaker.cc
+++ plugins/PFJetMaker.cc
@@ -1,13 +1,3 @@
-//-*- C++ -*-
-//
-// Package:    NtupleMaker
-// Class:      PFJetMaker
-//
-//*\class PFJetMaker PFJetMaker.cc CMS3/NtupleMakerMaker/src/PFJetMaker.cc
-//
-// Original Author:  Puneeth Kalavase
-//         Created:  Fri Jun  6 11:07:38 CDT 2008
-
 #include "FWCore/Framework/interface/EventSetup.h"
 #include "FWCore/Framework/interface/ESHandle.h"
 #include "DataFormats/Math/interface/LorentzVector.h"
@@ -22,156 +12,108 @@
 
 typedef math::XYZTLorentzVectorF LorentzVector;
 
-// Constructor
+using namespace std;
+using namespace edm;
+using namespace reco;
+
+
 PFJetMaker::PFJetMaker(const edm::ParameterSet& iConfig) :
-  aliasprefix_(iConfig.getUntrackedParameter<std::string>("aliasPrefix")) {
-    using namespace std;
-    using namespace edm;
+  aliasprefix_(iConfig.getUntrackedParameter<std::string>("aliasprefix"))
+{
+  pfJetsToken = consumes< edm::View<pat::Jet> >(iConfig.getParameter<edm::InputTag>("pfJetsInputTag"));
+  pfCandidatesToken = consumes<pat::PackedCandidateCollection>(iConfig.getParameter<edm::InputTag>("pfCandidatesTag"));
 
-    pfCandidatesToken = consumes<pat::PackedCandidateCollection>(iConfig.getParameter<edm::InputTag>("pfCandidatesTag"));
-    pfJetsToken = consumes<edm::View<pat::Jet> >(iConfig.getParameter<edm::InputTag>("pfJetsInputTag"));
-
-    aliasprefix_ = iConfig.getUntrackedParameter<std::string>("aliasPrefix");
-
-
-    produces<pat::JetCollection>().setBranchAlias(aliasprefix_);
-  }
-
-// Destructor
-PFJetMaker::~PFJetMaker(){
+  produces<pat::JetCollection>().setBranchAlias(aliasprefix_);
 }
 
-// ------------ method called once each job just before starting event loop  ------------
-void PFJetMaker::beginJob() {}
+PFJetMaker::~PFJetMaker(){}
 
-// ------------ method called once each job just after ending the event loop  ------------
-void PFJetMaker::endJob() {}
+void PFJetMaker::beginJob(){}
+void PFJetMaker::endJob(){}
 
 void PFJetMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup){
-
-  using namespace std;
-  using namespace edm;
-  using namespace reco;
-
   auto result = std::make_unique<pat::JetCollection>();
 
-  // create containers
-  unique_ptr<vector<LorentzVector> > pfjets_p4                        (new vector<LorentzVector>  );
-  unique_ptr<vector<float> >         pfjets_undoJEC                   (new vector<float>          );
-  unique_ptr<vector<float> >         pfjets_chargedHadronE            (new vector<float>          );
-  unique_ptr<vector<float> >         pfjets_neutralHadronE            (new vector<float>          );
-  unique_ptr<vector<float> >         pfjets_chargedEmE                (new vector<float>          );
-  unique_ptr<vector<float> >         pfjets_neutralEmE                (new vector<float>          );
-  unique_ptr<vector<float> >         pfjets_photonE                   (new vector<float>          );
-  unique_ptr<vector<float> >         pfjets_electronE                 (new vector<float>          );
-  unique_ptr<vector<float> >         pfjets_muonE                     (new vector<float>          );
-  unique_ptr<vector<float> >         pfjets_hfHadronE                 (new vector<float>          );
-  unique_ptr<vector<float> >         pfjets_hfEmE                     (new vector<float>          );
-  unique_ptr<vector<int>   >         pfjets_chargedHadronMultiplicity (new vector<int>            );
-  unique_ptr<vector<int>   >         pfjets_neutralHadronMultiplicity (new vector<int>            );
-  unique_ptr<vector<int>   >         pfjets_chargedMultiplicity       (new vector<int>            );
-  unique_ptr<vector<int>   >         pfjets_neutralMultiplicity       (new vector<int>            );
-  unique_ptr<vector<int>   >         pfjets_photonMultiplicity        (new vector<int>            );
-  unique_ptr<vector<int>   >         pfjets_electronMultiplicity      (new vector<int>            );
-  unique_ptr<vector<int>   >         pfjets_muonMultiplicity          (new vector<int>            );
-  unique_ptr<vector<float> >         pfjets_area                      (new vector<float>          );
-  unique_ptr<vector<float> >         pfjets_pileupJetId               (new vector<float>          );
-  unique_ptr<vector<int>   >         pfjets_partonFlavour             (new vector<int>            );
-  unique_ptr<vector<int>   >         pfjets_hadronFlavour             (new vector<int>            );
-  unique_ptr<vector<int>   >         pfjets_npfcands                  (new vector<int>            );
-  unique_ptr<vector<int>   >         pfjets_totalMultiplicity         (new vector<int>            );
-  unique_ptr<vector<float> >         pfjets_ptDistribution            (new vector<float>          );
-  unique_ptr<vector<float> >         pfjets_axis1                     (new vector<float>          );
-  unique_ptr<vector<float> >         pfjets_axis2                     (new vector<float>          );
-  // unique_ptr<vector<vector<LorentzVector> > > pfjets_pfcandmup4       (new vector<vector<LorentzVector> > );
-
-  unique_ptr<vector<float> >           pfjets_pfCombinedInclusiveSecondaryVertexV2BJetTag (new vector<float>  );
-  unique_ptr<vector<float> >           pfjets_pfDeepCSVJetTagsprobbPlusprobbb             (new vector<float>  );
-  unique_ptr<vector<TString> >         pfjets_bDiscriminatorNames                         (new vector<TString>        );
-  unique_ptr<vector<vector<float> > >  pfjets_bDiscriminators                             (new vector<vector<float> > );
-
-  //get pfcandidates
-  Handle<pat::PackedCandidateCollection> pfCandidatesHandle;
+  edm::Handle<pat::PackedCandidateCollection> pfCandidatesHandle;
   iEvent.getByToken(pfCandidatesToken, pfCandidatesHandle);
-  pfCandidates  = pfCandidatesHandle.product();
+  const pat::PackedCandidateCollection* pfCandidates = pfCandidatesHandle.product();
 
-  //PfJets
-  Handle<View<pat::Jet> > pfJetsHandle;
+  edm::Handle< edm::View<pat::Jet> > pfJetsHandle;
   iEvent.getByToken(pfJetsToken, pfJetsHandle);
 
-  for(View<pat::Jet>::const_iterator pfjet_it = pfJetsHandle->begin(); pfjet_it != pfJetsHandle->end(); pfjet_it++) {
-
+  result.reserve(pfJetsHandle->size());
+  for (edm::View<pat::Jet>::const_iterator pfjet_it = pfJetsHandle->begin(); pfjet_it != pfJetsHandle->end(); pfjet_it++){
     pat::Jet jet_result(*pfjet_it);
 
-    jet_result.addUserFloat("pt", pfjet_it->pt());
-    jet_result.addUserFloat("eta", pfjet_it->eta());
-    jet_result.addUserFloat("phi", pfjet_it->phi());
-    jet_result.addUserFloat("mass", pfjet_it->mass());
+    float undoJEC = pfjet_it->jecFactor("Uncorrected");
+    jet_result.addUserFloat("undoJEC", undoJEC);
 
-    jet_result.addUserFloat("undoJEC", pfjet_it->jecFactor("Uncorrected") );
-    jet_result.addUserFloat("chargedHadronE", pfjet_it->chargedHadronEnergy() );
-    jet_result.addUserFloat("neutralHadronE", pfjet_it->neutralHadronEnergy() );
-    jet_result.addUserFloat("chargedEmE", pfjet_it->chargedEmEnergy() );
-    jet_result.addUserFloat("neutralEmE", pfjet_it->neutralEmEnergy() );
-    jet_result.addUserFloat("photonE", pfjet_it->photonEnergy() );
-    jet_result.addUserFloat("electronE", pfjet_it->electronEnergy() );
-    jet_result.addUserFloat("muonE", pfjet_it->muonEnergy() );
-    jet_result.addUserFloat("hfHadronE", pfjet_it->HFHadronEnergy() );
-    jet_result.addUserFloat("hfEmE", pfjet_it->HFEMEnergy() );
-    jet_result.addUserInt("chargedMultiplicity", pfjet_it->chargedMultiplicity() );
-    jet_result.addUserInt("neutralMultiplicity", pfjet_it->neutralMultiplicity() );
-    jet_result.addUserInt("chargedHadronMultiplicity", pfjet_it->chargedHadronMultiplicity() );
-    jet_result.addUserInt("neutralHadronMultiplicity", pfjet_it->neutralHadronMultiplicity() );
-    jet_result.addUserInt("photonMultiplicity", pfjet_it->photonMultiplicity() );
-    jet_result.addUserInt("electronMultiplicity", pfjet_it->electronMultiplicity() );
-    jet_result.addUserInt("muonMultiplicity", pfjet_it->muonMultiplicity() );
-    jet_result.addUserFloat("area",pfjet_it->jetArea() );
+    jet_result.addUserInt("chargedMultiplicity", pfjet_it->chargedMultiplicity());
+    jet_result.addUserInt("neutralMultiplicity", pfjet_it->neutralMultiplicity());
+    jet_result.addUserInt("chargedHadronMultiplicity", pfjet_it->chargedHadronMultiplicity());
+    jet_result.addUserInt("neutralHadronMultiplicity", pfjet_it->neutralHadronMultiplicity());
+    jet_result.addUserInt("photonMultiplicity", pfjet_it->photonMultiplicity());
+    jet_result.addUserInt("electronMultiplicity", pfjet_it->electronMultiplicity());
+    jet_result.addUserInt("muonMultiplicity", pfjet_it->muonMultiplicity());
+
+    jet_result.addUserFloat("chargedHadronEnergy", pfjet_it->chargedHadronEnergy());
+    jet_result.addUserFloat("neutralHadronEnergy", pfjet_it->neutralHadronEnergy());
+    jet_result.addUserFloat("chargedEmEnergy", pfjet_it->chargedEmEnergy());
+    jet_result.addUserFloat("neutralEmEnergy", pfjet_it->neutralEmEnergy());
+    jet_result.addUserFloat("photonEnergy", pfjet_it->photonEnergy());
+    jet_result.addUserFloat("electronEnergy", pfjet_it->electronEnergy());
+    jet_result.addUserFloat("muonEnergy", pfjet_it->muonEnergy());
+    jet_result.addUserFloat("hfHadronEnergy", pfjet_it->HFHadronEnergy());
+    jet_result.addUserFloat("hfEmEnergy", pfjet_it->HFEMEnergy());
+    jet_result.addUserFloat("area", pfjet_it->jetArea());
+
     float pileupJetId = -999;
-    if ( pfjet_it->hasUserFloat("pileupJetId:fullDiscriminant") ) pileupJetId = pfjet_it->userFloat("pileupJetId:fullDiscriminant");
-    if ( pfjet_it->hasUserFloat("fullDiscriminant") ) pileupJetId = pfjet_it->userFloat("fullDiscriminant");
-    jet_result.addUserFloat("pileupJetId", pileupJetId );
-    jet_result.addUserInt("partonFlavour",pfjet_it->partonFlavour() );
-    jet_result.addUserInt("hadronFlavour",pfjet_it->hadronFlavour() );
+    if (pfjet_it->hasUserFloat("pileupJetId:fullDiscriminant")) pileupJetId = pfjet_it->userFloat("pileupJetId:fullDiscriminant");
+    if (pfjet_it->hasUserFloat("fullDiscriminant")) pileupJetId = pfjet_it->userFloat("fullDiscriminant");
+    jet_result.addUserFloat("pileupJetId", pileupJetId);
+    jet_result.addUserInt("partonFlavour", pfjet_it->partonFlavour());
+    jet_result.addUserInt("hadronFlavour", pfjet_it->hadronFlavour());
 
     // CSVv2
     jet_result.addUserFloat("btagCSVV2", pfjet_it->bDiscriminator("pfCombinedInclusiveSecondaryVertexV2BJetTags"));
 
     // DeepCSV
-    jet_result.addUserFloat("btagDeepCSVprobb", pfjet_it->bDiscriminator("pfDeepCSVJetTags:probb"));
-    jet_result.addUserFloat("btagDeepCSVprobbb", pfjet_it->bDiscriminator("pfDeepCSVJetTags:probbb"));
-    jet_result.addUserFloat("btagDeepCSVprobc", pfjet_it->bDiscriminator("pfDeepCSVJetTags:probc"));
-    jet_result.addUserFloat("btagDeepCSVprobudsg", pfjet_it->bDiscriminator("pfDeepCSVJetTags:probudsg"));
+    jet_result.addUserFloat("deepCSVprobb", pfjet_it->bDiscriminator("pfDeepCSVJetTags:probb"));
+    jet_result.addUserFloat("deepCSVprobbb", pfjet_it->bDiscriminator("pfDeepCSVJetTags:probbb"));
+    jet_result.addUserFloat("deepCSVprobc", pfjet_it->bDiscriminator("pfDeepCSVJetTags:probc"));
+    jet_result.addUserFloat("deepCSVprobudsg", pfjet_it->bDiscriminator("pfDeepCSVJetTags:probudsg"));
 
     // DeepFlavour (note: non-existent/gives dummy values for 2016 80X miniAOD v2)
-    jet_result.addUserFloat("btagDeepFlavprobc", pfjet_it->bDiscriminator("pfDeepFlavourJetTags:probc"));
-    jet_result.addUserFloat("btagDeepFlavprobbb", pfjet_it->bDiscriminator("pfDeepFlavourJetTags:probbb"));
-    jet_result.addUserFloat("btagDeepFlavprobb", pfjet_it->bDiscriminator("pfDeepFlavourJetTags:probb"));
-    jet_result.addUserFloat("btagDeepFlavprobg", pfjet_it->bDiscriminator("pfDeepFlavourJetTags:probg"));
-    jet_result.addUserFloat("btagDeepFlavproblepb", pfjet_it->bDiscriminator("pfDeepFlavourJetTags:problepb"));
-    jet_result.addUserFloat("btagDeepFlavprobuds", pfjet_it->bDiscriminator("pfDeepFlavourJetTags:probuds"));
+    jet_result.addUserFloat("deepFlavourprobb", pfjet_it->bDiscriminator("pfDeepFlavourJetTags:probb"));
+    jet_result.addUserFloat("deepFlavourprobbb", pfjet_it->bDiscriminator("pfDeepFlavourJetTags:probbb"));
+    jet_result.addUserFloat("deepFlavourprobc", pfjet_it->bDiscriminator("pfDeepFlavourJetTags:probc"));
+    jet_result.addUserFloat("deepFlavourprobg", pfjet_it->bDiscriminator("pfDeepFlavourJetTags:probg"));
+    jet_result.addUserFloat("deepFlavourproblepb", pfjet_it->bDiscriminator("pfDeepFlavourJetTags:problepb"));
+    jet_result.addUserFloat("deepFlavourprobuds", pfjet_it->bDiscriminator("pfDeepFlavourJetTags:probuds"));
 
     std::vector <reco::CandidatePtr> pfjet_cands = pfjet_it->daughterPtrVector();
-    jet_result.addUserInt("npfcands",pfjet_cands.size());
+    jet_result.addUserInt("npfcands", pfjet_cands.size());
 
     // Do calculation of top-tagger variables
-    const bool computeTopTaggerVariables = true;
-    if (computeTopTaggerVariables) {
+    constexpr bool computeTopTaggerVariables = true;
+    if (computeTopTaggerVariables){
       int totalMult = 0;
       float ptD     = 0;
       float axis1   = 0;
       float axis2   = 0;
-      if (pfjet_it->numberOfDaughters() != 0) {
-        float sum_weight(0.0), sum_dEta(0.0), sum_dPhi(0.0), sum_dEta2(0.0), sum_dPhi2(0.0), sum_dEta_dPhi(0.0), sum_pt(0.0);
+      if (pfjet_it->numberOfDaughters() != 0){
+        float sum_weight(0), sum_dEta(0), sum_dPhi(0), sum_dEta2(0), sum_dPhi2(0), sum_dEta_dPhi(0), sum_pt(0);
 
         // loop over the jet constituents (packed candidate situation)
-        for (auto part : pfjet_it->getJetConstituentsQuick()) {
-          if (part->charge()) { // charged particles
+        for (auto part : pfjet_it->getJetConstituentsQuick()){
+          if (part->charge()){ // charged particles
             auto p = dynamic_cast<const pat::PackedCandidate*>(part);
-            if (!p) std::cout << "ERROR: QGTagging variables cannot be computed for these jets!" << std::endl;
+            if (!p){ std::cout << "ERROR: QGTagging variables cannot be computed for these jets!" << std::endl; continue; }
             if (!(p->fromPV() > 1 && p->trackHighPurity())) continue;
             ++totalMult;
-          } else { // neutral particles
-            if (part->pt() < 1.0) continue;
+          }
+          else{ // neutral particles
+            if (part->pt() < 1.f) continue;
             ++totalMult;
           } // charged, neutral particles
 
@@ -187,10 +129,10 @@ void PFJetMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup){
           sum_dEta2     += dEta*dEta * weight;
           sum_dEta_dPhi += dEta*dPhi * weight;
           sum_dPhi2     += dPhi*dPhi * weight;
-        } // pfjet_it->getJetConstituentsQuick()
+        }
 
         // calculate axis2 and ptD
-        if (sum_weight > 0) {
+        if (sum_weight > 0.f){
           ptD = sqrt(sum_weight)/sum_pt;
           float ave_dEta  = sum_dEta  / sum_weight;
           float ave_dPhi  = sum_dPhi  / sum_weight;
@@ -199,33 +141,31 @@ void PFJetMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup){
           float a = ave_dEta2 - ave_dEta*ave_dEta;
           float b = ave_dPhi2 - ave_dPhi*ave_dPhi;
           float c = -(sum_dEta_dPhi/sum_weight - ave_dEta*ave_dPhi);
-          float delta = sqrt(fabs( (a-b)*(a-b) + 4*c*c ));
-          if(a+b-delta > 0) axis2 = sqrt(0.5*(a+b-delta));
-          else              axis2 = 0.0;
-          if(a+b+delta > 0) axis1 = sqrt(0.5*(a+b+delta));
-          else              axis1 = 0.0;
+          float delta = sqrt(fabs((a-b)*(a-b) + 4.*c*c));
+          if (a+b-delta > 0.f) axis2 = sqrt(0.5*(a+b-delta));
+          else                 axis2 = 0;
+          if (a+b+delta > 0.f) axis1 = sqrt(0.5*(a+b+delta));
+          else                 axis1 = 0;
         }
       }
 
-      jet_result.addUserFloat("totalMultiplicity",totalMult);
-      jet_result.addUserFloat("ptDistribution",ptD);
-      jet_result.addUserFloat("axis1",axis1);
-      jet_result.addUserFloat("axis2",axis2);
-    } else {
-      jet_result.addUserFloat("ptDistribution",pfjet_it->constituentPtDistribution());
-      jet_result.addUserFloat("totalMultiplicity",pfjet_it->numberOfDaughters());
-      jet_result.addUserFloat("axis1",-1);
-      jet_result.addUserFloat("axis2",-1);
+      jet_result.addUserFloat("ptDistribution", ptD);
+      jet_result.addUserFloat("totalMultiplicity", totalMult);
+      jet_result.addUserFloat("axis1", axis1);
+      jet_result.addUserFloat("axis2", axis2);
+    }
+    else{
+      jet_result.addUserFloat("ptDistribution", pfjet_it->constituentPtDistribution());
+      jet_result.addUserFloat("totalMultiplicity", pfjet_it->numberOfDaughters());
+      jet_result.addUserFloat("axis1", -1);
+      jet_result.addUserFloat("axis2", -1);
     }
 
     result->emplace_back(jet_result);
-
   }
 
   iEvent.put(std::move(result));
-
 }
 
-//define this as a plug-in
+
 DEFINE_FWK_MODULE(PFJetMaker);
-// vim: ts=2:sw=2
--- python/pfJetMaker_cfi.py
+++ python/pfJetMaker_cfi.py
@@ -1,14 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
 pfJetMaker = cms.EDProducer("PFJetMaker",
-  aliasPrefix = cms.untracked.string("pfjets"),
+  aliasprefix = cms.untracked.string("pfjets"),
   pfJetsInputTag                   = cms.InputTag("slimmedJets"),
   pfJetPtCut                       = cms.double(5.),
   pfCandidatesTag     = cms.InputTag("packedPFCandidates")
 )
 
 pfJetPUPPIMaker = cms.EDProducer("PFJetMaker",
-  aliasPrefix = cms.untracked.string("pfjets_puppi"),
+  aliasprefix = cms.untracked.string("pfjets_puppi"),
   pfJetsInputTag                   = cms.InputTag("slimmedJetsPuppi"),
   pfJetPtCut                       = cms.double(20.), #miniAOD doesn't go lower than 20
   pfCandidatesTag     = cms.InputTag("packedPFCandidates")
