6a7238657504a80e9bbd043543fb355ad7eec8ae
Ulascan Sarica <ulascan90@gmail.com>
2019-11-15 13:12:46 -0800

    Fix setup

--- install.sh
+++ install.sh
@@ -10,14 +10,14 @@ export SCRAM_ARCH=slc6_amd64_gcc700
 export CMS_PATH=/cvmfs/cms.cern.ch
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 scramv1 p -n ${CMSSW_release}${CMSSW_release_name} CMSSW $CMSSW_release
-cd ${CMSSW_release}${CMSSW_release_name}
+cd ${CMSSW_release}${CMSSW_release_name}/src
 eval $(scramv1 runtime -sh)
 
 # new upstream-only ignores user's cmssw, but makes cms-init much, much faster
 git cms-init --upstream-only
 
-#Preliminary electron scale and smearing corrections according to https://twiki.cern.ch/twiki/bin/view/CMS/EgammaPostRecoRecipes#2018_Preliminary_Energy_Correcti
-#We need the ElectronTools package to calculate smear and scale uncertainties so just download the ScaleAndSmearing files manualy 
+# Preliminary electron scale and smearing corrections according to https://twiki.cern.ch/twiki/bin/view/CMS/EgammaPostRecoRecipes#2018_Preliminary_Energy_Correcti
+# We need the ElectronTools package to calculate smear and scale uncertainties so just download the ScaleAndSmearing files manualy 
 git cms-merge-topic cms-egamma:EgammaPostRecoTools
 git cms-merge-topic cms-egamma:PhotonIDValueMapSpeedup1029
 git cms-merge-topic cms-egamma:slava77-btvDictFix_10210
@@ -28,27 +28,57 @@ git cms-addpkg EgammaAnalysis/ElectronTools
 # Twiki: https://twiki.cern.ch/twiki/bin/view/CMS/MissingETUncertaintyPrescription#Instructions_for_9_4_X_X_0_for_M
 git cms-merge-topic cms-met:METFixEE2017_949_v2_backport_to_102X
 
-# pT-dependent JERs and phi-dependent JECs
-#git cherry-pick 09e9a17cb26020bc507dffec70137bacc6f124c6 # This is the commit id directly from cmssw
-
 ## For reading 2 electron iso branches in 102X from 94X sample
 ## https://github.com/cms-sw/cmssw/issues/25573
 ## NOTE this should be taken out once merged/backported into CMSSW
 ## Merged after CMSSW_10_2_11
 #git cms-merge-topic Sam-Harper:IORulesForPFClusIso_1025
 
-cd src
+# e/gamma cluster production
+git cms-addpkg RecoEcal/EgammaClusterProducers
+
+# pT-dependent JERs and phi-dependent JECs
+## git cherry-pick 09e9a17cb26020bc507dffec70137bacc6f124c6 # This is the commit id directly from cmssw
+git cms-addpkg PhysicsTools/PatUtils
+git cms-addpkg PhysicsTools/PatAlgos
+# Need to replace two files from a commit present in CMSSW 11. Cannot do cms-merge-topic or simple cherry-pick.
+(
+  curl -O -L https://github.com/cms-sw/cmssw/raw/09e9a17cb26020bc507dffec70137bacc6f124c6/PhysicsTools/PatUtils/interface/SmearedJetProducerT.h
+  curl -O -L https://github.com/cms-sw/cmssw/raw/09e9a17cb26020bc507dffec70137bacc6f124c6/PhysicsTools/PatAlgos/plugins/JetCorrFactorsProducer.cc
+  mv SmearedJetProducerT.h PhysicsTools/PatUtils/interface/
+  mv JetCorrFactorsProducer.cc PhysicsTools/PatAlgos/plugins/
+)
+
+
+#######################################
+# No CMSSW packages beyond this point #
+#######################################
 
 git clone git@github.com:usarica/CMS3-NtupleMaker.git CMS3/NtupleMaker
-(cd CMS3/NtupleMaker; git checkout ${CMS3Tag}; )
+(
+  cd CMS3/NtupleMaker; git checkout ${CMS3Tag}; cd -
+  cd $CMSSW_BASE/src/CMS3/NtupleMaker/data/JECs ; . download.sh; cd -
+  cd $CMSSW_BASE/src/CMS3/NtupleMaker/data/JERs ; . download.sh; cd -
+)
 
 git clone git@github.com:cmstas/Dictionaries CMS3/Dictionaries
 
-mkdir $CMSSW_BASE/bullshit
-mv $CMSSW_BASE/src/* $CMSSW_BASE/bullshit/
-git cms-addpkg RecoEcal/EgammaClusterProducers
-mv $CMSSW_BASE/bullshit/* $CMSSW_BASE/src/
-rmdir $CMSSW_BASE/bullshit
+# MELA
+git clone git@github.com:cms-analysis/HiggsAnalysis-ZZMatrixElement.git ZZMatrixElement
+(cd ZZMatrixElement; source setup.sh -j;)
+#(cd ZZMatrixElement; git fetch; git checkout -b from-v222 v2.2.2; source setup.sh -j;)
+
+# MELA Analytics
+git clone git@github.com:usarica/MelaAnalytics.git
+#(cd MelaAnalytics; git fetch; git checkout -b from-v19 v1.9)
+
+# Common LHE tools
+git clone git@github.com:usarica/CommonLHETools.git
+#(cd CommonLHETools; git fetch; git checkout -b from-v132 v1.3.2)
+
+# CMSDataTools
+git clone git@github.com:usarica/CMSDataTools.git
+#(cd CMSDataTools; git fetch; git checkout -b from-v111 v1.1.1)
 
 #########################
 #  DeepAK8 fat jet tagger
@@ -68,36 +98,7 @@ scram setup mxnet-predict
 # cp NNKit/data/ak8/*.{json,params} $CMSSW_BASE/src/CMS3/NtupleMaker/test/
 # #######################
 
-
-#######################################
-# No CMSSW packages beyond this point #
-#######################################
-
-# MELA
-git clone git@github.com:cms-analysis/HiggsAnalysis-ZZMatrixElement.git ZZMatrixElement
-(cd ZZMatrixElement; source setup.sh -j;)
-#(cd ZZMatrixElement; git fetch; git checkout -b from-v222 v2.2.2; source setup.sh -j;)
-
-# MELA Analytics
-git clone git@github.com:usarica/MelaAnalytics.git
-#(cd MelaAnalytics; git fetch; git checkout -b from-v19 v1.9)
-
-# Common LHE tools
-git clone git@github.com:usarica/CommonLHETools.git
-#(cd CommonLHETools; git fetch; git checkout -b from-v132 v1.3.2)
-
-# CMSDataTools
-git clone git@github.com:usarica/CMSDataTools.git
-#(cd CMSDataTools; git fetch; git checkout -b from-v111 v1.1.1)
-
-(cd $CMSSW_BASE/src/CMS3/NtupleMaker/data/JECs ; . download.sh)
-
-
-cd $CMSSW_BASE/src
 scram b -j
-cd ..
 
 # see comment in patchesToSource.sh
 rm $CMSSW_BASE/lib/$SCRAM_ARCH/.poisonededmplugincache
-
-#cd $CMSSW_BASE/src/CMS3/NtupleMaker/test/
