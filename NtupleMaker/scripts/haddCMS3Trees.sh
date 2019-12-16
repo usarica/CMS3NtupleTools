7539e13e82d31b676b1f7eab62e52bbdf13bf5a7
Ulascan Sarica <ulascan90@gmail.com>
2019-12-15 20:22:57 -0800

    Add overwrite option to hadd

--- NtupleMaker/scripts/haddCMS3Trees.sh
+++ NtupleMaker/scripts/haddCMS3Trees.sh
@@ -3,6 +3,7 @@
 JOBSDIR=""
 OUTDIR=""
 NCORES="12"
+let OVERWRITE=0
 let printhelp=0
 for fargo in "$@";do
   fcnargname=""
@@ -20,6 +21,17 @@ for fargo in "$@";do
     fcnargname="$farg"
     fcnargname="${fcnargname#*=}"
     NCORES="$fcnargname"
+  elif [[ "$fargl" == "overwrite="* ]];then
+    fcnargname="$farg"
+    fcnargname="${fcnargname#*=}"
+    fcnargname="$(echo $fcnargname | awk '{print tolower($0)}')"
+    if [[ -z "$fcnargname" ]] || [[ "$fcnargname" == "t"* ]] || [[ "$fcnargname" == "y"* ]] || [[ "$fcnargname" == "1" ]];then
+      let OVERWRITE=1
+    elif [[ "$fcnargname" == "f"* ]] || [[ "$fcnargname" == "n"* ]] || [[ "$fcnargname" == "0" ]];then
+      let OVERWRITE=0
+    else
+      let printhelp=1
+    fi
   elif [[ "$fargl" == "help" ]];then
     let printhelp=1
   fi
@@ -30,15 +42,25 @@ if [[ $printhelp -eq 1 ]] || [[ -z "$JOBSDIR" ]] || [[ -z "$OUTDIR" ]]; then
   echo " - jobsdir: Job submission directory. Mandatory."
   echo " - outdir: Main output location. Mandatory."
   echo " - ncores: Number of cores for hadd (Default: 12)"
+  echo " - overwrite: Flag to specify whether the output file should be overwritten. For true value, specify either nothing after the option, or t*/y*/1. For false value, either do not specify this option, or put f*/n*/0. (Default: false)"
   exit 0
 fi
 
 
 for INDIR in $(ls ${JOBSDIR}); do
   for d in $(ls ${JOBSDIR}/${INDIR}); do
-    pushd ${JOBSDIR}/${INDIR}/${d}
+    JOBSINDIR=${JOBSDIR}/${INDIR}/${d}
     RECDIR=${OUTDIR}/${INDIR}/${d}
-    mkdir -p ${OUTDIR}/${INDIR}/${d}
+    OUTFILE=${RECDIR}/allevents.root
+
+    if [[ $OVERWRITE -eq 0 ]] && [[ -s ${OUTFILE} ]];then
+      echo "${OUTFILE} exists. Skipping..."
+      continue
+    fi
+
+    mkdir -p ${RECDIR}
+
+    pushd ${JOBSINDIR}
 
     flist=""
     for f in $(ls ./ | grep -e ".root" | grep -v allevents.root); do
@@ -49,7 +71,7 @@ for INDIR in $(ls ${JOBSDIR}); do
       fi
     done
 
-    hadd -ff -O -n 0 -j ${NCORES} ${RECDIR}/allevents.root $flist
+    hadd -ff -O -n 0 -j ${NCORES} ${OUTFILE} $flist
 
     popd
   done
