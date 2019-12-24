af86fe7e3da2c3bfb103d1b7ad9c8dd4c67cffeb
Ulascan Sarica <ulascan90@gmail.com>
2019-12-24 02:33:36 -0800

    Add capability to loop over multiple files

--- AnalysisTree/bin/trimCMS3Production.cc
+++ AnalysisTree/bin/trimCMS3Production.cc
@@ -5,6 +5,7 @@
 #include "TChain.h"
 #include "TTree.h"
 #include "TString.h"
+#include "SampleHelpersCore.h"
 #include "HelperFunctions.h"
 
 
@@ -43,6 +44,11 @@ void trimCMS3Production(TString infile, TString outfile, TString strfilter="(@mu
 int main(int argc, char** argv){
   if (argc!=4) return 1;
   cout << "Running " << argv[0] << "(\"" << argv[1] << "\", \"" << argv[2] << "\", \"" << argv[3] << "\")" << endl;
-  trimCMS3Production(argv[1], argv[2], argv[3]);
+  std::vector<TString> filelist = SampleHelpers::lsdir(argv[1]);
+  for (auto const& file:filelist){
+    TString cinput = argv[1]; cinput += "/" + file;
+    TString coutput = argv[2]; coutput += "/" + file;
+    trimCMS3Production(cinput, coutput, argv[3]);
+  }
   return 0;
 }
