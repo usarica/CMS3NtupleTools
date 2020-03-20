89292b99d221e8e55307b86b4f2457ffba7d35fc
Ulascan Sarica <ulascan90@gmail.com>
2020-03-20 15:50:56 -0700

    More triggers

--- AnalysisTree/interface/OffshellTriggerHelpers.h
+++ AnalysisTree/interface/OffshellTriggerHelpers.h
@@ -6,13 +6,24 @@
 
 namespace OffshellTriggerHelpers{
   enum TriggerType{
-    kDoubleMu=0,
+    kTripleLep=0,
+
+    kDoubleMu,
     kDoubleEle,
     kMuEle,
+
     kSingleMu,
     kSingleEle,
     kSinglePho,
-    kTripleLep,
+
+    kSingleMu_Control,
+    kSingleMu_Control_HighPt,
+    kSingleEle_Control,
+
+    kAK8PFJet_Control,
+    kPFHT_Control,
+    kPFMET_MHT_Control,
+
     nTriggerTypes
   };
 
