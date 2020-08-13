69fe2c363e50f098c967121ef707bbce8de15e8d
Ulascan Sarica <ulascan90@gmail.com>
2020-08-13 08:38:23 -0700

    Fix good vertex selection

--- NtupleMaker/src/VertexSelectionHelpers.cc
+++ NtupleMaker/src/VertexSelectionHelpers.cc
@@ -2,5 +2,5 @@
 
 
 bool VertexSelectionHelpers::testGoodVertex(reco::Vertex const& vtx){
-  return (vtx.isValid() && !vtx.isFake() && vtx.ndof()>vtx_ndof_thr && vtx.position().Rho()<=vtx_rho_thr && vtx.position().Z()<=vtx_z_thr);
+  return (vtx.isValid() && !vtx.isFake() && vtx.ndof()>vtx_ndof_thr && vtx.position().Rho()<=vtx_rho_thr && std::abs(vtx.position().Z())<=vtx_z_thr);
 }
