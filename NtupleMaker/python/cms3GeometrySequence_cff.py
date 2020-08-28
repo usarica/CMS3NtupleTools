8c9071efb6e5c29733e910a7cc7a729670181c19
Ulascan Sarica <ulascan90@gmail.com>
2020-08-28 16:08:33 -0700

    Geometry cfg for use in future

---
+++ NtupleMaker/python/cms3GeometrySequence_cff.py
@@ -0,0 +1,5 @@
+# Includes for geometry operations
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.GeometryDB_cff import *
+from Geometry.CaloEventSetup.CaloGeometry_cfi import *
