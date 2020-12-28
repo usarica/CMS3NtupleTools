1d87e4d80fd5171f1ab8b289a56b92a3e9c5be80
Ulascan Sarica <ulascan90@gmail.com>
2020-12-28 06:58:51 -0800

    Tweak submission script

--- NtupleMaker/test/metis_condor_executable.sh
+++ NtupleMaker/test/metis_condor_executable.sh
@@ -23,19 +23,23 @@ function getjobad {
 }
 function setup_chirp {
     if [ -e ./condor_chirp ]; then
-    # Note, in the home directory
+        # Note, in the home directory
         mkdir chirpdir
         mv condor_chirp chirpdir/
-        export PATH="$PATH:$(pwd)/chirpdir"
+        export PATH="${PATH}:$(pwd)/chirpdir"
         echo "[chirp] Found and put condor_chirp into $(pwd)/chirpdir"
     elif [ -e /usr/libexec/condor/condor_chirp ]; then
-        export PATH="$PATH:/usr/libexec/condor"
+        export PATH="${PATH}:/usr/libexec/condor"
         echo "[chirp] Found condor_chirp in /usr/libexec/condor"
     else
         echo "[chirp] No condor_chirp :("
     fi
 }
 function chirp {
+    command -v condor_chirp &> /dev/null
+    if [[ $? -ne 0 ]]; then
+      exit 0 # Just exit normally
+    fi
     # Note, $1 (the classad name) must start with Chirp
     condor_chirp set_job_attr_delayed $1 $2
     ret=$?
@@ -58,7 +62,9 @@ echo "OTHEROUTPUTS: $OTHEROUTPUTS"
 echo "PSETARGS: $PSETARGS"
 # echo  CLASSAD: $(cat "$_CONDOR_JOB_AD")
 
-echo "GLIDEIN_CMSSite: $GLIDEIN_CMSSite"
+if [[ ! -z ${GLIDEIN_CMSSite+x} ]]; then
+  echo "GLIDEIN_CMSSite: $GLIDEIN_CMSSite"
+fi
 echo "hostname: $(hostname)"
 echo "uname -a: $(uname -a)"
 echo "time: $(date +%s)"
@@ -106,18 +112,19 @@ else
     eval `scramv1 runtime -sh`
     mv ../$PSET pset.py
     if [ -e ../${tarfile} ]; then
-        mv ../${tarfile} ${tarfile};
-        tar xf ${tarfile};
+        mv ../${tarfile} ${tarfile}
+        tar xf ${tarfile}
     fi
     scram b
-    [ -e package.tar.gz ] && tar xf package.tar.gz
-    # Needed or else cmssw can't find libmcfm_[xyz].so
-    # export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${CMSSW_BASE}/src/JHUGenMELA/MELA/data/${SCRAM_ARCH}
-    # This is nicer than above. both work, and both have scary but benign warnings/printouts
-    cp ${CMSSW_BASE}/src/JHUGenMELA/MELA/data/${SCRAM_ARCH}/*.so ${CMSSW_BASE}/lib/${SCRAM_ARCH}/
-    # "Needed" to get rid of benign warnings/printouts
-    export ROOT_INCLUDE_PATH=${ROOT_INCLUDE_PATH}:${CMSSW_BASE}/src/JHUGenMELA/MELA/interface
 fi
+
+# Needed or else cmssw can't find libmcfm_[xyz].so
+# export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${CMSSW_BASE}/src/JHUGenMELA/MELA/data/${SCRAM_ARCH}
+# This is nicer than above. both work, and both have scary but benign warnings/printouts
+cp ${CMSSW_BASE}/src/JHUGenMELA/MELA/data/${SCRAM_ARCH}/*.so ${CMSSW_BASE}/lib/${SCRAM_ARCH}/
+# "Needed" to get rid of benign warnings/printouts
+export ROOT_INCLUDE_PATH=${ROOT_INCLUDE_PATH}:${CMSSW_BASE}/src/JHUGenMELA/MELA/interface
+
 
 # # logging every 45 seconds gives ~100kb log file/3 hours
 # dstat -cdngytlmrs --float --nocolor -T --output dsout.csv 180 >& /dev/null &
